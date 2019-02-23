a7478c101674b70c66d7872c90e1e0a27e6cd713
naveenm <naveenm@kth.se>
2019-02-23 21:43:48 +0100

    Refactoring magic literals: Full paths now built only once, improved readability

--- AD-EYE/ROS_Packages/src/AD-EYE/src/manager.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/manager.py
@@ -6,12 +6,14 @@ from std_msgs.msg import Int32
 from std_msgs.msg import Bool
 from roslaunch import rlutil, parent, configure_logging
 
-
 # Config: Common to more files and will be exported out
+
+# Basic folder locations
 ADEYE_PACKAGE_LOCATION = "/home/naveenm/AD-EYE-WASP/AD-EYE/ROS_Packages/src/AD-EYE/"
 QUICK_START_LOCATION = "quick_start/"
 LAUNCH_FOLDER_LOCATION = "launch/"
 
+# Names of each launch file
 RVIZ_LAUNCH_FILE_NAME = "my_rviz.launch"
 MAPPING_LAUNCH_FILE_NAME = "my_map.launch"
 LOCALIZATION_LAUNCH_FILE_NAME = "my_localization.launch"
@@ -21,32 +23,39 @@ SWITCH_LAUNCH_FILE_NAME = "switch.launch"
 MISSION_PLANNING_LAUNCH_FILE_NAME = "my_mission_planning.launch"
 MOTION_PLANNING_LAUNCH_FILE_NAME = "my_motion_planning.launch"
 
+# Full path to each launch file
+RVIZ_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, RVIZ_LAUNCH_FILE_NAME))
+MAPPING_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, MAPPING_LAUNCH_FILE_NAME))
+LOCALIZATION_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, LOCALIZATION_LAUNCH_FILE_NAME))
+SENSING_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, SENSING_LAUNCH_FILE_NAME))
+DETECTION_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, DETECTION_LAUNCH_FILE_NAME))
+SWITCH_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, LAUNCH_FOLDER_LOCATION, SWITCH_LAUNCH_FILE_NAME))
+MISSION_PLANNING_FULL_PATH = (
+            "%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, MISSION_PLANNING_LAUNCH_FILE_NAME))
+MOTION_PLANNING_FULL_PATH = (
+            "%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, MOTION_PLANNING_LAUNCH_FILE_NAME))
+
 state = 0  # 0=wait | 1=run
 
 uuid3 = rlutil.get_or_generate_uuid(None, False)
 configure_logging(uuid3)
-launch3 = parent.ROSLaunchParent(uuid3, [
-    ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, LOCALIZATION_LAUNCH_FILE_NAME,))])
+launch3 = parent.ROSLaunchParent(uuid3, [LOCALIZATION_FULL_PATH])
 
 uuid4 = rlutil.get_or_generate_uuid(None, False)
 configure_logging(uuid4)
-launch4 = parent.ROSLaunchParent(uuid4, [
-    (ADEYE_PACKAGE_LOCATION % DETECTION_LAUNCH_FILE_NAME)])
+launch4 = parent.ROSLaunchParent(uuid4, [DETECTION_FULL_PATH])
 
 uuid5 = rlutil.get_or_generate_uuid(None, False)
 configure_logging(uuid5)
-launch5 = parent.ROSLaunchParent(uuid5, [
-    (ADEYE_PACKAGE_LOCATION % MISSION_PLANNING_LAUNCH_FILE_NAME)])
+launch5 = parent.ROSLaunchParent(uuid5, [MISSION_PLANNING_FULL_PATH])
 
 uuid6 = rlutil.get_or_generate_uuid(None, False)
 configure_logging(uuid6)
-launch6 = parent.ROSLaunchParent(uuid6, [
-    (ADEYE_PACKAGE_LOCATION % MOTION_PLANNING_LAUNCH_FILE_NAME)])
+launch6 = parent.ROSLaunchParent(uuid6, [MOTION_PLANNING_FULL_PATH])
 
 uuid7 = rlutil.get_or_generate_uuid(None, False)
 configure_logging(uuid7)
-launch7 = parent.ROSLaunchParent(uuid7, [
-    ("%s%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, SWITCH_LAUNCH_FILE_NAME,))])
+launch7 = parent.ROSLaunchParent(uuid7, [SWITCH_FULL_PATH])
 
 pmap_stat_bool = False
 
@@ -90,7 +99,7 @@ def mycallback(data):
         rospy.loginfo("started6")
 
         launch7 = parent.ROSLaunchParent(uuid7, [
-            ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, SWITCH_LAUNCH_FILE_NAME, LAUNCH_FOLDER_LOCATION))])
+            SWITCH_FULL_PATH])
         launch7.start()
         rospy.loginfo("MANAGER: Switch launched")
 
@@ -129,16 +138,14 @@ if __name__ == '__main__':
     # Launch Rviz
     uuid0 = rlutil.get_or_generate_uuid(None, False)
     configure_logging(uuid0)
-    launch0 = parent.ROSLaunchParent(uuid0, [
-        (ADEYE_PACKAGE_LOCATION % RVIZ_LAUNCH_FILE_NAME)])
+    launch0 = parent.ROSLaunchParent(uuid0, [RVIZ_FULL_PATH])
     launch0.start()
     rospy.loginfo("MANAGER: Rviz launched")
 
     # Launch the map
     uuid1 = rlutil.get_or_generate_uuid(None, False)
     configure_logging(uuid1)
-    launch1 = parent.ROSLaunchParent(uuid1, [
-        (ADEYE_PACKAGE_LOCATION % MAPPING_LAUNCH_FILE_NAME)])
+    launch1 = parent.ROSLaunchParent(uuid1, [MAPPING_FULL_PATH])
     launch1.start()
     rospy.loginfo("MANAGER: Map launched")
     rospy.Subscriber("/pmap_stat", Bool, pmap_stat_callback)
@@ -150,8 +157,7 @@ if __name__ == '__main__':
     # Launch the sensing
     uuid2 = rlutil.get_or_generate_uuid(None, False)
     configure_logging(uuid2)
-    launch2 = parent.ROSLaunchParent(uuid2, [
-        (ADEYE_PACKAGE_LOCATION % SENSING_LAUNCH_FILE_NAME)])
+    launch2 = parent.ROSLaunchParent(uuid2, [SENSING_FULL_PATH])
     launch2.start()
     rospy.loginfo("MANAGER: Sensing launched")
 
