c52a3beaa156742a14b18afcd51078bbe3b999c3
naveenm <naveenm@kth.se>
2019-02-25 23:26:23 +0100

    Bugfix: require unique ID per start/stop of roslaunch

--- AD-EYE/ROS_Packages/src/AD-EYE/src/manager.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/manager.py
@@ -71,12 +71,16 @@ class FeatureControl:
     def __init__(self, filepath, feature_name, sleep_time_on_start=DEFAULT_WAIT_TIME, sleep_time_on_stop=DEFAULT_WAIT_TIME):
         self.uuid = rlutil.get_or_generate_uuid(None, False)
         configure_logging(self.uuid)
+        self.filepath = filepath
         self.Launch = parent.ROSLaunchParent(self.uuid, [filepath])
         self.FeatureName = feature_name
         self.sleep_time_on_start = sleep_time_on_start
         self.sleep_time_on_stop = sleep_time_on_stop
 
     def start(self):
+        self.uuid = rlutil.get_or_generate_uuid(None, False)
+        configure_logging(self.uuid)
+        self.Launch = parent.ROSLaunchParent(self.uuid, [self.filepath])
         self.Launch.start()
         rospy.loginfo("AD-EYE MANAGER: Started feature - %s", self.FeatureName)
         time.sleep(self.sleep_time_on_start)
