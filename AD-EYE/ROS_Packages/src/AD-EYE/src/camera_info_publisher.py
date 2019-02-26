89c634356b3beed3a46fa303bf8fd05419b4c8ef
naveenm <naveenm@kth.se>
2019-02-26 16:16:08 +0100

    Extracted Config from camera_info_publisher

--- AD-EYE/ROS_Packages/src/AD-EYE/src/camera_info_publisher.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/camera_info_publisher.py
@@ -3,19 +3,24 @@
 import rospy
 from sensor_msgs.msg import CameraInfo
 
+# --------------Config: To be imported from Simulink -------------------------------------------------
+# TODO Camera parameters should come from Simulink once, hardcoded for now
+
+msg = CameraInfo()
+msg.header.frame_id = 'camera'
+msg.height = 240
+msg.width = 320
+msg.D = [0.0, 0.0, 0.0, 0.0, 0.0]
+msg.K = [375, 0.0, 160, 0.0, 375, 120, 0.0, 0.0, 1.0]
+msg.R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+msg.P = [375, 0.0, 160, 0.0, 0.0, 375, 120, 0.0, 0.0, 0.0, 1.0, 0.0]
+
+# --------------------------------------------------------------------------------------------------------------------
+
 if __name__ == '__main__':
     rospy.init_node('camera_info_publisher', anonymous=True)
     rate = rospy.Rate(10.0)
     pub = rospy.Publisher('/camera_info', CameraInfo, queue_size=1)
     while not rospy.is_shutdown():
-        msg = CameraInfo()
-        msg.header.frame_id = 'camera'
-        msg.height = 240
-        msg.width = 320
-        msg.D = [0.0, 0.0, 0.0, 0.0, 0.0]
-        msg.K = [375, 0.0, 160, 0.0, 375, 120, 0.0, 0.0, 1.0]
-        msg.R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
-        msg.P = [375, 0.0, 160, 0.0, 0.0, 375, 120, 0.0, 0.0, 0.0, 1.0, 0.0]
-
         pub.publish(msg)
         rate.sleep()
