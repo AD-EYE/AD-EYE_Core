fc072a8a6193b690c833d467d12c81b3782a8137
naveenm <naveenm@kth.se>
2019-02-26 21:42:49 +0100

    Added more test cases for Feature Control class and tested with rosunit

--- AD-EYE/ROS_Packages/src/AD-EYE/src/test_featureControl.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/test_featureControl.py
@@ -1,22 +1,59 @@
+#!/usr/bin/env python
 from unittest import TestCase
-import time
 from FeatureControl import FeatureControl
+import rosnode
 
-SECS = 1
+Node_to_check = '/controlSwitch'
+
+SECS = 0.5
 filepath = "/home/naveenm/AD-EYE-WASP/AD-EYE/ROS_Packages/src/AD-EYE/launch/switch.launch"
 
 
 class TestFeatureControl(TestCase):
-    def test_start(self):
+
+    # Test to ensure a new Launch file is created each time start() is called, to avoid bugs where rllaunch fails due
+    # an incorrect launch configuration.
+    # Test checks that the object used to start a launch has changed
+
+    def test_start_1(self):
         test_feature = FeatureControl(filepath, "test_node", SECS, SECS)
         original_launch = test_feature.Launch
         test_feature.start()
-        time.sleep(SECS)
         test_feature.stop()
-        time.sleep(SECS)
         test_feature.start()
         new_launch = test_feature.Launch
         self.assertNotEqual(original_launch, new_launch)
+        test_feature.stop()
 
-    def test_stop(self):
-        self.fail()
+    # Test to ensure that the start() function actually starts a node
+    # Test kills nodes that could be running with the same name, then starts roslaunch
+    # and checks that the intended node is running as rosnode.get_node_names()
+    # only returns nodes that are currently running
+    def test_start_2(self):
+        test_feature = FeatureControl(filepath, "test_node", SECS, SECS)
+        rosnode.kill_nodes(Node_to_check)
+        test_feature.start()
+
+        X = rosnode.get_node_names()
+        test_feature.stop()
+        if Node_to_check in X:
+            pass
+        else:
+            self.fail()
+
+    # Test to ensure that the Launch file is stopped once the function stop() executes
+    # The launch file is considered to have been killed when the node it had started is no longer running
+    def test_stop_1(self):
+        test_feature = FeatureControl(filepath, "test_node", SECS, SECS)
+        test_feature.start()
+        test_feature.stop()
+        with self.assertRaises(Exception):
+            X = rosnode.get_node_names()
+        print "rereesss"
+
+
+if __name__ == '__main__':
+    # Ros unit testing framework is used to run all tests and save in .xml format
+    import rosunit
+
+    rosunit.unitrun('Feature_ctrl', 'test_bare_bones', TestFeatureControl)
