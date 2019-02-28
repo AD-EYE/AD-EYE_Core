0de6d0c2a6f9278a75e513113738fa97fcb6cbb0
naveenm <naveenm@kth.se>
2019-02-28 16:16:09 +0100

    Added José's changes for PreScan 8.6 xml read

--- AD-EYE/ROS_Packages/src/AD-EYE/include/prescanmodelloader.h
+++ AD-EYE/ROS_Packages/src/AD-EYE/include/prescanmodelloader.h
@@ -54,20 +54,20 @@ public:
     else{
       while (inputFile.good()){
         std::getline (inputFile,line);
-        if(line.find("<NatureElement xsi") != std::string::npos || line.find("<Building xsi") != std::string::npos || line.find("<InfraOther xsi") != std::string::npos){
+        if(line.find("<NatureElement xsi") != std::string::npos || line.find(" ObjectTypeID=\"12\"") != std::string::npos || line.find("<InfraOther xsi") != std::string::npos){
           // get type
           if(line.find("Nature") != std::string::npos){
             pm.type.push_back(0);
           }
-          if(line.find("Building") != std::string::npos){
+          if(line.find(" ObjectTypeID=\"12\"") != std::string::npos){
             pm.type.push_back(1);
           }
           if(line.find("InfraOther") != std::string::npos){
             pm.type.push_back(2);
           }
-          
+
           // get ID
-          element = line.substr(line.find("UniqueId")+10, line.find("SensorIDDetection")-2-line.find("UniqueId")+10);
+          element = line.substr(line.find("UniqueId")+10, line.find("Sensor")-2-line.find("UniqueId")+10);
           value = strtof(element.c_str(),0);
           pm.ID.push_back(value);
 
@@ -75,7 +75,7 @@ public:
           std::getline (inputFile,line);
           element = line.substr(line.find("X=")+3, line.find("Y=")-2-line.find("X=")+3);
           value = strtof(element.c_str(),0);
-          pm.posX.push_back(value);  
+          pm.posX.push_back(value);
           element = line.substr(line.find("Y=")+3, line.find("Z=")-2-line.find("Y=")+3);
           value = strtof(element.c_str(),0);
           pm.posY.push_back(value);
@@ -84,22 +84,22 @@ public:
           std::getline (inputFile,line);
           element = line.substr(line.find("Heading=")+9, line.find("/>")-2-line.find("Heading=")+9);
           value = strtof(element.c_str(),0);
-          pm.yaw.push_back(value);    
+          pm.yaw.push_back(value);
 
           // get Size
           std::getline (inputFile,line);
           std::getline (inputFile,line);
           element = line.substr(line.find("X=")+3, line.find("Y=")-2-line.find("X=")+3);
           value = strtof(element.c_str(),0);
-          pm.sizeX.push_back(value);  
+          pm.sizeX.push_back(value);
           element = line.substr(line.find("Y=")+3, line.find("Z=")-2-line.find("Y=")+3);
           value = strtof(element.c_str(),0);
-          pm.sizeY.push_back(value);      
+          pm.sizeY.push_back(value);
           element = line.substr(line.find("Z=")+3, line.find("/>")-2-line.find("Z=")+3);
           value = strtof(element.c_str(),0);
-          pm.sizeZ.push_back(value);  
+          pm.sizeZ.push_back(value);
         }
-      }         
+      }
     }
     inputFile.close();
   }
