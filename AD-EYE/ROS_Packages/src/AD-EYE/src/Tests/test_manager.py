2a6b9558a7ea17a3825cb2046ad397107d0d41d6
naveenm <naveenm@kth.se>
2019-02-28 17:03:30 +0100

    Added Unit Test for manager to check for invalid values

--- AD-EYE/ROS_Packages/src/AD-EYE/src/Tests/test_manager.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/Tests/test_manager.py
@@ -11,6 +11,7 @@ ROSCORE_SLEEP_TIME = 1
 
 ENABLE_SIMULINK_STATE = Int32(1)
 DISABLE_SIMULINK_STATE = Int32(0)
+INVALID_VALUE_SIMULINK_STATE = Int32(5)
 
 manager = importlib.import_module('manager')
 
@@ -20,7 +21,6 @@ manager.Mission_planning = FeatureControl(manager.MISSION_PLANNING_FULL_PATH, "M
 manager.Motion_planning = FeatureControl(manager.MOTION_PLANNING_FULL_PATH, "Motion_Planning")
 manager.Ssmp = FeatureControl(manager.SSMP_FULL_PATH, "SSMP")
 
-print manager.ENABLED
 
 
 class TestManager(TestCase):
@@ -29,7 +29,6 @@ class TestManager(TestCase):
     # The tests compare the number of currently running nodes as the rationale for success/failure
     # Disable -> enable should increase number of nodes running in ROS and vice versa. (Tests 1 and 2)
     # Disable -> Disable OR Enable -> Enable should not change number of running nodes (Tests 3 and 4)
-
     def Test_1(self):
         subprocess.Popen('roscore')
         rospy.sleep(ROSCORE_SLEEP_TIME)
@@ -63,6 +62,16 @@ class TestManager(TestCase):
         manager.simulink_state_callback(DISABLE_SIMULINK_STATE)
         initial_node_count = len(rosnode.get_node_names())
         manager.simulink_state_callback(DISABLE_SIMULINK_STATE)
+        final_node_count = len(rosnode.get_node_names())
+        self.assertEqual(final_node_count, initial_node_count)
+
+    # Test_5 checks that an invalid simulink state command does not have any effect i.e. does not launch any new nodes
+
+    def Test_5(self):
+        subprocess.Popen('roscore')
+        rospy.sleep(ROSCORE_SLEEP_TIME)
+        initial_node_count = len(rosnode.get_node_names())
+        manager.simulink_state_callback(INVALID_VALUE_SIMULINK_STATE)
         final_node_count = len(rosnode.get_node_names())
         self.assertEqual(final_node_count, initial_node_count)
 
