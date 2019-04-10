c9dfec2e5cabaf1a35bbbafbeb6161316fb8b5cf
Jose Manuel <jmgs@kth.se>
2019-04-10 15:24:15 +0200

    Version 2 of the Safety Supervisor. Read the points of the vector map and activates the safety planner when the gps is not close to any point.

--- AD-EYE/ROS_Packages/src/AD-EYE/src/safetySupervisor.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/safetySupervisor.py
@@ -6,6 +6,10 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Pose
 from std_msgs.msg import Int32
+from vector_map_msgs.msg import PointArray
+from vector_map_msgs.msg import Point
+#from grid_map_msgs.msg import GridMap
+#from grid_map_ros.hpp import grid_map_ros
 
 class safetySupervisor:
     def __init__(self):
@@ -15,6 +19,7 @@ class safetySupervisor:
 
         # Define the subscribers
         rospy.Subscriber("gnss_pose", PoseStamped, self.gnss_callback)
+        rospy.Subscriber("vector_map_info/point", PointArray, self.vecMapPoint_callback)
 
         # Define the publishers
         self.switch_pub = rospy.Publisher('switchCommand', Int32, queue_size=1)
@@ -22,15 +27,22 @@ class safetySupervisor:
         # Initialize the variables
         self.pose = Pose()
         self.state = self.SAFE
+        self.vectormap_Points = []
 
         # Initialize the flags
         self.gnss_flag = 0
+        self.vectormap_Points_flag = 0
 
     def evaluate(self):
-        if self.pose.position.x > 100:
-            self.state = self.UNSAFE
-        else:
-            self.state = self.SAFE
+        #if self.pose.position.x > 100:
+        #    self.state = self.UNSAFE
+        #else:
+        #    self.state = self.SAFE
+        self.state = self.UNSAFE
+        for i in range(0, len(self.vectormap_Points)):
+            if (abs(self.pose.position.x-self.vectormap_Points[i].ly)<1.75 and abs(self.pose.position.y-self.vectormap_Points[i].bx)<1.75):
+                self.state = self.SAFE
+                break
 
     def publish(self):
         msg = Int32()
@@ -42,10 +54,14 @@ class safetySupervisor:
         self.pose = data.pose
         self.gnss_flag = 1;
 
+    def vecMapPoint_callback(self, data):
+        self.vectormap_Points = data.data
+        self.vectormap_Points_flag = 1
+
     def loop(self):
         rate = rospy.Rate(10.0)
         while not rospy.is_shutdown():
-            if self.gnss_flag == 1:
+            if (self.gnss_flag == 1 and self.vectormap_Points_flag == 1):
                 self.evaluate()
                 self.publish()
                 rate.sleep()
