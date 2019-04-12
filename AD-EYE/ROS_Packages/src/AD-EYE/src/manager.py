89b058d3e28f2b1657433415f5ceb8035fea26e7
Jose Manuel <jmgs@kth.se>
2019-04-12 10:46:34 +0200

    Now the manager receives a list of values to decide the launchers that it should execute.

--- AD-EYE/ROS_Packages/src/AD-EYE/src/manager.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/manager.py
@@ -2,7 +2,7 @@
 # license removed for brevity
 import rospy
 import rospkg
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32MultiArray
 from FeatureControl import FeatureControl
 
 #  --------------Config: Common to more files and will be exported out--------------------------------------------------
@@ -34,6 +34,7 @@ LAUNCH_FOLDER_LOCATION = "launch/"
 RVIZ_LAUNCH_FILE_NAME = "my_rviz.launch"
 MAPPING_LAUNCH_FILE_NAME = "my_map.launch"
 LOCALIZATION_LAUNCH_FILE_NAME = "my_localization.launch"
+FAKE_LOCALIZATION_LAUNCH_FILE_NAME = "my_fake_localization.launch"
 SENSING_LAUNCH_FILE_NAME = "my_sensing.launch"
 DETECTION_LAUNCH_FILE_NAME = "my_detection.launch"
 SWITCH_LAUNCH_FILE_NAME = "switch.launch"
@@ -45,6 +46,7 @@ SSMP_LAUNCH_FILE_NAME = "SSMP.launch"
 RVIZ_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, RVIZ_LAUNCH_FILE_NAME))
 MAPPING_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, MAPPING_LAUNCH_FILE_NAME))
 LOCALIZATION_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, LOCALIZATION_LAUNCH_FILE_NAME))
+FAKE_LOCALIZATION_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, FAKE_LOCALIZATION_LAUNCH_FILE_NAME))
 SENSING_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, SENSING_LAUNCH_FILE_NAME))
 DETECTION_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, QUICK_START_LOCATION, DETECTION_LAUNCH_FILE_NAME))
 SWITCH_FULL_PATH = ("%s%s%s" % (ADEYE_PACKAGE_LOCATION, LAUNCH_FOLDER_LOCATION, SWITCH_LAUNCH_FILE_NAME))
@@ -65,58 +67,106 @@ MOTION_PLANNING_STOP_WAIT_TIME = 10
 
 #  ---------------------------------------------------------------------------------------------------------------------
 
-previous_simulink_state = DISABLED  # DISABLED = 0 = wait | ENABLED = 1 = run
+previous_simulink_state = [DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED]  # DISABLED = 0 = wait | ENABLED = 1 = run
+current_simulink_state = [DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED, DISABLED]
 point_map_ready = False
 
 
-def simulink_state_callback(current_simulink_state):
-    global previous_simulink_state
+def simulink_state_callback(msg):
+    global current_simulink_state
 
-    if previous_simulink_state != current_simulink_state.data:
-
-        if current_simulink_state.data == ENABLED:
-            Localization.start()
-            Detection.start()
-            Mission_planning.start()
-            Motion_planning.start()
-            Ssmp.start()
-
-        if current_simulink_state.data == DISABLED:
-            Localization.stop()
-            Detection.stop()
-            Mission_planning.stop()
-            Motion_planning.stop()
-            Ssmp.stop()
-
-        previous_simulink_state = current_simulink_state.data
-        rospy.loginfo("Simulink command change registered")
-
+    if msg.data != current_simulink_state:
+        rospy.loginfo("Message received")
+        current_simulink_state = msg.data
 
 if __name__ == '__main__':
     rospy.init_node('ADEYE_Manager')
     rospy.loginfo("ADEYE Manager: Started")
 
     # Set up subscribers for registering simulink control command
-    rospy.Subscriber("/Simulink_state", Int32, simulink_state_callback)
+    rospy.Subscriber("/Simulink_state", Int32MultiArray, simulink_state_callback)
 
     # Create Feature Control objects
     Rviz = FeatureControl(RVIZ_FULL_PATH, "Rviz")
     Mapping = FeatureControl(MAPPING_FULL_PATH, "Mapping", MAPPING_START_WAIT_TIME)
+    Sensing = FeatureControl(SENSING_FULL_PATH, "Sensing")
     Localization = FeatureControl(LOCALIZATION_FULL_PATH, "Localization", LOCALIZATION_START_WAIT_TIME,
                                   LOCALIZATION_STOP_WAIT_TIME)
-    Sensing = FeatureControl(SENSING_FULL_PATH, "Sensing")
+    Fake_Localization = FeatureControl(FAKE_LOCALIZATION_FULL_PATH, "Fake_Localization")
     Detection = FeatureControl(DETECTION_FULL_PATH, "Detection", sleep_time_on_stop=DETECTION_STOP_WAIT_TIME)
-    Switch = FeatureControl(SWITCH_FULL_PATH, "Switch")
     Mission_planning = FeatureControl(MISSION_PLANNING_FULL_PATH, "Mission_Planning", MISSION_PLANNING_START_WAIT_TIME,
                                       MISSION_PLANNING_STOP_WAIT_TIME)
     Motion_planning = FeatureControl(MOTION_PLANNING_FULL_PATH, "Motion_Planning",
                                      sleep_time_on_stop=MOTION_PLANNING_STOP_WAIT_TIME)
+    Switch = FeatureControl(SWITCH_FULL_PATH, "Switch")
     Ssmp = FeatureControl(SSMP_FULL_PATH, "SSMP")
 
-    # Launch basic features
-    Switch.start()
-    Rviz.start()
-    Mapping.start()
-    Sensing.start()
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        if current_simulink_state != previous_simulink_state:
+            if previous_simulink_state[0] != current_simulink_state[0]:
+                if current_simulink_state[0] == ENABLED:
+                    Rviz.start()
+                if current_simulink_state[0] == DISABLED:
+                    Rviz.stop()
 
-    rospy.spin()
+            if previous_simulink_state[1] != current_simulink_state[1]:
+                if current_simulink_state[1] == ENABLED:
+                    Mapping.start()
+                if current_simulink_state[1] == DISABLED:
+                    Mapping.stop()
+
+            if previous_simulink_state[2] != current_simulink_state[2]:
+                if current_simulink_state[2] == ENABLED:
+                    Sensing.start()
+                if current_simulink_state[2] == DISABLED:
+                    Sensing.stop()
+
+            if previous_simulink_state[3] != current_simulink_state[3]:
+                if current_simulink_state[3] == ENABLED:
+                    Localization.start()
+                if current_simulink_state[3] == DISABLED:
+                    Localization.stop()
+
+            if previous_simulink_state[4] != current_simulink_state[4]:
+                if current_simulink_state[4] == ENABLED:
+                    Fake_Localization.start()
+                if current_simulink_state[4] == DISABLED:
+                    Fake_Localization.stop()
+
+            if previous_simulink_state[5] != current_simulink_state[5]:
+                if current_simulink_state[5] == ENABLED:
+                    Detection.start()
+                if current_simulink_state[5] == DISABLED:
+                    Detection.stop()
+
+            if previous_simulink_state[6] != current_simulink_state[6]:
+                if current_simulink_state[6] == ENABLED:
+                    Mission_planning.start()
+                if current_simulink_state[6] == DISABLED:
+                    Mission_planning.stop()
+
+            if previous_simulink_state[7] != current_simulink_state[7]:
+                if current_simulink_state[7] == ENABLED:
+                    Motion_planning.start()
+                if current_simulink_state[7] == DISABLED:
+                    Motion_planning.stop()
+
+            if previous_simulink_state[8] != current_simulink_state[8]:
+                if current_simulink_state[8] == ENABLED:
+                    Switch.start()
+                if current_simulink_state[8] == DISABLED:
+                    Switch.stop()
+
+            if previous_simulink_state[9] != current_simulink_state[9]:
+                if current_simulink_state[9] == ENABLED:
+                    Ssmp.start()
+                if current_simulink_state[9] == DISABLED:
+                    Ssmp.stop()
+            previous_simulink_state = current_simulink_state
+        rate.sleep()
+    # Launch basic features
+    #Switch.start()
+    #Rviz.start()
+    #Mapping.start()
+    #Sensing.start()
