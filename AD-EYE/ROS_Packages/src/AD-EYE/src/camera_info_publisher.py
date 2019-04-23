7b6c81a18fab19c01ef65b4345e13a36dba6285b
Jose Manuel <jmgs@kth.se>
2019-04-23 14:04:59 +0200

    Changes to use two independent cameras, one for traffic light detection and the other one for object recognition.

--- AD-EYE/ROS_Packages/src/AD-EYE/src/camera_info_publisher.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/camera_info_publisher.py
@@ -15,12 +15,23 @@ msg.K = [1125, 0.0, 480, 0.0, 1125, 360, 0.0, 0.0, 1.0]
 msg.R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
 msg.P = [1125, 0.0, 480, 0.0, 0.0, 1125, 360, 0.0, 0.0, 0.0, 1.0, 0.0]
 
+msg2 = CameraInfo()
+msg2.header.frame_id = 'tl_camera'
+msg2.height = 720
+msg2.width = 960
+msg2.D = [0.0, 0.0, 0.0, 0.0, 0.0]
+msg2.K = [1125, 0.0, 480, 0.0, 1125, 360, 0.0, 0.0, 1.0]
+msg2.R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+msg2.P = [1125, 0.0, 480, 0.0, 0.0, 1125, 360, 0.0, 0.0, 0.0, 1.0, 0.0]
+
 # --------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     rospy.init_node('camera_info_publisher', anonymous=True)
     rate = rospy.Rate(10.0)
     pub = rospy.Publisher('/camera_info', CameraInfo, queue_size=1)
+    pub2 = rospy.Publisher('/tl_camera/camera_info', CameraInfo, queue_size=1)
     while not rospy.is_shutdown():
         pub.publish(msg)
+        pub2.publish(msg2)
         rate.sleep()
