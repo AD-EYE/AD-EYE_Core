7c0355f8e9c4579f5d25b8ad9f25cb3ee22acb5b
Florent <florent2@kth.se>
2019-09-20 16:14:34 +0200

    Moved the gridmap initialisation from the constructor to a dedicated method.

--- AD-EYE/ROS_Packages/src/AD-EYE/src/GridMapCreator_node.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/GridMapCreator_node.cpp
@@ -59,6 +59,7 @@ private:
     bool DynamicObjectsInitialized = false;
     //Map
     GridMap map;
+    float mapResolution;
     float submap_dimensions;
     //Ros utils
     ros::Rate rate;
@@ -77,7 +78,7 @@ public:
 
         // these three variables determine the performance of gridmap, the code will warn you whenever the performance becomes to slow to make the frequency
         submap_dimensions = 35;               // Both the length and the width of the submap in meters, increasing this value will cause the flattening node to become slower, 35 is the minimum value
-        float mapresolution = 0.25;           // 0.25 or lower number is the desired resolution, load time will significantly increase when increasing mapresolution,
+        mapResolution = 0.25;                 // 0.25 or lower number is the desired resolution, load time will significantly increase when increasing mapresolution,
         frequency = 30;                       // 20 Hz is the minimum desired rate to make sure dynamic objects are accurately tracked, remember to allign this value with the flattening_node
         rate = ros::Rate(frequency);
 
@@ -93,124 +94,7 @@ public:
         x_egoOld = 0;
         y_egoOld = 0;
 
-        // read out the vectormap files and store all information in 'veclane'
-        ros::NodeHandle p_nh("~");
-        std::string filePoint = p_nh.param<std::string>("filePoint","");
-        std::string fileLane = p_nh.param<std::string>("fileLane","");
-        std::string fileNode = p_nh.param<std::string>("fileNode","");
-        std::string fileDtlane = p_nh.param<std::string>("fileDtlane","");
-        VectorMap veclane;
-        veclane.load_vectormap(filePoint, fileLane, fileNode, fileDtlane);
-
-        // read out prescanmap from the pex file and store all information in 'pexObjects'
-        std::string filePex = p_nh.param<std::string>("filePex","");
-        PrescanModel pexObjects;
-        pexObjects.load_pexmap(filePex);
-
-        // Determine the boundaries of the map based on the maximum and minimum values for x and y as saved in the vectormap
-        float lowest_x = veclane.point_x.at(veclane.node_pid.at(0)-1);
-        float highest_x = veclane.point_x.at(veclane.node_pid.at(0)-1);
-        float lowest_y = veclane.point_y.at(veclane.node_pid.at(0)-1);
-        float highest_y = veclane.point_y.at(veclane.node_pid.at(0)-1);
-        for(int i = 1; i < (int)veclane.node_pid.size(); i++){
-            if(veclane.point_x.at(veclane.node_pid.at(i)-1) < lowest_x){
-                lowest_x = veclane.point_x.at(veclane.node_pid.at(i)-1);
-            }
-            if(veclane.point_x.at(veclane.node_pid.at(i)-1) > highest_x){
-                highest_x = veclane.point_x.at(veclane.node_pid.at(i)-1);
-            }
-            if(veclane.point_y.at(veclane.node_pid.at(i)-1) < lowest_y){
-                lowest_y = veclane.point_y.at(veclane.node_pid.at(i)-1);
-            }
-            if(veclane.point_y.at(veclane.node_pid.at(i)-1) > highest_y){
-                highest_y = veclane.point_y.at(veclane.node_pid.at(i)-1);
-            }
-        }
-        ROS_INFO("X: (%f, %f), Y: (%f, %f)", lowest_x, highest_x, lowest_y, highest_y);
-
-        // Create grid map consisting of four layers
-        map = GridMap({"StaticObjects", "DrivableAreas", "DynamicObjects", "Lanes"});
-        map.setFrameId("SSMP_map");
-        float maplength_x = highest_x-lowest_x;
-        float maplength_y = highest_y-lowest_y;
-        map.setGeometry(Length(maplength_x, maplength_y), mapresolution, Position(lowest_x+0.5*maplength_x, lowest_y+0.5*maplength_y));
-        ROS_INFO("Created map with size %f x %f m (%i x %i cells).", map.getLength().x(), map.getLength().y(), map.getSize()(0), map.getSize()(1));
-
-        // use data from pex file and vectormap to fill in the layers of the gridmap
-        // All cells in all layers must first be initialized to 0
-        for (GridMapIterator it(map); !it.isPastEnd(); ++it) {
-            map.at("DrivableAreas", *it) = 0;
-            map.at("StaticObjects", *it) = 0;
-            map.at("DynamicObjects", *it) = 0;
-            map.at("Lanes", *it) = 0;
-        }
-
-        // pex file data is used to built the staticObjects layer (stuff like buildings, nature, traffic lights), the value given to the cells is the height of the static object
-        for(int i = 0; i < (int)pexObjects.ID.size(); i++){
-            // building objects are always considered to be rectangles, and as such, a rectangle is created and the PolygonIterator is used. The number 0.01745 is a conversion from degrees to radians
-            if(pexObjects.type.at(i) == BUILDING){
-                grid_map::Polygon polygon = rectangle_creator(pexObjects.posX.at(i), pexObjects.posY.at(i), pexObjects.sizeX.at(i), pexObjects.sizeY.at(i), 0.01745*pexObjects.yaw.at(i));
-                for (grid_map::PolygonIterator iterator(map, polygon); !iterator.isPastEnd(); ++iterator) {
-                    map.at("StaticObjects", *iterator) = pexObjects.sizeZ.at(i);
-                }
-            }
-            // Traffic lights are created by filling in just one or two cells, as traffic lights are usually very small
-            if(pexObjects.type.at(i) == TRAFFICLIGHT){
-                Position center(pexObjects.posX.at(i), pexObjects.posY.at(i));
-                float radius = mapresolution;
-                for (grid_map::CircleIterator iterator(map, center, radius); !iterator.isPastEnd(); ++iterator) {
-                    map.at("StaticObjects", *iterator) = pexObjects.sizeZ.at(i);
-                }
-            }
-            // Nature is created using the circleIterator, as trees and bushes do not really have a definite shape, a circle describes their shape the best
-            if(pexObjects.type.at(i) == NATURE){
-                Position center(pexObjects.posX.at(i), pexObjects.posY.at(i));
-                float radius;
-                if(pexObjects.sizeX.at(i) > pexObjects.sizeY.at(i)){
-                    radius = 0.5*pexObjects.sizeX.at(i);
-                }
-                else{
-                    radius = 0.5*pexObjects.sizeY.at(i);
-                }
-                for (grid_map::CircleIterator iterator(map, center, radius); !iterator.isPastEnd(); ++iterator) {
-                    map.at("StaticObjects", *iterator) = pexObjects.sizeZ.at(i);
-                }
-            }
-        }
-
-        //Map Road
-        grid_map::Polygon polygon;
-        polygon.setFrameId(map.getFrameId());
-        Position _point1;
-        Position _point2;
-        float dx;
-        float dy;
-        float angle;
-        float lanewidth;
-        for(int i = 0 ; i < static_cast<int>(veclane.lane_startpoint.size()) ; i++) { //For every lane
-            if(!nh.ok()) { break; }
-            polygon.removeVertices();
-            _point1.x() = veclane.point_x.at(veclane.node_pid.at(veclane.lane_startpoint.at(i) - 1 ) - 1); //Get the first
-            _point1.y() = veclane.point_y.at(veclane.node_pid.at(veclane.lane_startpoint.at(i) - 1 ) - 1);
-            _point2.x() = veclane.point_x.at(veclane.node_pid.at(veclane.lane_endpoint.at(i) - 1 ) - 1); // and the second point
-            _point2.y() = veclane.point_y.at(veclane.node_pid.at(veclane.lane_endpoint.at(i) - 1 ) - 1);
-            angle = atan2(_point2.y()-_point1.y(), _point2.x()-_point1.x()); //Calculate the angle
-            angle += HALF_PI;
-            lanewidth = veclane.dtlane_leftwidth.at(veclane.lane_did.at(i)-1);
-            dx = lanewidth*cos(angle);
-            dy = lanewidth*sin(angle);
-            polygon.addVertex(Position(_point2.x() + dx, _point2.y() + dy)); //Draw the rectangle around
-            polygon.addVertex(Position(_point1.x() + dx, _point1.y() + dy));
-            polygon.addVertex(Position(_point1.x() - dx, _point1.y() - dy));
-            polygon.addVertex(Position(_point2.x() - dx, _point2.y() - dy));
-            for(grid_map::PolygonIterator iterator(map, polygon) ; !iterator.isPastEnd() ; ++iterator) {
-                map.at("DrivableAreas", *iterator) = 1;
-            }
-            //Add a half-circle at the end to help filling when road turns
-            for(grid_map::CircleIterator iterator(map, _point2, lanewidth) ; !iterator.isPastEnd() ; ++iterator) {
-                map.at("DrivableAreas", *iterator) = 1;
-            }
-        }
+        initializeGridMap();
 
         // Create footprint for car
         footprint_ego.header.frame_id = "SSMP_base_link";
@@ -339,6 +223,126 @@ public:
           }
           rate.sleep();
       }
+    }
+
+    void initializeGridMap() {
+        // read out the vectormap files and store all information in 'veclane'
+        ros::NodeHandle p_nh("~");
+        std::string filePoint = p_nh.param<std::string>("filePoint","");
+        std::string fileLane = p_nh.param<std::string>("fileLane","");
+        std::string fileNode = p_nh.param<std::string>("fileNode","");
+        std::string fileDtlane = p_nh.param<std::string>("fileDtlane","");
+        VectorMap veclane;
+        veclane.load_vectormap(filePoint, fileLane, fileNode, fileDtlane);
+
+        // Determine the boundaries of the map based on the maximum and minimum values for x and y as saved in the vectormap
+        float lowest_x = veclane.point_x.at(veclane.node_pid.at(0)-1);
+        float highest_x = veclane.point_x.at(veclane.node_pid.at(0)-1);
+        float lowest_y = veclane.point_y.at(veclane.node_pid.at(0)-1);
+        float highest_y = veclane.point_y.at(veclane.node_pid.at(0)-1);
+        for(int i = 1; i < (int)veclane.node_pid.size(); i++){
+            if(veclane.point_x.at(veclane.node_pid.at(i)-1) < lowest_x){
+                lowest_x = veclane.point_x.at(veclane.node_pid.at(i)-1);
+            }
+            if(veclane.point_x.at(veclane.node_pid.at(i)-1) > highest_x){
+                highest_x = veclane.point_x.at(veclane.node_pid.at(i)-1);
+            }
+            if(veclane.point_y.at(veclane.node_pid.at(i)-1) < lowest_y){
+                lowest_y = veclane.point_y.at(veclane.node_pid.at(i)-1);
+            }
+            if(veclane.point_y.at(veclane.node_pid.at(i)-1) > highest_y){
+                highest_y = veclane.point_y.at(veclane.node_pid.at(i)-1);
+            }
+        }
+        ROS_INFO("X: (%f, %f), Y: (%f, %f)", lowest_x, highest_x, lowest_y, highest_y);
+
+        // Create grid map consisting of four layers
+        map = GridMap({"StaticObjects", "DrivableAreas", "DynamicObjects", "Lanes"});
+        map.setFrameId("SSMP_map");
+        float maplength_x = highest_x-lowest_x;
+        float maplength_y = highest_y-lowest_y;
+        map.setGeometry(Length(maplength_x, maplength_y), mapResolution, Position(lowest_x+0.5*maplength_x, lowest_y+0.5*maplength_y));
+        ROS_INFO("Created map with size %f x %f m (%i x %i cells).", map.getLength().x(), map.getLength().y(), map.getSize()(0), map.getSize()(1));
+
+        // All cells in all layers must first be initialized to 0
+        for (GridMapIterator it(map); !it.isPastEnd(); ++it) {
+            map.at("DrivableAreas", *it) = 0;
+            map.at("StaticObjects", *it) = 0;
+            map.at("DynamicObjects", *it) = 0;
+            map.at("Lanes", *it) = 0;
+        }
+
+        // read out prescanmap from the pex file and store all information in 'pexObjects'
+        std::string filePex = p_nh.param<std::string>("filePex","");
+        PrescanModel pexObjects;
+        pexObjects.load_pexmap(filePex);
+
+        // pex file data is used to built the staticObjects layer (stuff like buildings, nature, traffic lights), the value given to the cells is the height of the static object
+        for(int i = 0; i < (int)pexObjects.ID.size(); i++){
+            // building objects are always considered to be rectangles, and as such, a rectangle is created and the PolygonIterator is used. The number 0.01745 is a conversion from degrees to radians
+            if(pexObjects.type.at(i) == BUILDING){
+                grid_map::Polygon polygon = rectangle_creator(pexObjects.posX.at(i), pexObjects.posY.at(i), pexObjects.sizeX.at(i), pexObjects.sizeY.at(i), 0.01745*pexObjects.yaw.at(i));
+                for (grid_map::PolygonIterator iterator(map, polygon); !iterator.isPastEnd(); ++iterator) {
+                    map.at("StaticObjects", *iterator) = pexObjects.sizeZ.at(i);
+                }
+            }
+            // Traffic lights are created by filling in just one or two cells, as traffic lights are usually very small
+            if(pexObjects.type.at(i) == TRAFFICLIGHT){
+                Position center(pexObjects.posX.at(i), pexObjects.posY.at(i));
+                float radius = mapResolution;
+                for (grid_map::CircleIterator iterator(map, center, radius); !iterator.isPastEnd(); ++iterator) {
+                    map.at("StaticObjects", *iterator) = pexObjects.sizeZ.at(i);
+                }
+            }
+            // Nature is created using the circleIterator, as trees and bushes do not really have a definite shape, a circle describes their shape the best
+            if(pexObjects.type.at(i) == NATURE){
+                Position center(pexObjects.posX.at(i), pexObjects.posY.at(i));
+                float radius;
+                if(pexObjects.sizeX.at(i) > pexObjects.sizeY.at(i)){
+                    radius = 0.5*pexObjects.sizeX.at(i);
+                }
+                else{
+                    radius = 0.5*pexObjects.sizeY.at(i);
+                }
+                for (grid_map::CircleIterator iterator(map, center, radius); !iterator.isPastEnd(); ++iterator) {
+                    map.at("StaticObjects", *iterator) = pexObjects.sizeZ.at(i);
+                }
+            }
+        }
+
+        //Map Road with the data from the vector map
+        grid_map::Polygon polygon;
+        polygon.setFrameId(map.getFrameId());
+        Position point1;
+        Position point2;
+        float dx;
+        float dy;
+        float angle;
+        float lanewidth;
+        for(int i = 0 ; i < static_cast<int>(veclane.lane_startpoint.size()) ; i++) { //For every lane
+            if(!nh_.ok()) { break; }
+            polygon.removeVertices();
+            point1.x() = veclane.point_x.at(veclane.node_pid.at(veclane.lane_startpoint.at(i) - 1 ) - 1); //Get the first
+            point1.y() = veclane.point_y.at(veclane.node_pid.at(veclane.lane_startpoint.at(i) - 1 ) - 1);
+            point2.x() = veclane.point_x.at(veclane.node_pid.at(veclane.lane_endpoint.at(i) - 1 ) - 1); // and the second point
+            point2.y() = veclane.point_y.at(veclane.node_pid.at(veclane.lane_endpoint.at(i) - 1 ) - 1);
+            angle = atan2(point2.y()-point1.y(), point2.x()-point1.x()); //Calculate the angle
+            angle += HALF_PI;
+            lanewidth = veclane.dtlane_leftwidth.at(veclane.lane_did.at(i)-1);
+            dx = lanewidth*cos(angle);
+            dy = lanewidth*sin(angle);
+            polygon.addVertex(Position(point2.x() + dx, point2.y() + dy)); //Draw the rectangle around
+            polygon.addVertex(Position(point1.x() + dx, point1.y() + dy));
+            polygon.addVertex(Position(point1.x() - dx, point1.y() - dy));
+            polygon.addVertex(Position(point2.x() - dx, point2.y() - dy));
+            for(grid_map::PolygonIterator iterator(map, polygon) ; !iterator.isPastEnd() ; ++iterator) {
+                map.at("DrivableAreas", *iterator) = 1;
+            }
+            //Add a half-circle at the end to help filling when road turns
+            for(grid_map::CircleIterator iterator(map, point2, lanewidth) ; !iterator.isPastEnd() ; ++iterator) {
+                map.at("DrivableAreas", *iterator) = 1;
+            }
+        }
     }
 
     grid_map::Polygon rectangle_creator(float X, float Y, float length, float width, float angle){
