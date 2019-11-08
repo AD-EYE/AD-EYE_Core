7a48ac63e23d3af2913e6480aa604ce7ee472c4f
maurits2 <maurits2@kth.se>
2019-11-08 16:31:09 +0100

    TA with TACombination

--- AD-EYE/TA_OpenSCENARIO_interface.m
+++ AD-EYE/TA_OpenSCENARIO_interface.m
@@ -1,15 +1,33 @@
- name_model = "Simulation";
+ name_model = "Map_pedestrian_autoware";
 name_ego = "Mazda_RX8_Coupe_1"; 
-name_experiment_template = "Map_pedestrian_autoware";
-name_experiment = name_experiment_template;
+name_experiment = "Map_pedestrian_autoware";
 
-L = xosc_generation(name_experiment_template,models);
+ExpNameArray = ["Map_pedestrian_autoware1" "Map_pedestrian_autoware2"];
+AutowareExpNameArray = ["Map_pedestrian_autoware" ];
+AutowareConfigArray = ["AutowareConfig.xlsx" ];
+GoalConfigArray = ["GoalConfig.xlsx" ];
+SimulinkConfigArray = ["SimulinkConfig.xlsx"];
+TagsConfigArray = ["TagsConfig" ];
+SHHConfig = "ssh";
 
-for i = 1:L
-    name_experiment_template = strcat("Map_pedestrian_autoware",int2str(i));
-API_main(name_model,name_ego,name_experiment,name_experiment_template)
+% ExpNameArray = ["e1" "e2"];
+% AutowareExpNameArray = ["a1" "a2"];
+% AutowareConfigArray = ["ac1" "ac2" "ac3"];
+% GoalConfigArray = ["g1" "g2"];
+% SimulinkConfigArray = ["sc1" "sc2"];
+% TagsConfigArray = ["t1" "t2"];
+% SHHConfig = "ssh";
+
+%Creating multiple .xosc
+listOfNames = OpenScenarioMod(convertStringsToChars(name_experiment));
+
+for i = 1:length(listOfNames)
+    listOfNames(i)
+API_main(name_ego,name_experiment,listOfNames(i))
 end
 
-
-
-    
+%TACombinations(ExpNameArray, AutowareExpNameArray, AutowareConfigArray, GoalConfigArray, SimulinkConfigArray, TagsConfigArray, SHHConfig)
+% 
+% cd '..\..\TA'
+% rosshutdown
+%   TA('TAOrder.csv')  
