142ffab634a0043445b8a2feead9a5e96da48e60
maurits2 <maurits2@kth.se>
2019-11-11 15:50:33 +0100

    New changes made to code

--- AD-EYE/OpenSCENARIO/Code/simulink_ego.m
+++ AD-EYE/OpenSCENARIO/Code/simulink_ego.m
@@ -1,6 +1,6 @@
 function simulink_ego(name_simulink,models, name_ego)
 
- for i = 1:length(models.worldmodel.object  ) %Declare number of objects in xml file %main for loop
+for i = 1:length(models.worldmodel.object  ) %Declare number of objects in xml file %main for loop
     
     if(convertCharsToStrings(models.worldmodel.object{i, 1}.name  ) == name_ego)
         location = strcat(name_simulink,convertCharsToStrings(models.worldmodel.object{i,1}.name) ,"/");
@@ -19,7 +19,7 @@ function simulink_ego(name_simulink,models, name_ego)
                 if(getSimulinkBlockHandle(b2(x,1)) ~= -1)
                     delete_block(b2{x,1})
                 end
-            end            
+            end
             %delete_line
             for p = 1:length(location1)
                 h = get_param(convertStringsToChars(strcat(location,location1(p,1))),'LineHandles');
@@ -73,9 +73,9 @@ function simulink_ego(name_simulink,models, name_ego)
             set_param(location3,'LinkStatus','inactive')
             add_line(convertStringsToChars(location), convertStringsToChars(strcat(Blockname3,"/1")),....
                 convertStringsToChars(strcat(Blockname4,"/1" )) ) %connect blocks
-        end        
+        end
         
-
+        
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Connecting lines
         
         %create variable with all the names of input port of the Main_block and Dynamic_Empty
@@ -87,15 +87,15 @@ function simulink_ego(name_simulink,models, name_ego)
         s2 = get_param(convertStringsToChars(strcat(location,Blockname3)),'PortConnectivity');
         s = s1;
         
-          x = length(h1);
-          for k = 1:length(h1)
-              h1(k,1) = strrep(h1(k,1), "//","/");
-          end
-          for j =1:length(q1)
-              x = x+1;
-              h1(x,1) = strrep(q1(j,1), "//","/");
-              s(x,1) = s2(j,1);
-          end
+        x = length(h1);
+        for k = 1:length(h1)
+            h1(k,1) = strrep(h1(k,1), "//","/");
+        end
+        for j =1:length(q1)
+            x = x+1;
+            h1(x,1) = strrep(q1(j,1), "//","/");
+            s(x,1) = s2(j,1);
+        end
         
         %Create another varable with all the names of input port of the Main_block for comparision
         %h2 is variable h1 stripped down
@@ -105,33 +105,36 @@ function simulink_ego(name_simulink,models, name_ego)
                 h2(k,1) = strrep(h2(k,1), strcat("/",int2str(p)),"");
                 h2(k,1) = strrep(h2(k,1), strcat("/",Blockname0),"");
                 h2(k,1) = strrep(h2(k,1), strcat("/",Blockname3),"");
-            end                         
-        end        
+            end
+        end
         
-
+        
         
         %create variable with all the names of input port of the Main_block
         for k = 1:length(h1)
             existing(k,1) = getSimulinkBlockHandle(convertStringsToChars(h2(k,1)));
             if(existing(k,1) ~=-1)
                 h1(k,1) =  strrep(h1(k,1), strcat(name_simulink,name_ego,"/",Blockname0,"/"),"");
-                 h1(k,1) =  strrep(h1(k,1), strcat(name_simulink,name_ego,"/",Blockname3,"/"),"");
+                h1(k,1) =  strrep(h1(k,1), strcat(name_simulink,name_ego,"/",Blockname3,"/"),"");
                 %see if Main_block is already connected
                 if(s(k,1).SrcBlock == -1 && k <= length(s1))
-                    add_line(location, h1(k,1),strcat(Blockname0,"/",int2str(k)))                
-                elseif(s(k,1).SrcBlock == -1 ) 
-                 add_line(location, h1(k,1),strcat(Blockname3,"/",int2str(k-length(s1))))                
+                    add_line(location, h1(k,1),strcat(Blockname0,"/",int2str(k)))
+                elseif(s(k,1).SrcBlock == -1 )
+                    add_line(location, h1(k,1),strcat(Blockname3,"/",int2str(k-length(s1))))
                 end
             end
-        end        
+        end
         
         %delete State variable in Dynamics_Empty
         Blockname0 = "Simulink_state_NEW";
         Blockname1 = "Dynamics_Empty";
         location1 = convertStringsToChars(strcat(location,Blockname1,"/STATE_",name_ego));
         if (getSimulinkBlockHandle(location1) ~= -1)
-            delete_block(location1)
-        end       
+            h = get_param(location1,'Linehandles');
+            delete_line(h.Outport(1));
+            delete_line(h.Inport(1));
+            delete_block(location1);
+        end
         
         Simulink.BlockDiagram.expandSubsystem(location0,'CreateArea','On')
     end %if statement checking which object in Prescan is the ego vehicle
