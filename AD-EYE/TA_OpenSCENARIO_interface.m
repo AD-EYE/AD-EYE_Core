e5b09369d66085625f05e30d9db5b21c57a978b3
maurits2 <maurits2@kth.se>
2019-11-12 11:32:15 +0100

    TA_interface new ego

--- AD-EYE/TA_OpenSCENARIO_interface.m
+++ AD-EYE/TA_OpenSCENARIO_interface.m
@@ -1,13 +1,14 @@
- name_model = "Map_pedestrian_autoware";
-name_ego = "Mazda_RX8_Coupe_1"; 
-name_experiment = "Map_pedestrian_autoware";
+name_model = "KTH_pedestrian_autoware";
+name_ego = "BMW_X5_SUV_1"; 
+name_experiment = "KTH_pedestrian_autoware";
 
-ExpNameArray = ["Map_pedestrian_autoware1" "Map_pedestrian_autoware2"];
-AutowareExpNameArray = ["Map_pedestrian_autoware" ];
+EgoNameArray = ["BMW_X5_SUV_1" "BMW_X5_SUV_1"];
+ExpNameArray = ["KTH_pedestrian_autoware1" "KTH_pedestrian_autoware2"];
+AutowareExpNameArray = ["KTH_pedestrian_autoware" "KTH_pedestrian_autoware" ];
 AutowareConfigArray = ["AutowareConfig.xlsx" ];
 GoalConfigArray = ["GoalConfig.xlsx" ];
 SimulinkConfigArray = ["SimulinkConfig.xlsx"];
-TagsConfigArray = ["TagsConfig" ];
+TagsConfigArray = [""];
 SHHConfig = "ssh";
 
 % ExpNameArray = ["e1" "e2"];
@@ -25,9 +26,8 @@ for i = 1:length(listOfNames)
     listOfNames(i)
 API_main(name_ego,name_experiment,listOfNames(i))
 end
-
-%TACombinations(ExpNameArray, AutowareExpNameArray, AutowareConfigArray, GoalConfigArray, SimulinkConfigArray, TagsConfigArray, SHHConfig)
-% 
-% cd '..\..\TA'
-% rosshutdown
-%   TA('TAOrder.csv')  
+%     %cd '..\..\TA'
+%     TACombinations(ExpNameArray, AutowareExpNameArray, EgoNameArray, AutowareConfigArray, GoalConfigArray, SimulinkConfigArray, TagsConfigArray, SHHConfig)
+%  
+%     rosshutdown
+%       TA('TAOrder.csv')  
