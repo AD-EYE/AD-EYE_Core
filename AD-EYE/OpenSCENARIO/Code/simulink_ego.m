8028d37bdecc0527085b00538e042308c6649033
maurits2 <maurits2@kth.se>
2019-11-12 11:31:08 +0100

    Ros library updated

--- AD-EYE/OpenSCENARIO/Code/simulink_ego.m
+++ AD-EYE/OpenSCENARIO/Code/simulink_ego.m
@@ -125,17 +125,17 @@ for i = 1:length(models.worldmodel.object  ) %Declare number of objects in xml f
             end
         end
         
-        %delete State variable in Dynamics_Empty
-        Blockname0 = "Simulink_state_NEW";
-        Blockname1 = "Dynamics_Empty";
-        location1 = convertStringsToChars(strcat(location,Blockname1,"/STATE_",name_ego));
-        if (getSimulinkBlockHandle(location1) ~= -1)
-            h = get_param(location1,'Linehandles');
-            delete_line(h.Outport(1));
-            delete_line(h.Inport(1));
-            delete_block(location1);
-        end
-        
+%         %delete State variable in Dynamics_Empty
+%         Blockname0 = "Simulink_state_NEW";
+%         Blockname1 = "Dynamics_Empty";
+%         location1 = convertStringsToChars(strcat(location,Blockname1,"/STATE_",name_ego));
+%         if (getSimulinkBlockHandle(location1) ~= -1)
+%             h = get_param(location1,'Linehandles');
+%             delete_line(h.Outport(1));
+%             delete_line(h.Inport(1));
+%             delete_block(location1);
+%         end
+%         
         Simulink.BlockDiagram.expandSubsystem(location0,'CreateArea','On')
     end %if statement checking which object in Prescan is the ego vehicle
 end %for loop over all Prescan objects
