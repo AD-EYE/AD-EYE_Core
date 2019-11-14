d668ae4ba5ed9848f7276e002993fe93a40bd097
Florent <florent2@kth.se>
2019-11-14 14:29:11 +0100

    Fix the critical area
    It uses a polygonIterator on the 'not-aligned-'GridMap

--- AD-EYE/ROS_Packages/src/AD-EYE/src/safetySupervisor.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/safetySupervisor.cpp
@@ -7,6 +7,11 @@
 #include <std_msgs/Int32.h>
 #include <autoware_msgs/Lane.h>
 
+#include <cpp_utils/pose_datatypes.h>
+
+#include <visualization_msgs/Marker.h> //Used for critical area visualization
+#include <std_msgs/ColorRGBA.h>        //Used for critical area visualization
+
 //using namespace grid_map;
 
 /*!
@@ -24,13 +29,17 @@ private:
     ros::Subscriber subGridmap;
     ros::Subscriber subAutowareTrajectory;
 
+    //ros::Publisher pubArea;  //Used for critical area visualization
+
     // constants
     bool SAFE = 0;
     bool UNSAFE = 1;
     //Critical area
     const float car_length = 5;
     const float car_width = 2;
-    const grid_map::Length critAreaSize; //(4 * car_length, 2 * car_width +1);
+    const float critArea_length = 2.5 * car_length; //Size of the critical Area
+    const float critArea_width = car_width * 1.6;
+    grid_map::Polygon critArea;
     const float pi = 3.141592654;
 
     // variables
@@ -50,15 +59,16 @@ public:
      * \param nh A reference to the ros::NodeHandle initialized in the main function.
      * \details Initialize the node and its components such as publishers and subscribers.
      */
-    SafetySupervisor(ros::NodeHandle &nh) : nh_(nh), critAreaSize(4 * car_length, 2 * car_width +1)
+    SafetySupervisor(ros::NodeHandle &nh) : nh_(nh)
     {
         // Initialize the node, publishers and subscribers
-
         pubSwitch = nh_.advertise<std_msgs::Int32>("/switchCommand", 1, true);
 
         subGnss = nh_.subscribe<geometry_msgs::PoseStamped>("/gnss_pose", 100, &SafetySupervisor::gnss_callback, this);
         subGridmap = nh_.subscribe<grid_map_msgs::GridMap>("/SafetyPlannerGridmap", 1, &SafetySupervisor::gridmap_callback, this);
         subAutowareTrajectory = nh_.subscribe<autoware_msgs::Lane>("/final_waypoints", 1, &SafetySupervisor::autowareTrajectory_callback, this);
+
+        //pubArea = nh_.advertise<visualization_msgs::Marker>("/critArea", 1, true);  //Used for critical area visualization
 
         // Initialize the variables
         state = SAFE;
@@ -161,15 +171,38 @@ public:
         //Is there a dynamic object in the critical area
         const float x = pose.position.x;    //Center is currently in the fron of the car
         const float y = pose.position.y;
-        const float a = pose.orientation.z;
-        const grid_map::Position critAreaCenter(x + car_length*cos(a*pi)/2, y + car_length*sin(a*pi)/2);
-        bool isOk;
-        grid_map::SubmapGeometry critArea(gridmap, critAreaCenter, critAreaSize, isOk);
-        if(!isOk) {
-            ROS_ERROR("Getting Submap from Gridmap failed");
-            return;
-        }
-        for(grid_map::SubmapIterator areaIt(critArea) ; !areaIt.isPastEnd() ; ++areaIt) {
+        const float a = cpp_utils::extract_yaw(pose.orientation);// * pi;
+        const grid_map::Position center(x + car_length * cos(a)/2, y + car_length * sin(a)/2);
+
+        critArea.removeVertices();
+        const grid_map::Position point1(center.x() + (cos(a) * critArea_length - sin(a) * critArea_width)/2,
+                                        center.y() + (sin(a) * critArea_length + cos(a) * critArea_width)/2);
+        const grid_map::Position point2(point1.x() + sin(a) * critArea_width, point1.y() - cos(a) * critArea_width);
+        const grid_map::Position point3(point2.x() - cos(a) * critArea_length, point2.y() - sin(a) * critArea_length);
+        const grid_map::Position point4(point1.x() - cos(a) * critArea_length, point1.y() - sin(a) * critArea_length);
+        /*critArea.addVertex(grid_map::Position(center.x() + (cos(a) * critArea_length - sin(a) * critArea_width)/2,
+                                        center.y() + (sin(a) * critArea_length + cos(a) * critArea_width)/2));
+        critArea.addVertex(grid_map::Position(critArea.getVertex(0).x() + sin(a) * critArea_width,
+                    critArea.getVertex(0).y() + cos(a) * critArea_width));
+        critArea.addVertex(grid_map::Position(critArea.getVertex(1).x() - cos(a) * critArea_length,
+                    critArea.getVertex(1).y() - sin(a) * critArea_length));
+        critArea.addVertex(grid_map::Position(critArea.getVertex(0).x() - cos(a) * critArea_length,
+                    critArea.getVertex(0).y() - sin(a) * critArea_length));*/
+        critArea.addVertex(point1);
+        critArea.addVertex(point2);
+        critArea.addVertex(point3);
+        critArea.addVertex(point4);
+
+        /*visualization_msgs::Marker ca_visu;  //Used for critical area visualization
+        std_msgs::ColorRGBA color;
+        color.r = 1.0;
+        color.a = 1.0;
+        grid_map::PolygonRosConverter::toLineMarker(critArea, color, 0.2, 0.5, ca_visu);
+        ca_visu.header.frame_id = gridmap.getFrameId();
+        ca_visu.header.stamp.fromNSec(gridmap.getTimestamp());
+        pubArea.publish(ca_visu);*/
+
+        for(grid_map::PolygonIterator areaIt(gridmap, critArea) ; !areaIt.isPastEnd() ; ++areaIt) {
             if(gridmap.at("DynamicObjects", *areaIt) > 0) { //If there is something inside the area
                 ROS_WARN_THROTTLE(1, "There is a dynamic object in the critical Area !");
                 state = UNSAFE;
