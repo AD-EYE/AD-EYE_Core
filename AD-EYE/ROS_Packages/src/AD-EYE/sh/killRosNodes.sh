170a1a52bca1303502d5ea69547ebe2077553b48
Florent <florent2@kth.se>
2020-01-16 15:31:31 +0100

    TA sh kill scripts : better chance to kill ros nodes

--- AD-EYE/ROS_Packages/src/AD-EYE/sh/killRosNodes.sh
+++ AD-EYE/ROS_Packages/src/AD-EYE/sh/killRosNodes.sh
@@ -1,5 +1,12 @@
 rosnode kill -a
 killall -9 rosmaster
+
+roslaunch_pid=`pgrep -x roslaunch`
+
+if [ -n "$roslaunch_pid" ]; then
+    pkill -P $roslaunch_pid
+fi
+
 sleep 5
 exit
 
--- AD-EYE/ROS_Packages/src/AD-EYE/sh/killall.sh
+++ AD-EYE/ROS_Packages/src/AD-EYE/sh/killall.sh
@@ -1,6 +1,12 @@
 rosnode kill -a>/dev/null 2>&- & disown
 killall -9 rosmaster>/dev/null 2>&- & disown
 
+roslaunch_pid=`pgrep -x roslaunch`
+
+if [ -n "$roslaunch_pid" ]; then
+    pkill -P $roslaunch_pid
+fi
+
 ps -aux | grep "ssh" 
 pkill -f "ssh" #>/dev/null 2>&- disown
 sleep 2
