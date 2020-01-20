87e4c7b864fe5a936400f2ebb47a47024fc106d2
Florent <florent2@kth.se>
2020-01-20 15:47:47 +0100

    TA: Improved the node killing mechanism previously introduced

--- AD-EYE/ROS_Packages/src/AD-EYE/sh/killRosNodes.sh
+++ AD-EYE/ROS_Packages/src/AD-EYE/sh/killRosNodes.sh
@@ -1,11 +1,11 @@
 rosnode kill -a
 killall -9 rosmaster
 
-roslaunch_pid=`pgrep -x roslaunch`
-
-if [ -n "$roslaunch_pid" ]; then
-    pkill -P $roslaunch_pid
-fi
+# Killing all processes launched by all roslaunch
+for pid in $(pgrep -x roslaunch)
+do
+    pkill -P $pid
+done
 
 sleep 5
 exit
--- AD-EYE/ROS_Packages/src/AD-EYE/sh/killall.sh
+++ AD-EYE/ROS_Packages/src/AD-EYE/sh/killall.sh
@@ -1,11 +1,11 @@
 rosnode kill -a>/dev/null 2>&- & disown
 killall -9 rosmaster>/dev/null 2>&- & disown
 
-roslaunch_pid=`pgrep -x roslaunch`
-
-if [ -n "$roslaunch_pid" ]; then
-    pkill -P $roslaunch_pid
-fi
+# Killing all processes launched by all roslaunch
+for pid in $(pgrep -x roslaunch)
+do
+    pkill -P $pid
+done
 
 ps -aux | grep "ssh" 
 pkill -f "ssh" #>/dev/null 2>&- disown
