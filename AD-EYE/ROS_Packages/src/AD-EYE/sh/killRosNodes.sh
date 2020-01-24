1bdab6c248a97a8f2fdf3cbdb6d1fe03b2e0bc8a
Florent <florent2@kth.se>
2020-01-24 15:06:30 +0100

    TA: Improved killing nodes scripts.
    Better chance to kill nodes.

--- AD-EYE/ROS_Packages/src/AD-EYE/sh/killRosNodes.sh
+++ AD-EYE/ROS_Packages/src/AD-EYE/sh/killRosNodes.sh
@@ -1,5 +1,11 @@
 rosnode kill -a
 killall -9 rosmaster
+
+# Killing all processes launched by all roslaunch
+for pid in $(pgrep -x roslaunch)
+do
+    pkill -P $pid
+done
+
 sleep 5
 exit
-
--- AD-EYE/ROS_Packages/src/AD-EYE/sh/killall.sh
+++ AD-EYE/ROS_Packages/src/AD-EYE/sh/killall.sh
@@ -1,7 +1,13 @@
 rosnode kill -a>/dev/null 2>&- & disown
 killall -9 rosmaster>/dev/null 2>&- & disown
 
-ps -aux | grep "ssh" 
+# Killing all processes launched by all roslaunch
+for pid in $(pgrep -x roslaunch)
+do
+    pkill -P $pid
+done
+
+ps -aux | grep "ssh"
 pkill -f "ssh" #>/dev/null 2>&- disown
 sleep 2
 exit
