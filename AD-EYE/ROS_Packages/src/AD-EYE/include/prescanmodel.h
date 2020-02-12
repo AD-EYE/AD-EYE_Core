f32ee9ec63a169b8eec9661c8452f9ffb24c75e6
jmgs <jmgs@kth.se>
2020-02-12 09:57:35 +0100

    Fix the error with the pex that happeded during the merging

--- AD-EYE/ROS_Packages/src/AD-EYE/include/prescanmodel.h
+++ AD-EYE/ROS_Packages/src/AD-EYE/include/prescanmodel.h
@@ -78,9 +78,10 @@ struct PrescanModel
                         type = SAFEAREA;
                         element = line.substr(line.find("Description=\"SAFE")+17, 3);
                         value = strtof(element.c_str(),0);
+
                         if(value < 1 || value > 256){
                             std::stringstream ss;
-                            ss << "Value of the SafeArea out of the range 1-256" << filePex;
+                            ss << "Value of the SafeArea out of the range 1-256. Value = " << value << filePex;
                             ROS_ERROR_STREAM(ss.str());
                             throw std::runtime_error(ss.str());
                         }
