9a5a44ffa50ee5e18ac4b35ce65adaf903991177
Maxime Sainte Catherine <maximesc@kth.se>
2020-08-10 15:30:34 +0200

    Added clustering and extraction of polygons

--- AD-EYE/ROS_Packages/src/AD-EYE/src/safetyChannelPerception.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/safetyChannelPerception.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 // #include <std_msgs/Float32MultiArray.h>
 #include <pcl_ros/point_cloud.h>
+#include <autoware_msgs/CloudClusterArray.h>
+#include <geometry_msgs/PolygonStamped.h>
+#include <jsk_recognition_msgs/PolygonArray.h>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
@@ -10,8 +13,10 @@ class SafetyChannelPerception {
         ros::NodeHandle &nh_;
         ros::Subscriber sub_Lidar_;
         ros::Publisher pub_PointCloud_;
+        ros::Publisher pub_Polygons_;
         ros::Rate rate_;
         PointCloud point_cloud_;
+        std::string polygon_frame_ = "SSMP_map";
 
         void Lidar_callback(const PointCloud::ConstPtr& msg) {
             printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
@@ -29,11 +34,25 @@ class SafetyChannelPerception {
             pub_PointCloud_.publish(point_cloud_);
         }
 
+        void ClusterCallback(const autoware_msgs::CloudClusterArray::ConstPtr& msg) {
+            jsk_recognition_msgs::PolygonArray poly_array;
+            for(auto cluster: msg->clusters)
+            {
+                poly_array.header.frame_id = cluster.convex_hull.header.frame_id;
+                poly_array.polygons.push_back(cluster.convex_hull);
+            }
+            printf ("publishing a polygon");
+            pub_Polygons_.publish(poly_array);
+        }
+
     public:
         SafetyChannelPerception(ros::NodeHandle& nh): nh_(nh), rate_(10)
         {
-            sub_Lidar_ = nh.subscribe<PointCloud>("/points_raw", 1, &SafetyChannelPerception::Lidar_callback, this);
+            // sub_Lidar_ = nh.subscribe<PointCloud>("/points_raw", 1, &SafetyChannelPerception::Lidar_callback, this);
+            sub_Lidar_ = nh.subscribe<autoware_msgs::CloudClusterArray>("detection/lidar_detector/cloud_clusters", 1, &SafetyChannelPerception::ClusterCallback, this);
+
             pub_PointCloud_ = nh.advertise<PointCloud>("/points_filtered", 1, true);
+            pub_Polygons_ = nh.advertise<jsk_recognition_msgs::PolygonArray>("detection/polygons", 1, true);
 
         }
 
