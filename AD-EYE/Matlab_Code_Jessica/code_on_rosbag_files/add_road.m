7f0af383823d2b8dd5591afebab6e47139e28900
BILLAT Jessica <billat@kth.se>
2020-08-18 17:41:41 +0200

    fixe issu with delta offset and connection for add_road; and correct issu on uniqueid for writeConnection

--- AD-EYE/Matlab_Code_Jessica/code_on_rosbag_files/add_road.m
+++ AD-EYE/Matlab_Code_Jessica/code_on_rosbag_files/add_road.m
@@ -1,16 +1,23 @@
-% add roads to pex file
-% create an experiment
-%prescan.api.experiment.Experiment;
+%% 
+% this scipt extract gps data from rosbag file name bus. After add bezier roads to
+% pex file of experiment present on the folder of the scipt
+% if there is no experiment, the script don't work
+% To run the script you just need to modify the name of the file at line 9
+%% 
 
 % extract gps data from file bus_signal
 points_data=GPS_data('20180810150607_bus_signals');
 % now we have a matrix with x,y and z coordinates
 
-%we will define point for add bezier road or flex road 
+%%
+%we will define point for add bezier road or flex road. here is bezier road
 data=suppr_duplicates(points_data);
 len=length(data);
 i=1;
+xpName = prescan.experiment.getDefaultFilename();
+nbBezier=detect_nb_road('BezierRoad',xpName);
 while i<(len-1)
+    % definition of third point
     x1=data(i,1);
     x2=data(i+1,1);
     x3=data(i+2,1);
@@ -18,83 +25,88 @@ while i<(len-1)
     y2=data(i+1,2);
     y3=data(i+2,2);
     z1=data(i,3);
-    z2=data(i+1,2);
+    z2=data(i+1,3);
     z3=data(i+2,3);
-
+    
+    % definition of position
     position.x=x1;
     position.y=y1;
     position.z=z1;
-% %     egal=points_egal_test(x1,y1,z1,x2,y2,z2,x3,y3,z3);
-%     if x1==x2 && y1==y2 && z1==z2
-%         i=i+1;
-%         disp('3 points equal');
-%     elseif x2==x3 && y2==y3 && z2==z3
-%         orient=orientation(x1,y1,x2,y2);
-%         delta=calculate_delta(x1,y1,z1,x2,y2,z2);
-%         heading=0;
-%         tension.entry=10;
-%         tension.exit=10;
-%         add_bezierRoad(position,orient,heading,delta,tension);
-%         i=i+2;
-%     else
-        orient=orientation(x1,y1,x2,y2);
-        delta=calculate_delta(x1,y1,z1,x2,y2,z2);
-        heading=relative_heading(x1,y1,x2,y2,x3,y3);
-        tension.entry=10;
-        tension.exit=10;
-        add_bezierRoad(position,orient,heading,delta,tension);
-        i=i+1;
-end
-% end
-if i==len-1
-    x1=points_data(i,1);
-    x2=points_data(i+1,1);
-    y1=points_data(i,2);
-    y2=points_data(i+1,2);
-    z1=points_data(i,3);
-    z2=points_data(i+1,2);
-%     if x1==x2 && y1==y2 && z1==z2
-%         i=i+1;
-%     else
-        orient=orientation(x1,y1,x2,y2);
-        delta=calculate_delta(x1,y1,z1,x2,y2,z2);
+    % definition of orientation
+    orient=orientation(x1,y1,x2,y2);
+    %definition of delta offset
+    delta=calculate_delta(x1,y1,z1,x2,y2,z2,orient);
+    %definition of relative heading
+    heading=pi-relative_heading(x1,y1,x2,y2,x3,y3);
+    % definition of tensions
+    tension.entry=10;
+    tension.exit=10;
+    % add bezier road
+    add_bezierRoad(position,orient,heading,delta,tension);
+    % add link between roads
+    if i>1
+        add_connection(strcat('CurvedRoad_',num2str(nbBezier)),strcat('CurvedRoad_',num2str(nbBezier+1)),1,0);
+    end
+    i=i+1;
+    nbBezier=nbBezier+1;
+    % definition for the last road
+    if i==(len-1)
+        position.x=x2;
+        position.y=y2;
+        position.z=z2;
+        orient=orientation(x2,y2,x3,y3);
+        delta=calculate_delta(x2,y2,z2,x3,y3,z3,orient);
         heading=0;
         tension.entry=10;
         tension.exit=10;
         add_bezierRoad(position,orient,heading,delta,tension);
-% 
+        % add connection to the last road
+        add_connection(strcat('CurvedRoad_',num2str(nbBezier)),strcat('CurvedRoad_',num2str(nbBezier+1)),1,0);
+    end
+%     
 end
-%end
-%or=orientation(points_data(1,1),points_data(1,2),points_data(2,1),points_data(2,2))
-%he=relative_heading(points_data(1,1),points_data(1,2),points_data(2,1),points_data(2,2))
 
+
+%%
+% function which calculate the distance between two point
 function dist=distance(x1,y1,x2,y2)
     x=(x2-x1)^2;
     y=(y2-y1)^2;
     dist=sqrt(x+y); 
 end
+
+%functio, which calculate the orientation of the road
 function orient=orientation(x1,y1,x2,y2)
-    dist=distance(x1,y1,x2,y2);
-     orient=asin( (y2-y1)/dist); %result in rad
+    a=distance(x1,y1,x2,y2);
+    b=distance(x1,y1,x2,y1);
+    if x2<x1
+        orient=pi-acos(b/a);
+    else
+        orient=acos(b/a); %result in rad
+    end
 end
 
-function delta=calculate_delta(x1,y1,z1,x3,y3,z3)
-    delta.x=x3-x1;
-    delta.y=y3-y1;
-    delta.z=z3-z1;
-end
+%function wich calculate delta offset of the end of the road
+function delta=calculate_delta(x1,y1,z1,x2,y2,z2,orien)
+    x2b=x2-x1;
+    y2b=y2-y1;
+    x2f=x2b*cos(orien)-y2b*sin(orien);
+    y2f=x2b*sin(orien)+y2b*cos(orien);
+    delta.x=abs(x2f);
+    delta.y=abs(y2f); 
+    delta.z=z2-z1;
+end 
 
+% function wich calculate the angle of the relative heading
 function heading=relative_heading(x1,y1,x2,y2,x3,y3)
-%     alpha=orientation(x1,y1,x2,y2);
-%     dist=distance(x2,y2,x3,y3);
-%     beta=asin( (x3-x2)/dist);
-%     heading=pi/2-beta-alpha;
     a=distance(x1,y1,x2,y2);
     b=distance(x2,y2,x3,y3);
-    c=distance(x1,y1,x3,y3);
+    c=distance(x1,y1,x3,y3); 
     d=(a^2+b^2-c^2)/(2*a*b);
     heading=acos(d);
 end
+
+%function wich permit to suppress duplicates points
 function data=suppr_duplicates(gps_data)
     len=length(gps_data);
     current=gps_data(1,:);
---
+++ AD-EYE/Matlab_Code_Jessica/code_on_rosbag_files/writeConnectionToPexFile.m
@@ -0,0 +1,58 @@
+%%
+%RoadA_Id is the id of the first road like 'CurvedRoad_1' and RoadB_Id is
+%the id of the second road. JointaId indicated if the connection is with
+%the beginning of the road (0) or the end of the road (1). If it is
+%Xcrossing or roundabout, JointId=0,1,2 ou 3. If it is Ycrossing,
+%JointId=0,1 ou 2. 
+%%
+
+function writeConnectionToPexFile(RoadA_Id,RoadB_Id,JointaId,JointbId,ExperimentPexFile,RoadPexFile)
+
+%load all files
+pexFileName=ExperimentPexFile;
+RoadTemplate=RoadPexFile;
+    
+%Make backup of the currnet PEX file
+backupFolderPath = [pwd '\BackupPEXFiles'];
+if ~exist(backupFolderPath)
+    mkdir(backupFolderPath);
+end
+
+currentTime = datestr(now,'yy-mm-dd_HH-MM-SS');
+copyfile([pwd '\' pexFileName], [backupFolderPath '\Backup_' currentTime '_' pexFileName]);
+
+
+%Convert PEX to STRUCT
+disp('Loading the experiment and template PEX files...') %message for the commande
+loadedPexFile = xml2struct(pexFileName); 
+loadedTemplate = xml2struct(RoadTemplate);
+
+indexConnection=length(loadedPexFile.Experiment.InfraStructure.RoadSegmentConnections.Connection);
+indexConnection= indexConnection+1;
+
+%get the connection template
+currentStruct=loadedTemplate.Experiment.InfraStructure.RoadSegmentConnections.Connection{1,1};
+
+%add the properties of connection
+currentStruct.Attributes.id = strcat('RoadJointConnection_',num2str(indexConnection));
+currentStruct.Attributes.Road_A_UniqueId = RoadA_Id;
+currentStruct.Attributes.Road_B_UniqueId = RoadB_Id;
+currentStruct.Attributes.Joint_A_Id= num2str(JointaId);
+currentStruct.Attributes.Joint_B_Id= num2str(JointbId);
+currentStruct.Attributes.UniqueId=num2str(indexConnection*5);
+
+loadedPexFile.Experiment.InfraStructure.RoadSegmentConnections.Connection{1,indexConnection}=currentStruct;
+
+ %Convert the populated STRUCT to PEX
+disp('Overwriting changes to PEX file...')
+struct2xml(loadedPexFile,pexFileName)
+
+
+
+%Overwriting the current PEX file with the populated one
+copyfile([pwd '\' pexFileName '.xml'], [pwd '\' pexFileName]);
+
+disp(['Done...A back up of original PEX file is made at: ' backupFolderPath '\Backup_' currentTime '_' pexFileName]);
+
+end
+
