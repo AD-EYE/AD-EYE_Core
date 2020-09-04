4732b63f09a541c9338057ccbbf649f08d6fe4ac
abhinav <abhinav2@kth.se>
2020-09-04 16:34:00 +0200

    experiment A node running

--- AD-EYE/ROS_Packages/src/AD-EYE/src/ExperimentA.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/ExperimentA.py
@@ -44,7 +44,8 @@ def storePosition (L):
     global Dy_car
     global speed
     Loc = [L[0]+Dx_car,L[1]+Dy_car,L[2]]
-    storeData(Loc,speed,Pedestrian) # we call the procedure only once (errors with line 48 if not)
+    if Store == True :
+        storeData(Loc,speed,Pedestrian) # we call the procedure only once (errors with line 48 if not)
 
 def storespeed(Sp):
     global speed
@@ -59,22 +60,78 @@ def storeData (Loc,speed,Pedestrian) :
     global CollSp
     global Coll
     global distance_Ego_P
+    global stop_pub
     if len(speed)>1 : # in order to have access to the previous speed
         spL = len(speed)-1
         if Loc[1]> -423.30 : # if the car did almost the half of its path (to not have tests if it is not necessary)
             d = np.sqrt( (Loc[1]-Pedestrian[1])**2 + (Loc[0]-Pedestrian[0])**2 )
             distance_Ego_P.append(d)
+            print(d)
 
-            if distance_Ego_P[len(distance_Ego_P)-2]<d : # if the distance is rising, so the pedestrian has been passed
-                if Coll == False : # if the collision didn't occurred
-                    file.write("The pedestrian wasn't on the road when the car passed him --> no collision, no stop \n")
-                    file.close()
-                    stop_pub = rospy.Publisher("/simulink/experiment_stopper",Bool, queue_size = 1) # stop_publisher
-                    stop_pub.publish(Bool(True)) # stop_publisher
-                else : #if the collision occurred
+            if len(distance_Ego_P)>2 : # necessary for line 73
+
+                if distance_Ego_P[len(distance_Ego_P)-3]<distance_Ego_P[len(distance_Ego_P)-2] : # if the distance is rising, so the pedestrian has been passed,
+                                                                                                 # we don't compare distance_Ego_P[len(distance_Ego_P)-2] with d
+                                                                                                 # because they are equal when the car stops
+                    if Coll == False : # if the collision didn't occurred
+
+                        # writes the parameters of the experiment
+                        MaxVel = rospy.get_param("adeye/motion_planning/op_common_params/maxVelocity")
+                        file.write("Set speed = "+str(MaxVel)+" , ")
+                        Rain = rospy.get_param("/simulink/rain_intensity")
+                        file.write("Set rain intensity = "+str(Rain)+" , ")
+                        Reflectivity = rospy.get_param("/simulink/reflectivity")
+                        file.write("Set reflectivity = "+str(Reflectivity)+" , ")
+                        Distance = rospy.get_param("/simulink/trigger_distance")
+                        file.write("Set trigger distance = "+str(Distance)+" , ")
+
+                        file.write("The pedestrian wasn't on the road when the car passed him --> no collision, no stop \n")
+                        file.close()
+
+                        stop_pub.publish(True) # stop_publisher
+                    else : #if the collision occurred
+                        if (speed[spL]==0.0) and (speed[spL-1]!=0.0): # if the car just stoped (to test it only once)
+                            Collision = 'Yes'
+                            StopDistance = - d
+
+                            # writes the parameters of the experiment
+                            MaxVel = rospy.get_param("adeye/motion_planning/op_common_params/maxVelocity")
+                            file.write("Set speed = "+str(MaxVel)+" , ")
+                            Rain = rospy.get_param("/simulink/rain_intensity")
+                            file.write("Set rain intensity = "+str(Rain)+" , ")
+                            Reflectivity = rospy.get_param("/simulink/reflectivity")
+                            file.write("Set reflectivity = "+str(Reflectivity)+" , ")
+                            Distance = rospy.get_param("/simulink/trigger_distance")
+                            file.write("Set trigger distance = "+str(Distance)+" , ")
+
+                            file.write("Collision? "+Collision) # then we write all the data needed in ExperimentA.csv
+                            file.write(', ')
+                            file.write("Coll speed = "+CollSp)
+                            file.write(', ')
+                            file.write("Stop dist = "+str(StopDistance))
+                            file.write('\n')
+                            file.close()
+
+                            stop_pub.publish(True) # stop_publisher
+
+                else :
+                    if d<2.5 : # if there is a collision
+                        if Coll == False : # we set the collision speed
+                            Coll = True
+                            CollSp = str(speed[spL-1])
                     if (speed[spL]==0.0) and (speed[spL-1]!=0.0): # if the car just stoped (to test it only once)
-                        Collision = 'Yes'
-                        StopDistance = - d
+                        Collision = 'No'
+                        StopDistance = d
+
+                        # writes the parameters of the experiment
+                        MaxVel = rospy.get_param("adeye/motion_planning/op_common_params/maxVelocity")
+                        file.write("Set speed = "+str(MaxVel)+" , ")
+                        Rain = rospy.get_param("/simulink/rain_intensity")
+                        file.write("Set rain intensity = "+str(Rain)+" , ")
+                        Reflectivity = rospy.get_param("/simulink/reflectivity")
+                        file.write("Set reflectivity = "+str(Reflectivity)+" , ")
+                        Distance = rospy.get_param("/simulink/trigger_distance")
+                        file.write("Set trigger distance = "+str(Distance)+" , ")
 
                         file.write("Collision? "+str(Collision)) # then we write all the data needed in ExperimentA.csv
                         file.write(', ')
@@ -83,27 +140,8 @@ def storeData (Loc,speed,Pedestrian) :
                         file.write("Stop dist = "+str(StopDistance))
                         file.write('\n')
                         file.close()
-                        stop_pub = rospy.Publisher("/simulink/experiment_stopper",Bool, queue_size = 1) # stop_publisher
-                        stop_pub.publish(Bool(True)) # stop_publisher
 
-            else :
-                if d<2.5 : # if there is a collision
-                    if Coll == False : # we set the collision speed
-                        Coll = True
-                        CollSp = str(speed[spL-1])
-                if (speed[spL]==0.0) and (speed[spL-1]!=0.0): # if the car just stoped (to test it only once)
-                    Collision = 'No'
-                    StopDistance = d
-
-                    file.write("Collision? "+str(Collision)) # then we write all the data needed in ExperimentA.csv
-                    file.write(', ')
-                    file.write("Coll speed = "+CollSp)
-                    file.write(', ')
-                    file.write("Stop dist = "+str(StopDistance))
-                    file.write('\n')
-                    file.close()
-                    stop_pub = rospy.Publisher("/simulink/experiment_stopper",Bool, queue_size = 1) # stop_publisher
-                    stop_pub.publish(Bool(True)) # stop_publisher
+                        stop_pub.publish(True) # stop_publisher
 
 # we declare the global values
 Loc = []
@@ -116,22 +154,16 @@ Dx_car = - 2.461*np.cos(angle_car)
 Dy_car = 2.461*np.sin(angle_car)
 distance_Ego_P = []
 
-# writes the parameters of the experiment
-if Store == True :
-    MaxVel = rospy.get_param("adeye/motion_planning/op_common_params/maxVelocity")
-    file.write("Set speed = "+str(MaxVel)+" , ")
-    Rain = rospy.get_param("/simulink/rain_intensity")
-    file.write("Set rain intensity = "+str(Rain)+" , ")
-    Reflectivity = rospy.get_param("/simulink/reflectivity")
-    file.write("Set reflectivity = "+str(Reflectivity)+" , ")
-    Distance = rospy.get_param("/simulink/trigger_distance")
-    file.write("Set trigger distance = "+str(Distance)+" , ")
 
-# listens to the topics
+
+#listens to the topics
 if __name__ == '__main__':
+
     rospy.init_node('ExperimentA',anonymous = True)
 
     rospy.Subscriber("/gnss_pose", PoseStamped, Position)
     rospy.Subscriber("/current_velocity", TwistStamped, speedf)
     rospy.Subscriber("/simulink/pedestrian_pose", Point, pedestrianf)
+
+    stop_pub = rospy.Publisher("/simulink/stop_experiment",Bool, queue_size = 1) # stop_publisher
     rospy.spin()
