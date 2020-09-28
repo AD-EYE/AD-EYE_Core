57e93b616d23fe7240877ebe7f0eccfb11aea006
Maxime Sainte Catherine <maximesc@kth.se>
2020-09-28 16:11:33 +0200

    Removed unecessary lines from managerFileLaunch.sh

--- AD-EYE/ROS_Packages/src/AD-EYE/sh/managerFileLaunch.sh
+++ AD-EYE/ROS_Packages/src/AD-EYE/sh/managerFileLaunch.sh
@@ -3,9 +3,5 @@
 export DISPLAY=:0
 source $HOME/AD-EYE_Core/AD-EYE/ROS_Packages/src/AD-EYE/sh/tempbash
 roslaunch adeye manager_simulation.launch test_automation:=true>/dev/null 2>&- & disown
-sleep 1
-roslaunch adeye ExperimentA.launch>/dev/null 2>&- & disown
-roslaunch adeye ExperimentB.launch>/dev/null 2>&- & disown
-sleep 5
 echo "error" > file=sys.stderr
 exit
