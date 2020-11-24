efc0ddf0b4e6b0c1264a2b470f2a04b9a4ebc42d
Maxime Sainte Catherine <maximesc@kth.se>
2020-11-24 17:43:14 +0100

    Added the option to have only twist from autoware or to have also steer, brake, accel and gear

--- AD-EYE/ROS_Packages/src/AD-EYE/src/sender.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/sender.py
@@ -5,14 +5,17 @@ import math as m
 from autoware_msgs.msg import VehicleCmd
 from geometry_msgs.msg import TwistStamped
 from std_msgs.msg import UInt32
+import sys
 
 class Sender:
 
-    def __init__(self):
-        self.steer_pub = rospy.Publisher('steer', UInt32, queue_size=1)
-        self.brake_pub = rospy.Publisher('brake', UInt32, queue_size=1)
-        self.accel_pub = rospy.Publisher('accel', UInt32, queue_size=1)
-        self.gear_pub = rospy.Publisher('gear', UInt32, queue_size=1)
+    def __init__(self, use_only_twist):
+        self.use_only_twist = use_only_twist
+        if not self.use_only_twist:
+            self.steer_pub = rospy.Publisher('steer', UInt32, queue_size=1)
+            self.brake_pub = rospy.Publisher('brake', UInt32, queue_size=1)
+            self.accel_pub = rospy.Publisher('accel', UInt32, queue_size=1)
+            self.gear_pub = rospy.Publisher('gear', UInt32, queue_size=1)
         self.twist_pub = rospy.Publisher('autowareTwist', TwistStamped, queue_size=1)  # autowareTwist - TwistS
         rospy.Subscriber("vehicle_cmd", VehicleCmd, self.autowareCmdCallback)
 
@@ -27,14 +30,21 @@ class Sender:
         accel = data.accel_cmd.accel
         gear = data.gear
         Twist = data.twist_cmd
-        self.steer_pub.publish(steer)
-        self.brake_pub.publish(brake)
-        self.accel_pub.publish(accel)
-        self.gear_pub.publish(gear)
+        if not self.use_only_twist:
+            self.steer_pub.publish(steer)
+            self.brake_pub.publish(brake)
+            self.accel_pub.publish(accel)
+            self.gear_pub.publish(gear)
         self.twist_pub.publish(Twist)
 
 
 if __name__ == '__main__':
     rospy.init_node('sender', anonymous=True)
-    Sender()
+    if len(sys.argv) < 3:
+        Sender(True)
+    else:
+        if sys.argv[1]=="false":
+            Sender(False)
+        else:
+            Sender(True)
     rospy.spin()
