6310a731fb134b56c166526827b2cf84672ff34b
Sonakhi <sonakhi.kth.se>
2020-12-16 21:14:35 +0100

    Modified header file

--- AD-EYE/ROS_Packages/src/AD-EYE/include/prescanmodel.h
+++ AD-EYE/ROS_Packages/src/AD-EYE/include/prescanmodel.h
@@ -11,200 +11,173 @@
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/locale.hpp>
 #include <boost/locale/generator.hpp>
+#include <boost/foreach.hpp>
+#include <boost/optional/optional.hpp>
 
-#define NATURE 0
-#define BUILDING 1
-#define TRAFFICLIGHT 2
-#define SAFEAREA 3
-
-/*!
- * \brief This structure handles data from the Prescan Experiment.
- * \details It loads the data from .pex files
- */
-
-struct PrescanObject
-{
-    int ID;
-    double posX;
-    double posY;
-    double yaw;
-    double sizeX;
-    double sizeY;
-    double sizeZ;
-    double safetyAreaValue;
-};
+#define NATURE 13
+#define BUILDING 12
+#define TRAFFICLIGHT 15
+#define SAFEAREA 17
+#define STATICCAR 1
+#define ATTR_SET ".<xmlattr>"
+#define DECIMAL_BASE 10
+#define SAFE_STRING_POS 4
 
 struct PrescanModel
 {
-    std::vector<PrescanObject> buildingObjects;
-    std::vector<PrescanObject> natureObjects;
-    std::vector<PrescanObject> trafficlightObjects;
-    std::vector<PrescanObject> safeAreaObjects;
-    std::vector<PrescanObject> carObjects;
+    /*!
+    * \brief This structure handles data from the Prescan Experiment.
+    * \details It loads the data from .pex files
+    */
+    struct PrescanObject
+    {
+        int ID;
+        double posX;
+        double posY;
+        double yaw;
+        double sizeX;
+        double sizeY;
+        double sizeZ;
+        int safetyAreaValue;
+    };
 
-    bool ULElementFound = false;
+	// Containers to save the objects based on object ID
+	std::vector<PrescanObject> safeAreaObjects;
+	std::vector<PrescanObject> natureObjects;
+	std::vector<PrescanObject> buildingObjects;
+	std::vector<PrescanObject> trafficlightObjects;
+	std::vector<PrescanObject> staticCarsObjects;
 
+	
+    /*!
+     * \brief Loads the data from the xml into the respective vectors
+     * \param local_ptree 	The child node from the xml file to be inserted into the vector
+	 * \param u_id 			The unique id of the Actor
+	 * \param type_id 		The object id of the Actor
+	 * \param safe_value 	The safe area value of the actor
+     */
+	void load_actor(boost::property_tree::ptree::value_type& local_ptree, int u_id, int type_id, int safe_value)
+	{		
+		// Populating the containers with data from the xml
+		PrescanObject Obj;
+		Obj.ID = u_id;
+		Obj.posX = local_ptree.second.get<double>("Location.<xmlattr>.X");
+		Obj.posY = local_ptree.second.get<double>("Location.<xmlattr>.Y");
+		Obj.yaw = local_ptree.second.get<double>("Orientation.<xmlattr>.Heading");
+		Obj.sizeX = local_ptree.second.get<double>("Size.<xmlattr>.X");
+		Obj.sizeY = local_ptree.second.get<double>("Size.<xmlattr>.Y");
+		Obj.sizeZ = local_ptree.second.get<double>("Size.<xmlattr>.Z");
+		Obj.safetyAreaValue = safe_value;
+		
+		// Inserting data into the respective containers 
+		if(type_id == BUILDING)
+		{
+			buildingObjects.push_back(Obj);
+		}
+		if(type_id == NATURE)
+		{
+			natureObjects.push_back(Obj);
+		}
+		if(type_id == TRAFFICLIGHT)
+		{
+			trafficlightObjects.push_back(Obj);
+		}
+		if(type_id == SAFEAREA)
+		{
+			safeAreaObjects.push_back(Obj);
+		}
+		if(type_id == STATICCAR)
+		{
+			staticCarsObjects.push_back(Obj);
+		}
+	}
+	
     /*!
      * \brief Loads the data of the Prescan Experiment from the given file
      * \param filePex The path to the .pex file
      */
-    void load_pexmap(std::string filePex) {
-        using boost::property_tree::ptree;
+	void load_pexmap(std::string filePex)
+	{
+		using boost::property_tree::ptree;
+		using boost::optional;
 
-        // Populate tree structure (pt):
+		ptree pt;
+		read_xml(filePex, pt);
 
-        ptree pt;
-        read_xml(filePex, pt);
+		ptree pt_Experiment = pt.get_child("Experiment");
 
-        // Traverse pt:
+		int safetyAreaValue = 0;
+		int u_id = 0;
+		int obj_id = 0;
+		
+		// looping through the entire xml
+		BOOST_FOREACH(ptree::value_type& val_Experiment, pt.get_child("Experiment"))
+		{
+			// checking if the child node is "Actors" in "Experiment"
+			if (val_Experiment.first == "Actors")
+			{
+				// tree with all the child nodes inside "Actors"
+				ptree pt_Actors = pt_Experiment.get_child("Actors");
+				// looping through all the child nodes inside "Actors"
+				BOOST_FOREACH(ptree::value_type& val_Actors, pt_Actors)
+				{
+					// tree with all the attributes of each "Actor"
+					ptree pt_Actor_Attr = val_Actors.second.get_child("<xmlattr>");
+					// tree whose all the child nodes of Trajectories
+					ptree pt_tra = val_Actors.second.get_child("Trajectories");
+					// looping through the attributes of "Actor"
+					BOOST_FOREACH(ptree::value_type& val_Actor_Attr, pt_Actor_Attr)
+					{
+						//std::cout << "Values: " << val_Actor_Attr.first.data() << ":" <<val_Actor_Attr.second.data()<< std::endl;
+						// populating the u_id with the data of the attribute name "UniqueId"
+						if(val_Actor_Attr.first == "UniqueId")
+						{
+							u_id = strtol(val_Actor_Attr.second.data().c_str(),NULL,DECIMAL_BASE);
+						}
+						// populating the obj_id with the data of the attribute name "ObjectTypeID"
+						if(val_Actor_Attr.first == "ObjectTypeID")
+						{
+							obj_id = strtol(val_Actor_Attr.second.data().c_str(),NULL,DECIMAL_BASE);
+						}
+						//std::cout << "After Values: " << val_Actor_Attr.first.data() << ":" <<u_id<<":" <<obj_id<< std::endl;
+					}
+					
+					// Checking if the "Trajectory" node has a child named "TrajectoryBase"
+					optional< ptree& > child = pt_tra.get_child_optional("TrajectoryBase");
+					if( !child )
+					{
+					  //std::cout << "Trajectory not found" << u_id <<std::endl;
+					  load_actor(val_Actors, u_id, obj_id, safetyAreaValue);
+					}
+					else
+						std::cout << "Dynamic Actor" << u_id <<std::endl;				
+				}
+			}
 
-        //AppList List;
-        PrescanObject Obj;
-        BOOST_FOREACH(ptree::value_type const& v, pt.get_child("Actors"))
-        {
-            if (v.first == "Actor")
-            {                
-                Obj.ID = v.first.get<int>("Actor.UniqueId");
-                Obj.posX = v.second.get<double>("Location.X");
-                Obj.posY = v.second.get<double>("Location.Y");
-                Obj.yaw = v.second.get<double>("Orientation.Heading");
-                Obj.sizeX = v.second.get<double>("Size.X");
-                Obj.sizeY = v.second.get<double>("Size.Y");
-                Obj.sizeZ = v.second.get<double>("Size.Z");
-                
-                              
-               // List.push_back(App);
-            }
-        }
-        BOOST_FOREACH(ptree::value_type const& v, pt.get_child("Environment"))
-        {
-            if (v.first == "Underlays")
-            {
-                if(v.second.get<std::string>("Underlay.Description").size()>=4 && v.second.get<std::string>("Underlay.Description").substr(0,4))
-                {
-                Obj.safetyAreaValue = v.second.get<std::string>("Underlay.Description");             
-                }
-            }
-         }
-         
-         std::cout<<
-
-        // parsing variables
-/*        std::string line;
-        std::string element;
-        float value;
-
-        PrescanObject object;
-        int type;
-
-        // open file pex and save important data
-        std::ifstream inputFile;
-        inputFile.open (filePex);
-        if (!inputFile.is_open()){
-            std::stringstream ss;
-            ss << "Couldn't open file" << filePex;
-            ROS_ERROR_STREAM(ss.str());
-            throw std::runtime_error(ss.str());
-        }
-        else{
-            while (inputFile.good()){
-                std::getline (inputFile,line);
-                if(line.find("ObjectTypeID=\"9\"") != std::string::npos) {
-                    ULElementFound = true;
-                }
-                if(line.find("<NatureElement xsi") != std::string::npos ||
-                        line.find(" ObjectTypeID=\"12\"") != std::string::npos ||
-                        line.find(" ObjectTypeID=\"17\"") != std::string::npos || // Concrete areas can be declared as safe area
-                        line.find("<InfraOther xsi") != std::string::npos ||
-                        line.find("<Actor ") != std::string::npos ) {
-
-                    // get type
-                    if(line.find("Description=\"SAFE") != std::string::npos) {
-                        type = SAFEAREA;
-                        element = line.substr(line.find("Description=\"SAFE")+17, 3);
-                        value = strtof(element.c_str(),0);
-
-                        if(value < 1 || value > 256){
-                            std::stringstream ss;
-                            ss << "Value of the SafeArea out of the range 1-256. Value = " << value << filePex;
-                            ROS_ERROR_STREAM(ss.str());
-                            throw std::runtime_error(ss.str());
-                        }
-                        object.safetyAreaValue = value;
-                        //std::cout << element << ";" << value << std::endl;
-                    }
-                    else if(line.find("Nature") != std::string::npos){
-                        type = NATURE;
-                    }
-                    else if(line.find(" ObjectTypeID=\"12\"") != std::string::npos){
-                        type = BUILDING;
-                    }
-                    else if(line.find("xsi:type=\"RectangularBox") != std::string::npos) {
-                        type = BUILDING;
-                    }
-                    else if(line.find("TrafficLightRoadSideNL") != std::string::npos){
-                        type = TRAFFICLIGHT;
-                    } else {
-                        continue;
-                    }
-
-                    // Get ID
-                    element = line.substr(line.find("UniqueId")+10, line.find("Sensor")-2-line.find("UniqueId")+10);
-                    value = strtof(element.c_str(),0);
-                    object.ID = value;
-
-                    // Get Position
-                    std::getline (inputFile,line);
-                    element = line.substr(line.find("X=")+3, line.find("Y=")-2-line.find("X=")+3);
-                    value = strtof(element.c_str(),0);
-                    object.posX = value;
-                    element = line.substr(line.find("Y=")+3, line.find("Z=")-2-line.find("Y=")+3);
-                    value = strtof(element.c_str(),0);
-                    object.posY = value;
-
-                    // Get Orientation
-                    std::getline (inputFile,line);
-                    element = line.substr(line.find("Heading=")+9, line.find("/>")-2-line.find("Heading=")+9);
-                    value = strtof(element.c_str(),0);
-                    object.yaw = value;
-
-                    //Get Size
-                    std::getline (inputFile,line);
-                    std::getline (inputFile,line);
-                    element = line.substr(line.find("X=")+3, line.find("Y=")-2-line.find("X=")+3);
-                    value = strtof(element.c_str(),0);
-                    object.sizeX = value;
-                    element = line.substr(line.find("Y=")+3, line.find("Z=")-2-line.find("Y=")+3);
-                    value = strtof(element.c_str(),0);
-                    object.sizeY = value;
-                    element = line.substr(line.find("Z=")+3, line.find("/>")-2-line.find("Z=")+3);
-                    value = strtof(element.c_str(),0);
-                    object.sizeZ = value;
-
-                    // Save the object
-                    if(type == SAFEAREA) {
-                        this->safeAreaObjects.push_back(object);
-                    }
-                    else if(type == NATURE){
-                        this->natureObjects.push_back(object);
-                    }
-                    else if(type == BUILDING){
-                        this->buildingObjects.push_back(object);
-                    }
-                    else if(type == TRAFFICLIGHT){
-                        this->trafficlightObjects.push_back(object);
-                    } else {
-                        continue;
-                    }
-
-
-                }
-            }
-        }
-        inputFile.close();*/
-    }
-
+			// checking if the child node is "Environment" in "Experiment"
+			if (val_Experiment.first == "Environment")
+			{
+				// looping through all the child nodes inside "Environment"
+				BOOST_FOREACH(ptree::value_type& val_Environment, pt_Experiment.get_child("Environment"))
+				{
+					// checking if the child node is "Underlays" in "Environment"
+					if (val_Environment.first == "Underlays")
+					{
+						// looping through all the child nodes inside "Underlays"
+						BOOST_FOREACH(ptree::value_type& val_Underlays, val_Experiment.second.get_child("Underlays"))
+						{
+							// checking the codition if the attribute "Description" has the value "SAFE" in it
+							if(val_Environment.second.get<std::string>("Underlay.<xmlattr>.Description").size()>=SAFE_STRING_POS && val_Environment.second.get<std::string>("Underlay.<xmlattr>.Description").substr(0,SAFE_STRING_POS) == "SAFE")
+							{
+								safetyAreaValue = strtol(val_Environment.second.get<std::string>("Underlay.<xmlattr>.Description").substr(SAFE_STRING_POS).c_str(),NULL,DECIMAL_BASE);          
+							}
+							load_actor(val_Underlays, val_Environment.second.get<int>("Underlay.<xmlattr>.UniqueId"), val_Environment.second.get<int>("Underlay.<xmlattr>.ObjectTypeID"), safetyAreaValue);
+						}
+					}
+				}
+			}
+		}
+	}
 };
-
 
 #endif
