566b3ba6297f5d937e399f84bb704288e0c8d443
adeye <adeye@adeye02.kth.se>
2021-01-27 15:02:01 +0100

    Reworked mapping script

--- Helper_Scripts/Mapping/mapping_script.py
+++ Helper_Scripts/Mapping/mapping_script.py
@@ -7,9 +7,11 @@ from autoware_config_msgs.msg import ConfigNDTMapping
 from autoware_config_msgs.msg import ConfigApproximateNDTMapping
 import subprocess
 from sensor_msgs.msg import Imu, PointCloud2
+from geometry_msgs.msg import PoseStamped
 import argparse
 from datetime import datetime
 import os
+import glob
 import roslaunch
 import time
 
@@ -22,6 +24,7 @@ def create_parser():
     parser = argparse.ArgumentParser(description='Description goes here')
     parser.add_argument("--rosbag-folder", type=str, default="/home/adeye/Downloads/KTH_map/Rosbags/Splitted_bags/", help="Path to rosbag files")
     parser.add_argument("--output-dir", type=str, default="/home/adeye/Downloads/KTH_map/Rosbags/Generated_pcd", help="Path to PCD output dir")
+    parser.add_argument("--rviz", type=str, default="n", help="Open Rviz? (y/N)")
     my_args = parser.parse_args()
     return my_args
 
@@ -33,9 +36,9 @@ def generate_PCD_path(args):
     return output_path
     
 def force_kill_ros():
-    subprocess.Popen(["rosnode", "kill", "-a"])
-    time.sleep(10)
-    subprocess.Popen(["killall", "-9", "rosmaster"])
+    subprocess.call(["rosnode", "kill", "-a"])
+    time.sleep(1)
+    subprocess.call(["killall", "-9", "rosmaster"])
 
 
 class ProcessManager(): 
@@ -49,6 +52,10 @@ class ProcessManager():
         self.args = args
         self.FNULL = open(os.devnull, 'w')
         self.output_filename =  generate_PCD_path(args)
+        if args.rviz == "Y" or args.rviz == "y":
+            self.start_rviz = True
+        else:
+            self.start_rviz = False
 
 
     def transform_imu(self):
@@ -73,28 +80,30 @@ class ProcessManager():
         rospy.set_param('tf_pitch', pitch)
         rospy.set_param('tf_yaw', yaw)
         cmd = ['roslaunch', 'runtime_manager', 'setup_tf.launch', 'x:=0.0', 'y:=0.0', 'z:=0.0', str(yaw), str(pitch), str(roll), 'frame_id:=/base_link', 'child_frame_id:=/velodyne', 'period_in_ms:=10']
-        subprocess.Popen(cmd, stdout=self.FNULL, stderr=self.FNULL) #TODO: Output stderr
+        self.tf_process = subprocess.Popen(cmd, stdout=self.FNULL, stderr=self.FNULL) #TODO: Output stderr
 
 
     def play_rosbag(self):
         """Play rosbag"""
         print("Playing rosbag")
         cmd = ["rosrun", "topic_tools", "relay", "/velodyne_points", "/points_raw"]
-        subprocess.Popen (cmd)
+        self.relay_process = subprocess.Popen (cmd)
         rosbag_path = self.args.rosbag_folder
         rospy.set_param('use_sim_time', 'true')
         cmd = ["rosbag", "play", str(rosbag_path)]
-        subprocess.Popen(cmd, stdout=self.FNULL, stderr=self.FNULL)
+        self.rosbag_process = subprocess.Popen(cmd, stdout=self.FNULL, stderr=self.FNULL)
+
 
     def start_ndt_mapping(self):
         print("Starting ndt-mapping")
 
-        settings = "{header: {seq: 0, stamp: {secs: 0, nsecs: 0}, frame_id: ''}, resolution: 0.5, step_size: 0.1, trans_epsilon: 0.01, max_iterations: 50, leaf_size: 0.1, min_scan_range: 0.2, max_scan_range: 20.0, min_add_scan_shift: 2.0}"
+        print("If the rosbag is big, it is recomended to edit ndt_mapping code to disable the publishing of the map. This will allow to generate maps that are bigger than 1GB.")
 
-        # settings = "{header: {seq: 0, stamp: {secs: 0, nsecs: 0}, frame_id: ''}, resolution: 1.0, step_size: 0.1, trans_epsilon: 0.01, max_iterations: 50, leaf_size: 2.0, min_scan_range: 1.5, max_scan_range: 75.0, min_add_scan_shift: 2.0}"
+
+        settings = "{header: {seq: 0, stamp: {secs: 0, nsecs: 0}, frame_id: ''}, resolution: 1.0, step_size: 0.1, trans_epsilon: 0.01, max_iterations: 50, leaf_size: 2.0, min_scan_range: 1.5, max_scan_range: 75.0, min_add_scan_shift: 2.0}"
 
         cmd = ["rostopic", "pub", "/config/ndt_mapping", "autoware_config_msgs/ConfigNDTMapping", settings]
-        subprocess.Popen(cmd)
+        self.topic_pub_process = subprocess.Popen(cmd)
 
         rospy.set_param("/ndt_mapping/imu_topic", "/imu/data")
         rospy.set_param("/ndt_mapping/imu_upside_down", True)
@@ -105,36 +114,13 @@ class ProcessManager():
 
         quer = roslaunch.core.Node('lidar_localizer', 'queue_counter')
         self.launch.launch(quer)
-        ndt = roslaunch.core.Node('lidar_localizer', 'ndt_mapping')
-        self.launch.launch(ndt)
-
-    def start_approximate_ndt_mapping(self):
-        print("Starting approximate-ndt-mapping")
-
-        settings = "{header: {seq: 0, stamp: {secs: 0, nsecs: 0}, frame_id: ''}, resolution: 0.5, step_size: 0.1, trans_epsilon: 0.01, max_iterations: 50, leaf_size: 0.1, min_scan_range: 0.2, max_scan_range: 20.0, min_add_scan_shift: 2.0}"
-        # settings = "{header: {seq: 0, stamp: {secs: 0, nsecs: 0}, frame_id: ''}, resolution: 1.0, step_size: 0.1, trans_epsilon: 0.01, max_iterations: 50, leaf_size: 2.0, min_scan_range: 1.5, max_scan_range: 75.0, min_add_scan_shift: 2.0}"
-
-        cmd = ["rostopic", "pub", "/config/approximate_ndt_mapping", "autoware_config_msgs/ConfigApproximateNDTMapping", settings]
-        subprocess.Popen(cmd)
-
-        rospy.set_param("/approximate_ndt_mapping/imu_topic", "/imu/data")
-        rospy.set_param("/approximate_ndt_mapping/imu_upside_down", True)
-        rospy.set_param("/approximate_ndt_mapping/incremental_voxel_update", True)
-        rospy.set_param("/approximate_ndt_mapping/method_type", False)
-        rospy.set_param("/approximate_ndt_mapping/use_imu", True)
-        rospy.set_param("/approximate_ndt_mapping/use_odom", False)
-
-        
-        quer = roslaunch.core.Node('lidar_localizer', 'queue_counter')
-        self.launch.launch(quer)
-        ndt = roslaunch.core.Node('lidar_localizer', 'approximate_ndt_mapping')
-        self.launch.launch(ndt)
+        print("starting ndt in a separate terminal")
+        self.ndt_mapping_process = subprocess.Popen(['xterm', '-e', 'rosrun lidar_localizer ndt_mapping'])
 
 
     def run_rviz(self): 
         """Set all nodes that should be launched"""
-        subprocess.Popen(["rosrun", "rviz", "rviz", "-d" , "/home/adeye/AD-EYE_Core/Helper_Scripts/Mapping/mapping_config.rviz"])
-
+        self.rviz_process = subprocess.Popen(["rosrun", "rviz", "rviz", "-d" , "/home/adeye/AD-EYE_Core/Helper_Scripts/Mapping/mapping_config.rviz"])
 
 
     def start(self): 
@@ -150,21 +136,38 @@ class ProcessManager():
 
         self.start_ndt_mapping()
 
-        # self.run_rviz()
+        if self.start_rviz:
+            self.run_rviz()
 
         # the mapping process is finished when no new message is published on the topic /ndt_map
         try: 
             while True: # while we receive message mapping is not done
                 self.launch.spin_once()
-                rospy.wait_for_message("/ndt_map", PointCloud2, timeout=500) # if this function tims out, it throws an exception
+
+                rospy.wait_for_message("/current_pose", PoseStamped, timeout=500) # if this function tims out, it throws an exception
                 print("Waiting for the mapping to be finished")
         except: # when no message has been published, the exception leads here
             print("Saving the map as a pcd file")
-            # subprocess.Popen(["rostopic", "pub", "-1" ,"/config/approximate_ndt_mapping_output", "autoware_config_msgs/ConfigApproximateNDTMappingOutput", output])
-            subprocess.Popen(["rostopic", "pub", "-1" ,"/config/ndt_mapping_output", "autoware_config_msgs/ConfigNDTMappingOutput", output])
-            time.sleep(10)
+            topic_output_pub_process = subprocess.Popen(["rostopic", "pub", "-1" ,"/config/ndt_mapping_output", "autoware_config_msgs/ConfigNDTMappingOutput", output])
+            time.sleep(5)
+            topic_output_pub_process.terminate()
         finally: 
             self.launch.stop()
+            # deleting unnecessary files generated by ndt_mapping
+            files_to_delete = glob.glob('ndt_mapping*.csv')
+            for file in files_to_delete:
+                try:
+                    os.remove(file)
+                except:
+                    print("Error while deleting file : ", filePath)
+            self.tf_process.terminate()
+            self.relay_process.terminate()
+            self.rosbag_process.terminate()
+            self.ndt_mapping_process.terminate()
+            self.topic_pub_process.terminate()
+            if self.start_rviz:
+                self.rviz_process.terminate()
+	    
 
 
 class RoscoreWrapper(): 
@@ -172,14 +175,15 @@ class RoscoreWrapper():
     Singleton implementation prevents from creating more than one instance."""
 
     __initialized = False
+
     def __init__(self):
         if RoscoreWrapper.__initialized:
             raise Exception("You can't create more than 1 instance of Roscore.")
         RoscoreWrapper.__initialized = True
+
     def start(self):
         try:
             self.roscore_process = subprocess.Popen(['roscore'])
-            #self.roscore_pid = self.roscore_process.pid  # pid of the roscore process (which has child processes)
         except OSError as e:
             sys.stderr.write('roscore could not be run')
             raise e
@@ -217,7 +221,6 @@ if __name__ == "__main__":
         if not os.path.isfile(args.rosbag_folder): 
             raise OSError(2, 'No such file or directory', args.rosbag_folder)
         
-        # subprocess.Popen(['roscore'])
         roscore_wrapper.start()
         process_manager = ProcessManager(args) # this process manager will start the different ROS nodes that are needed for the mapping
         process_manager.start()
