c64e080ae6d5364b312e23e09149475f7943c9bd
Maxime Sainte Catherine <maximesc@kth.se>
2021-02-12 17:56:20 +0100

    Investigating frame issue

--- AD-EYE/ROS_Packages/src/AD-EYE/src/point_cloud_frame_changer.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/point_cloud_frame_changer.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "tf/transform_listener.h"
+#include <iostream>
+#include "pcl_conversions/pcl_conversions.h"
 #include "pcl_ros/transforms.h"
 
 
@@ -15,15 +17,25 @@ class PointCloudFrameChanger {
         void pointcloud_transform_callback(sensor_msgs::PointCloud2 cloud_in)
         {
             sensor_msgs::PointCloud2 cloud_out_transformed;
-            try {
-                tf_listener_.waitForTransform("/base_link", "/world", cloud_in.header.stamp,
-                                            ros::Duration(3.0));
-                pcl_ros::transformPointCloud("/world", cloud_in,
-                                            cloud_out_transformed, tf_listener_);
-            }
-            catch(tf::TransformException &ex) {
-                ROS_ERROR("%s",ex.what());
-            }
+            std::string error_msg;
+            tf_listener_.waitForTransform("/base_link", "/world", cloud_in.header.stamp, ros::Duration(3.0), ros::Duration(0.01), &error_msg);
+            pcl_ros::transformPointCloud("/world", cloud_in, cloud_out_transformed, tf_listener_);
+
+            // try {
+            //     tf_listener_.waitForTransform("/base_link", "/world", cloud_in.header.stamp,
+            //                                 ros::Duration(3.0), ros::Duration(0.01), &error_msg);
+            //     tf::StampedTransform transform;
+            //     tf_listener_.lookupTransform("/world","/base_link", cloud_in.header.stamp, transform);
+            //     pcl_ros::transformPointCloud(cloud_in, cloud_out_transformed, transform);
+            //     // pcl_ros::transformPointCloud("/world", cloud_in, cloud_out_transformed, tf_listener_);
+            // }
+            // catch(tf::TransformException &ex) {
+            //     std::cout << "error caught" << std::endl;
+            //     ROS_ERROR("%s",ex.what());
+            // }
+
+            // Convert PCL type to ROS message
+            std::cout << "message from the waitForTransform" << error_msg << std::endl;
             pub_.publish(cloud_out_transformed);
         }
 
