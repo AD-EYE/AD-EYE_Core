3d09b25974cf3d85a3921f0c9099fd0939579086
Abhirami <abhsv@kth.se>
2021-03-08 15:15:11 +0100

    Aligned the label of textbox to center. Managed to collect the coordinates of a point on an image on mouseclick.

--- AD-EYE/ROS_Packages/src/GUI/adeye_gui/gui/gui.js
+++ AD-EYE/ROS_Packages/src/GUI/adeye_gui/gui/gui.js
@@ -1089,6 +1089,65 @@ function createGenericCard()
     row_div.appendChild(generic);
 }
 //----------------generic card----------------
-       
 
-         
+//----------------goal setting--------------------------
+
+/*  var myImg = document.getElementById("myImgId");
+ myImg.onmousedown = GetCoordinates;
+  */
+function FindPosition(oElement)
+{
+
+   
+  if(typeof( oElement.offsetParent ) != "undefined")
+  {
+
+    for(var posX = 0, posY = 0; oElement; oElement = oElement.offsetParent)
+    {
+      posX += oElement.offsetLeft;
+      posY += oElement.offsetTop;
+
+    }
+      return [ posX, posY ];
+    }
+    else
+    {
+      return [ oElement.x, oElement.y ];
+    }
+}
+
+function GetCoordinates(e)
+{
+  var PosX = 0;
+  var PosY = 0;
+  var ImgPos;
+  ImgPos = FindPosition(myImg);
+/*   document.getElementById("x1").innerHTML = ImgPos[1];
+ */
+
+  if (!e) 
+  var e = window.event;
+
+
+  if (e.pageX || e.pageY)
+  {
+
+    PosX = e.pageX;
+    PosY = e.pageY;
+
+  }
+  else if (e.clientX || e.clientY)
+    {
+      PosX = e.clientX + document.body.scrollLeft
+        + document.documentElement.scrollLeft;
+      PosY = e.clientY + document.body.scrollTop
+        + document.documentElement.scrollTop;
+
+    }
+  PosX = PosX - ImgPos[0];
+  PosY = PosY - ImgPos[1];
+  document.getElementById("x-co-ordinate").innerHTML = PosX;
+  document.getElementById("y-co-ordinate").innerHTML = PosY;
+} 
+
+//----------------goal setting------------------
