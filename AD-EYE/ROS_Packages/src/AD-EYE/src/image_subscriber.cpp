6ba4b61223a7604d9e93a1af1d0ec0e10bf6eb97
Abhirami Samy Vijaybhaskar <abhsv@kth.se>
2021-04-22 11:15:56 +0200

    Added an image subscriber to subscribe to the image publisher

--- AD-EYE/ROS_Packages/src/AD-EYE/src/image_subscriber.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/image_subscriber.cpp
@@ -1,16 +1,25 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Image.h"
+#include "sys/time.h"
+
+struct timeval end_cam;
 
 void imageCallback(const sensor_msgs::Image::ConstPtr& msg)
 {
-    ROS_INFO("I heard: [%s]", msg -> data.header);
+    /*Display sequence number that is received from publisher*/
+    std::cout<<unsigned(msg->data.front())<<std::endl;
+    /*Get the timestamp at the subscriber*/
+    gettimeofday(&end_cam, 0);
+    long int end_time = end_cam.tv_sec;
+    /*Display timestamp for the given sequence number*/
+    std::cout<<end_time<<std::endl;
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_subscriber");
     ros::NodeHandle n;
-    ros::Subcriber sub = n.subscribe("chatter", 1000, imageCallback);
+    ros::Subscriber sub = n.subscribe("camera_1/image_raw", 1000, imageCallback);
     ros::spin();
     return 0;
 }
