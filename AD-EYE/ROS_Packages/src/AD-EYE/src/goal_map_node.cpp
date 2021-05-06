8ddbc9311907f580b14fdf0b267e2478c8f0b7bf
Ruchit <ruchit@kth.se>
2021-05-06 12:07:45 +0200

    Resolved gui pixels callback naming

--- AD-EYE/ROS_Packages/src/AD-EYE/src/goal_map_node.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/goal_map_node.cpp
@@ -143,9 +143,9 @@ public:
         // Initialize node, publishers and subscribers
         map_extractor_ = nh.subscribe<grid_map_msgs::GridMap>("/safety_planner_gridmap", 1, &GoalMapNode::gridMapExtractorCallback, this);
         pub_extract_image_ = nh.advertise<sensor_msgs::Image>("/lane_layer_image", 1, true);
-        goal_pixels_ = nh.subscribe<std_msgs::Int16MultiArray>("/gui/goal_pixels", 1, &GoalMapNode::goalPixlesCoordinatesCallback, this);
+        goal_pixels_ = nh.subscribe<std_msgs::Int16MultiArray>("/gui/goal_pixels", 1, &GoalMapNode::goalPixelsCoordinatesCallback, this);
         pub_goal_position_ = nh.advertise<geometry_msgs::PoseStamped>("/adeye/goals", 1, true);
-
+    }
     /*!
      * \brief The main function of the Node. Contains the main loop.
      * \brief Basically, publish the ROS image and update it when grid map position is changed.
