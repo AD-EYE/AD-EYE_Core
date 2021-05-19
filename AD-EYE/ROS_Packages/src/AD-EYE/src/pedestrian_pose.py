961163f37cf82c2df10eb09150af7dd468b41f9f
Mercy <mercyt@kth.se>
2021-05-19 22:54:45 +0200

    Modified the node so as to send multiple poses

--- AD-EYE/ROS_Packages/src/AD-EYE/src/pedestrian_pose.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/pedestrian_pose.py
@@ -4,47 +4,58 @@ import roslib
 import tf
 import math
 from std_msgs.msg import Int32
+from geometry_msgs.msg import PoseArray
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
 
 class pedestrianPose ():
     
-    def __init__(self):
-        self.egopose_sub = rospy.Subscriber('/gnss_pose', PoseStamped, self.gnssPoseCallback)
-        self.goal_pub = rospy.Publisher('/pedestrian_pose', PoseStamped, queue_size=1)
+    def __init__(self,pose_List):
+
+        self.poseList = pose_List
+        
+        self.pedestrian_pose_array_pub = rospy.Publisher('/pedestrian_pose', PoseArray, queue_size=1)
     
-    def gnssPoseCallback(self,egopose):
-        pedestrianpose = PoseStamped()
-        pose_tolerance = Int32()
-        pose_tolerance.data = 20
-       
-        
-        pedestrianpose.pose.position.x = egopose.pose.position.x + pose_tolerance.data
-        pedestrianpose.pose.position.y = egopose.pose.position.y + pose_tolerance.data
+        pedestrian_pose_array= PoseArray()
 
-        pedestrianpose.pose.position.z = egopose.pose.position.z
-
-        pedestrianpose.pose.orientation.x = egopose.pose.orientation.x
-        pedestrianpose.pose.orientation.y = egopose.pose.orientation.y
-        pedestrianpose.pose.orientation.z = egopose.pose.orientation.z
-        pedestrianpose.pose.orientation.w = egopose.pose.orientation.w
-
-        pedestrianpose.header.seq = 1 
-        pedestrianpose.header.stamp = rospy.Time.now() 
-        pedestrianpose.header.frame_id = "map"
-        
+        for newPose in self.poseList:
+            pedestrian_pose_array.poses.append(Pose(newPose.pose.position, newPose.pose.orientation)) 
         rate = rospy.Rate(1)
-        
-        self.goal_pub.publish(pedestrianpose)
-        rospy.loginfo(pedestrianpose)
+        self.pedestrian_pose_array_pub.publish(pedestrian_pose_array)
+        rospy.loginfo(pedestrian_pose_array)
         rate.sleep()
-  
+
+def createPose(Px, Py, internal_Counter):
+    human_pose = PoseStamped()
+    human_pose.pose.position.x = Px + internal_Counter
+    human_pose.pose.position.y = Py + internal_Counter
+    human_pose.pose.position.z = 0.0 
+    human_pose.pose.orientation.x = 0.0
+    human_pose.pose.orientation.y = 0.0
+    human_pose.pose.orientation.z = 0.0 
+    human_pose.pose.orientation.w = 1.0
+
+    human_pose.header.seq = 1 
+    human_pose.header.stamp = rospy.Time.now() 
+    human_pose.header.frame_id = "map" 
+    return human_pose 
 
 if __name__=="__main__":
     rospy.init_node('pedestrian_pose')
     print ("pedestrianpose")
-    pedestrianPose()
-    rospy.spin()
+    counter = 0
+    while counter < 50 :
+        internal_Counter = 5 * counter
+        pose_List = []
+        pose_List.append( createPose(55.0, 176.0, internal_Counter) )
+        pose_List.append( createPose(100.0, 231.0, internal_Counter) )
+        pose_List.append( createPose(105.0, 236.0, internal_Counter) )
+        pose_List.append( createPose(110.0, 241.0, internal_Counter) )
+        pose_List.append( createPose(115.0, 246.0, internal_Counter) )
+        pose_List.append( createPose(120.0, 251.0, internal_Counter) )
+        counter = counter + 1
+        pedestrianPose(pose_List)
+        rospy.spin()
     
    
     
