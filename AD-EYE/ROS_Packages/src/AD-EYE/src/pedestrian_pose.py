6218abf4d198a5693b92c9b4a9c6bea7ec5167de
Mercy <mercyt@kth.se>
2021-05-20 09:54:47 +0200

    Send the poses directly from an array instead of list

--- AD-EYE/ROS_Packages/src/AD-EYE/src/pedestrian_pose.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/pedestrian_pose.py
@@ -3,58 +3,56 @@ import rospy
 import roslib
 import tf
 import math
-from std_msgs.msg import Int32
+
 from geometry_msgs.msg import PoseArray
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
 
 class pedestrianPose ():
     
-    def __init__(self,pose_List):
-
-        self.poseList = pose_List
-        
+    def __init__(self,pedestrian_pose_array):
         self.pedestrian_pose_array_pub = rospy.Publisher('/pedestrian_pose', PoseArray, queue_size=1)
     
-        pedestrian_pose_array= PoseArray()
-
-        for newPose in self.poseList:
-            pedestrian_pose_array.poses.append(Pose(newPose.pose.position, newPose.pose.orientation)) 
+        self.pedestrian_pose_array = pedestrian_pose_array
         rate = rospy.Rate(1)
-        self.pedestrian_pose_array_pub.publish(pedestrian_pose_array)
-        rospy.loginfo(pedestrian_pose_array)
+        self.pedestrian_pose_array_pub.publish(self.pedestrian_pose_array)
+        rospy.loginfo(self.pedestrian_pose_array)
         rate.sleep()
 
-def createPose(Px, Py, internal_Counter):
-    human_pose = PoseStamped()
-    human_pose.pose.position.x = Px + internal_Counter
-    human_pose.pose.position.y = Py + internal_Counter
-    human_pose.pose.position.z = 0.0 
-    human_pose.pose.orientation.x = 0.0
-    human_pose.pose.orientation.y = 0.0
-    human_pose.pose.orientation.z = 0.0 
-    human_pose.pose.orientation.w = 1.0
+def addPoseToArray(Px, Py, pose_increment_value, pedestrian_pose_array):
+    human_pose = Pose()
+    human_pose.position.x = Px + pose_increment_value
+    human_pose.position.y = Py + pose_increment_value
+    human_pose.position.z = 0.0 
+    human_pose.orientation.x = 0.0
+    human_pose.orientation.y = 0.0
+    human_pose.orientation.z = 0.0 
+    human_pose.orientation.w = 1.0
 
-    human_pose.header.seq = 1 
-    human_pose.header.stamp = rospy.Time.now() 
-    human_pose.header.frame_id = "map" 
-    return human_pose 
+    #human_pose.header.seq = 1 
+    #human_pose.header.stamp = rospy.Time.now() 
+    #human_pose.header.frame_id = "map" 
+
+    pedestrian_pose_array.poses.append(Pose(human_pose.position, human_pose.orientation))
 
 if __name__=="__main__":
     rospy.init_node('pedestrian_pose')
-    print ("pedestrianpose")
+    
     counter = 0
-    while counter < 50 :
-        internal_Counter = 5 * counter
-        pose_List = []
-        pose_List.append( createPose(55.0, 176.0, internal_Counter) )
-        pose_List.append( createPose(100.0, 231.0, internal_Counter) )
-        pose_List.append( createPose(105.0, 236.0, internal_Counter) )
-        pose_List.append( createPose(110.0, 241.0, internal_Counter) )
-        pose_List.append( createPose(115.0, 246.0, internal_Counter) )
-        pose_List.append( createPose(120.0, 251.0, internal_Counter) )
+    while counter < 10 :
+        pose_increment_value = 5 * counter
+        pedestrian_pose_array = PoseArray()
+        #pedestrian_pose_array.header.seq = 1 
+        #pedestrian_pose_array.header.stamp = rospy.Time.now() 
+        #pedestrian_pose_array.header.frame_id = "map"
+        addPoseToArray(55.0, 176.0, pose_increment_value, pedestrian_pose_array) 
+        addPoseToArray(100.0, 181.0, pose_increment_value, pedestrian_pose_array) 
+        addPoseToArray(105.0, 186.0, pose_increment_value, pedestrian_pose_array)
+        addPoseToArray(110.0, 191.0, pose_increment_value, pedestrian_pose_array)
+        addPoseToArray(115.0, 196.0, pose_increment_value, pedestrian_pose_array)
+        addPoseToArray(120.0, 201.0, pose_increment_value, pedestrian_pose_array)
         counter = counter + 1
-        pedestrianPose(pose_List)
+        pedestrianPose(pedestrian_pose_array)
         rospy.spin()
     
    
