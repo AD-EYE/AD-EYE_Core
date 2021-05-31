802bb54e83ad17debd080a4304bea15fc588c408
Mercy <mercyt@kth.se>
2021-05-31 18:42:03 +0200

    Improved the code conventions

--- AD-EYE/ROS_Packages/src/AD-EYE/src/loop_sender.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/loop_sender.py
@@ -6,50 +6,49 @@ import tf
 import time
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
-from std_msgs.msg import Int32
 
 #Initiate the class loopgoal which will publish the goals one by one and make the car loop around these goal points.
-class loopgoal ():
+class LoopGoal ():
 
     #Constructor
-    def __init__(self,goalList):
+    def __init__(self,goal_list):
         
-        self.goal_List = goalList
-        self.current_goal_Index = 0
+        self.goal_list_ = goal_list
+        self.current_goal_index_ = 0
                
         #Set thePublishers Subscribers 
-        self.position_sub = rospy.Subscriber('/gnss_pose', PoseStamped, self.gnssPoseCallback)
-        self.goal_pub = rospy.Publisher('/adeye/goals', PoseStamped, queue_size=1)  
+        self.position_sub_ = rospy.Subscriber('/gnss_pose', PoseStamped, self.gnssPoseCallback)
+        self.goal_pub_ = rospy.Publisher('/adeye/goals', PoseStamped, queue_size=1)  
           
     #Function for publishing new goals 
     def publishNewGoal(self,index): 
         print("Publish New Goal: ",index)
-        self.goal_pub.publish(self.goal_List[index])        
-        self.current_goal_Index = index
+        self.goal_pub_.publish(self.goal_list_[index])        
+        self.current_goal_index_ = index
 
     #Function for calculating the euclidean distance between egopose and goalpose
-    def getDistance(self, egoPose, x2, y2):
-        distance = ( (x2 - egoPose.x) ** 2 + (y2 - egoPose.y) ** 2 ) ** 0.5
+    def getDistance(self, ego_pose, x2, y2):
+        distance = ( (x2 - ego_pose.x) ** 2 + (y2 - egoPose.y) ** 2 ) ** 0.5
         print("Distance to goal: ",distance)
         return(distance)
 
     #Callback function which checks whether the car is within a distance of the goal and if so calls the function to publish the next goal,listens to the topic /gnss_pose   
-    def gnssPoseCallback(self,egoPose):
+    def gnssPoseCallback(self,ego_pose):
 
-        x = self.goal_List[self.current_goal_Index].pose.position.x
-        y = self.goal_List[self.current_goal_Index].pose.position.y        
+        x = self.goal_list_[self.current_goal_index_].pose.position.x
+        y = self.goal_list_[self.current_goal_index_].pose.position.y        
         
         rate = rospy.Rate(1)               #Rate at which the goals are being published in hz
-        
-        if(self.getDistance(egoPose.pose.position, x, y) <= 50 ): 
-            index_Counter = 0
-            if( self.current_goal_Index < (len(self.goal_List) - 1) ):
-                index_Counter = self.current_goal_Index + 1
+        DISTANCE_TOLERANCE = 50            #in meters
+        if(self.getDistance(ego_pose.pose.position, x, y) <= DISTANCE_TOLERANCE ): 
+            index_counter = 0                       #Initiate the Index counter
+            if( self.current_goal_index_ < (len(self.goal_list_) - 1) ):
+                index_counter = self.current_goal_index_ + 1
 
-            print("index_Counter: ",index_Counter, "; current_goal_Index: ",self.current_goal_Index)            
-            self.publishNewGoal(index_Counter)
+            print("index_Counter: ",index_counter, "; current_goal_index_: ",self.current_goal_index_)            
+            self.publishNewGoal(index_counter)
         else:
-            self.publishNewGoal(self.current_goal_Index)
+            self.publishNewGoal(self.current_goal_index_)
                     
         rate.sleep()
                  
@@ -70,14 +69,14 @@ def createGoal(Px, Py, Qx, Qy, Qw):
     return goal
 
 if __name__=="__main__":
-    print("the car loop")
+    
     rospy.init_node('loop_sender') #Initialising the node.
 
     #Creating a list for storing the goal points.
-    goal_List = []
-    goal_List.append( createGoal(257.0, 170.0, 0.0, 0.0, 1.0) )
-    goal_List.append( createGoal(223.0, 226.0, 0.0, 1.0, 0.0) )
-    goal_List.append( createGoal(84.0, 170.0, 0.0, 0.0, 1.0) )
+    goal_list = []
+    goal_list.append( createGoal(257.0, 170.0, 0.0, 0.0, 1.0) )
+    goal_list.append( createGoal(223.0, 226.0, 0.0, 1.0, 0.0) )
+    goal_list.append( createGoal(84.0, 170.0, 0.0, 0.0, 1.0) )
    
-    loopgoal(goal_List)
+    LoopGoal(goal_list)
     rospy.spin()
