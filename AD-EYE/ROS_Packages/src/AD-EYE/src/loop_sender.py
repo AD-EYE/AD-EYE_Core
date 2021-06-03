f99d1b2fb1afa45fda9d85d08f5cf3c18dfe9d49
Mercy <mercyt@kth.se>
2021-06-03 14:03:54 +0200

    Added Print statements

--- AD-EYE/ROS_Packages/src/AD-EYE/src/loop_sender.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/loop_sender.py
@@ -7,29 +7,34 @@ import time
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
 
-#Initiate the class loopgoal which will publish the goals one by one and make the car loop around these goal points.
-class LoopGoal ():
+#Initiate the class BaseMapGoalLooper which will publish the goals one by one and make the car loop around these goal points.
+class BaseMapGoalLooper ():
 
     #Constructor
     def __init__(self,goal_list):
         
         self.goal_list_ = goal_list
         self.current_goal_index_ = 0
-               
+        self.DISTANCE_TOLERANCE_ = 50     #in meters 
+        self.loop_counter_ = 1       # Counter to count the number of loops the car has completed
+        self.goal_counter_ = 1       #Counter to count the number of goals published
+
+        print("Total Goals Published: ",self.goal_counter_, "; Current Loop Number: ",self.loop_counter_)
         #Set thePublishers Subscribers 
         self.position_sub_ = rospy.Subscriber('/gnss_pose', PoseStamped, self.gnssPoseCallback)
         self.goal_pub_ = rospy.Publisher('/adeye/goals', PoseStamped, queue_size=1)  
           
     #Function for publishing new goals 
-    def publishNewGoal(self,index): 
-        print("Publish New Goal: ",index)
+    def publishNewGoal(self,index):
+        if(self.current_goal_index_ != index):
+            print("Total Goals Published: ",self.goal_counter_, "; Current Loop Number: ",self.loop_counter_)
         self.goal_pub_.publish(self.goal_list_[index])        
         self.current_goal_index_ = index
+        
 
     #Function for calculating the euclidean distance between egopose and goalpose
     def getDistance(self, ego_pose, x2, y2):
-        distance = ( (x2 - ego_pose.x) ** 2 + (y2 - egoPose.y) ** 2 ) ** 0.5
-        print("Distance to goal: ",distance)
+        distance = ( (x2 - ego_pose.x) ** 2 + (y2 - ego_pose.y) ** 2 ) ** 0.5
         return(distance)
 
     #Callback function which checks whether the car is within a distance of the goal and if so calls the function to publish the next goal,listens to the topic /gnss_pose   
@@ -37,20 +42,21 @@ class LoopGoal ():
 
         x = self.goal_list_[self.current_goal_index_].pose.position.x
         y = self.goal_list_[self.current_goal_index_].pose.position.y        
-        
-        rate = rospy.Rate(1)               #Rate at which the goals are being published in hz
-        DISTANCE_TOLERANCE = 50            #in meters
-        if(self.getDistance(ego_pose.pose.position, x, y) <= DISTANCE_TOLERANCE ): 
-            index_counter = 0                       #Initiate the Index counter
+        index_counter = 0                       #Initiate the Index counter
+                     
+                  
+        if(self.getDistance(ego_pose.pose.position, x, y) <= self.DISTANCE_TOLERANCE_): 
+            
             if( self.current_goal_index_ < (len(self.goal_list_) - 1) ):
                 index_counter = self.current_goal_index_ + 1
+                self.goal_counter_ = self.goal_counter_+ 1
+                self.publishNewGoal(index_counter)
+            else:
+                self.loop_counter_ = self.loop_counter_+ 1
 
-            print("index_Counter: ",index_counter, "; current_goal_index_: ",self.current_goal_index_)            
-            self.publishNewGoal(index_counter)
         else:
             self.publishNewGoal(self.current_goal_index_)
-                    
-        rate.sleep()
+                
                  
 #Setting the goal Position and Orientation coordinates
 def createGoal(Px, Py, Qx, Qy, Qw):
@@ -78,5 +84,5 @@ if __name__=="__main__":
     goal_list.append( createGoal(223.0, 226.0, 0.0, 1.0, 0.0) )
     goal_list.append( createGoal(84.0, 170.0, 0.0, 0.0, 1.0) )
    
-    LoopGoal(goal_list)
+    BaseMapGoalLooper(goal_list)
     rospy.spin()
