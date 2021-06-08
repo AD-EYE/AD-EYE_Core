4fc0e78dddc840872dae31c396c415971fcc648a
Alexis Schaëfer <alexissc@kth.se>
2021-06-08 16:31:03 +0100

    Created the Mapping folder and generated the point cloud map

---
+++ AD-EYE/Experiments/W07_Neighborhood/Mapping/W07_Neighborhood_cs_grt_rtw/W07_Neighborhood_cs_types.h
@@ -0,0 +1,40 @@
+/*
+ * W07_Neighborhood_cs_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "W07_Neighborhood_cs".
+ *
+ * Model version              : 1.92
+ * Simulink Coder version : 9.1 (R2019a) 23-Nov-2018
+ * C source code generated on : Mon Jun  7 13:17:09 2021
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_W07_Neighborhood_cs_types_h_
+#define RTW_HEADER_W07_Neighborhood_cs_types_h_
+#include "rtwtypes.h"
+#include "builtin_typeid_types.h"
+#include "multiword_types.h"
+#include "zero_crossing_types.h"
+#include "prescan_selfsensor_data.h"
+#include "prescan_motion_data.h"
+#include "prescan_stateactuator_data.h"
+#include "prescan_controller_data.h"
+#include "prescan_terminator_data.h"
+#include "prescan_synchronizer_data.h"
+
+/* Parameters (default storage) */
+typedef struct P_W07_Neighborhood_cs_T_ P_W07_Neighborhood_cs_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_W07_Neighborhood_cs_T RT_MODEL_W07_Neighborhood_cs_T;
+
+#endif                             /* RTW_HEADER_W07_Neighborhood_cs_types_h_ */
