57fb5d49e81e4918b85edd4114ce79d6acd74f49
Nicolas_Pelissier <pelissi@kth.se>
2021-06-08 17:30:43 +0200

    Generation of pointcloud_files W08

---
+++ AD-EYE/Experiments/W08_Small_Town/Mapping/W08_Small_Town_cs_grt_rtw/builtin_typeid_types.h
@@ -0,0 +1,46 @@
+/*
+ * builtin_typeid_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "W08_Small_Town_cs".
+ *
+ * Model version              : 1.85
+ * Simulink Coder version : 9.1 (R2019a) 23-Nov-2018
+ * C source code generated on : Mon Jun  7 14:22:15 2021
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef BUILTIN_TYPEID_TYPES_H
+#define BUILTIN_TYPEID_TYPES_H
+#include "rtwtypes.h"
+#ifndef BUILTIN_TYPEID_TYPES
+#define BUILTIN_TYPEID_TYPES
+
+/* Enumeration of built-in data types */
+typedef enum {
+  SS_DOUBLE = 0,                       /* real_T    */
+  SS_SINGLE = 1,                       /* real32_T  */
+  SS_INT8 = 2,                         /* int8_T    */
+  SS_UINT8 = 3,                        /* uint8_T   */
+  SS_INT16 = 4,                        /* int16_T   */
+  SS_UINT16 = 5,                       /* uint16_T  */
+  SS_INT32 = 6,                        /* int32_T   */
+  SS_UINT32 = 7,                       /* uint32_T  */
+  SS_BOOLEAN = 8                       /* boolean_T */
+} BuiltInDTypeId;
+
+#define SS_NUM_BUILT_IN_DTYPE          ((int_T)SS_BOOLEAN+1)
+
+/* Enumeration for MAT-file logging code */
+typedef int_T DTypeId;
+
+#endif                                 /* BUILTIN_TYPEID_TYPES */
+#endif                                 /* BUILTIN_TYPEID_TYPES_H */
