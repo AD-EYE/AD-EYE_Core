4016dd34768788843c4071314e9e8602d6c2ca44
Alexis Schaëfer <alexissc@kth.se>
2021-06-30 12:03:11 +0200

    Fixed the path to take into account the different login names on different computers

--- Helper_Scripts/Git_user_setup_scripts/Ubuntu_clear_git_user_info.sh
+++ Helper_Scripts/Git_user_setup_scripts/Ubuntu_clear_git_user_info.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
-cd /home/adeye/AD-EYE_Core
+cd ~/AD-EYE_Core
 git config --local user.name ""
 git config --local user.email ""
 git config --global user.name ""
 git config --global user.email ""
-cd /home/adeye/AD-EYE_Core/autoware.ai/src/autoware/common
+cd ~/AD-EYE_Core/autoware.ai/src/autoware/common
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/AD-EYE_Core/autoware.ai/src/autoware/core_perception
+cd ~/AD-EYE_Core/autoware.ai/src/autoware/core_perception
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/AD-EYE_Core/autoware.ai/src/autoware/core_planning
+cd ~/AD-EYE_Core/autoware.ai/src/autoware/core_planning
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/AD-EYE_Core/autoware.ai/src/autoware/documentation
+cd ~/AD-EYE_Core/autoware.ai/src/autoware/documentation
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/AD-EYE_Core/autoware.ai/src/autoware/messages
+cd ~/AD-EYE_Core/autoware.ai/src/autoware/messages
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/AD-EYE_Core/autoware.ai/src/autoware/simulation
+cd ~/AD-EYE_Core/autoware.ai/src/autoware/simulation
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/AD-EYE_Core/autoware.ai/src/autoware/utilities
+cd ~/AD-EYE_Core/autoware.ai/src/autoware/utilities
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/AD-EYE_Core/autoware.ai/src/autoware/visualization
+cd ~/AD-EYE_Core/autoware.ai/src/autoware/visualization
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/AD-EYE_Core/Pex_Data_Extraction
+cd ~/AD-EYE_Core/Pex_Data_Extraction
 git config --local user.name ""
 git config --local user.email ""
-cd /home/adeye/
+cd ~
