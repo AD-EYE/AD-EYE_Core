cb318f6ec39d9d5246a248bb6058a848af4f67e0
Melissa Cruz <melissac@kth.se>
2021-08-13 09:53:40 +0200

    Correct some mistakes in computation of TTC

--- AD-EYE/ROS_Packages/src/AD-EYE/src/kpi_computation.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/kpi_computation.cpp
@@ -58,15 +58,17 @@ private:
     ros::Subscriber sub_trajectory_ego_;
     ros::Subscriber sub_other_cars_;
 
+    //Ros utils
+    ros::Rate rate_;
+    float frequency_ = 20; // this value should be aligned with the frequency value used in the GridMapCreator_node
+
     // Array that stores information of different KPIs
     std_msgs::Float32MultiArray kpi_array_;
 
     // Parameters for the update of KPIs
     bool ttc_first_callback_ = true;
-
-    // Time parameters
-    float time_ = 0;
-    float time_old_ = 0;
+    bool mttc_first_callback_ = true;
+    bool mttc_second_callback_ = true;
 
     // Velocity of the ego car
     float velocity_ego_ = 0;
@@ -77,8 +79,6 @@ private:
     // Position of the ego car
     float x_ego_;
     float y_ego_;
-    float x_ego_old_;
-    float y_ego_old_;
     bool position_ego_callback_ = false;
 
     // The trajectory of the ego car
@@ -96,62 +96,79 @@ private:
     float x_other_cars_old_;
     float y_other_cars_old_;
     float velocity_other_cars_;
+    float velocity_other_cars_old_;
+    float acceleration_other_cars_;
     bool other_cars_callback_ = false;
+    // parameter to know if there is other car in the trajectory
+    bool car_in_trajectory_ = false;
 
     //Dimensions
     float length_ego_ = 4.6;
     float width_ego_ = 2.2;
 
-    //Ros utils
-    ros::Rate rate_;
+    // Time range useful to compute velocity of other cars and acceleration.
+    float dt = 1 / frequency_;
 
     /*!
      * \brief Update the value of KPI Time to Collision.
      * \details TTC is defined as D/dV,
      * D the distance between ego car and the other car,
-     * dV the difference of velocitybetween two vehicles.
+     * dV the difference of velocity between the two vehicles.
      */
     void ttcUpdate() {
         // the distance between other cars and the ego car
         float d;
         // the difference between the velocity of the ego car and the other car
         float delta_v;
+        // Reset the value
+        car_in_trajectory_ = false;
         // The number of other cars
         size_t N_other = other_cars_.poses.size();
         // Search the closest other car in the trajectory of the ego car
         d = pow(pow(other_cars_.poses.at(0).position.x - x_ego_, 2) + pow(other_cars_.poses.at(0).position.y - y_ego_, 2), 0.5);
+        x_other_cars_ = other_cars_.poses.at(0).position.x;
+        y_other_cars_ = other_cars_.poses.at(0).position.y;
         for(int i = 0; i < (int)N_other; i++) {
-            grid_map::Position pos_other;
-            pos_other.x() = other_cars_.poses.at(i).position.x;
-            pos_other.y() = other_cars_.poses.at(i).position.y;
-            grid_map::Position pos_trajectory;
-            pos_trajectory.x() = trajectory_.waypoints.at(i).pose.pose.position.x;
-            pos_trajectory.y() = trajectory_.waypoints.at(i).pose.pose.position.y;
+            float x_pos_other = other_cars_.poses.at(i).position.x; // x coordinate of other car to compare with the trajectory
+            float y_pos_other = other_cars_.poses.at(i).position.y; // y coordinate of other car to compare with the trajectory
+            float x_pos_trajectory = trajectory_.waypoints.at(i).pose.pose.position.x; // x coordinate of the trajectory point to compare
+            float y_pos_trajectory = trajectory_.waypoints.at(i).pose.pose.position.y; // y coordinate of the trajectory point to compare
             // If the other car is in the trajectory
-            if((pos_other.x() = pos_trajectory.x()) && (pos_other.y() = pos_trajectory.y())){
+            if((x_pos_other = x_pos_trajectory) && (y_pos_other = y_pos_trajectory)){
                 // If the other car is the closest of all already tested other cars
-                if(pow(pow(other_cars_.poses.at(i).position.x - x_ego_, 2) + pow(other_cars_.poses.at(i).position.y - y_ego_, 2), 0.5) < d) {
-                    d = pow(pow(other_cars_.poses.at(i).position.x - x_ego_, 2) + pow(other_cars_.poses.at(i).position.y - y_ego_, 2), 0.5);
-                    x_other_cars_ = other_cars_.poses.at(i).position.x;
-                    y_other_cars_ = other_cars_.poses.at(i).position.y;
+                if(pow(pow(x_pos_other - x_ego_, 2) + pow(y_pos_other - y_ego_, 2), 0.5) < d) {
+                    d = pow(pow(x_pos_other - x_ego_, 2) + pow(y_pos_other - y_ego_, 2), 0.5);
+                    x_other_cars_ = x_pos_other;
+                    y_other_cars_ = y_pos_other;
                 }
+                car_in_trajectory_ = true;
             }
-            
-        }
-        // If this is the first time the function is called, the velocity can't be computed.
-        if(!ttc_first_callback_) {
-            // The value of the velocity of other cars
-            velocity_other_cars_ = pow(pow((x_other_cars_ - x_other_cars_old_)/(time_ - time_old_), 2) + pow((y_other_cars_ - y_other_cars_old_)/(time_ - time_old_), 2), 0.5);
-            delta_v = velocity_other_cars_ - velocity_ego_;
+            std::cout<<"x other = "<<x_pos_other<<std::endl;
+            std::cout<<"y other = "<<y_pos_other<<std::endl;
         }
 
-        kpi::ttc = d / delta_v;
+        // If there is no car in the trajectory of the ego car, TTC can't be computed.
+        if(!car_in_trajectory_) {
+            return;
+        }
+
+        // If this is the first time the function is called, the velocity can't be computed.
+        if(!ttc_first_callback_) {
+            // The value of the velocity of other cars is obtained by derivation of the position
+            float v_x = (x_other_cars_ - x_other_cars_old_)/dt; // x component of the velocity
+            float v_y = (y_other_cars_ - y_other_cars_old_)/dt; // y component of the velocity
+            velocity_other_cars_ = pow(pow(v_x, 2) + pow(v_y, 2), 0.5);
+            delta_v = fabs(velocity_other_cars_ - velocity_ego_);
+
+            kpi::ttc = d / delta_v;
+        }
+        ttc_first_callback_ = false;
 
         // Update the position of other car
         x_other_cars_old_ = x_other_cars_;
         y_other_cars_old_ = y_other_cars_;
     }
-    
+
     /*!
      * \brief gridmapCallback: called when information of the gridmap has changed.
      * \param msg A smart pointer to the message from the topic.
@@ -219,7 +236,7 @@ public:
         sub_other_cars_ = nh_.subscribe<geometry_msgs::PoseArray>("/pose_otherCar", 1, &RiskEnvelopeComputation::otherCarsCallback, this);
         sub_gridmap_ = nh_.subscribe<grid_map_msgs::GridMap>("/safety_planner_gridmap", 1, &RiskEnvelopeComputation::gridmapCallback, this);
         
-        rate_ = ros::Rate(20);
+        rate_ = ros::Rate(frequency_);
     }
 
     void run() {
@@ -228,9 +245,14 @@ public:
 
                 if(other_cars_callback_) {
                     ttcUpdate();
+                    other_cars_callback_ = false;
                 }
                 else {
-                    kpi::ttc = -1; // Value if there is no other cars on the trajectory
+                    kpi::ttc = -1; // Value if there is no other cars detected
+                }
+
+                if(!car_in_trajectory_) {
+                    kpi::ttc = -1; // Value if there is no other car in the ego car trajectory
                 }
 
                 std::cout<<"TTC = "<<kpi::ttc<<std::endl; // Test if the ttc is well updated.
