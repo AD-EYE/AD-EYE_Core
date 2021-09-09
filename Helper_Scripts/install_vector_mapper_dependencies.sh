53e7c187b20c8d00b513a30f7a08e9b3f5b419b8
Mercy <mercyt@kth.se>
2021-09-09 23:34:10 +0200

    Modified the script

--- Helper_Scripts/install_vector_mapper_dependencies.sh
+++ Helper_Scripts/install_vector_mapper_dependencies.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+#This bash script installs the python dependencies required for installing vector mapper
 
+# Ask whether we need to install vector mapper
 read -p "Do you want to install the Vector Mapper? (y/n)" answer
+
 
 case $answer in 
     [Yy] )
@@ -12,11 +15,22 @@ case $answer in
     echo "Answer with a y or n "; exit 1;;
 esac
 
+# Go to the location where the requirements.txt file is and check if it exists
 cd ~/AD-EYE_Core/Pex_Data_Extraction/pex2csv
-pwd
-git checkout dev
+
+echo "Checking whether the file requirements.txt exists in your system ...."
+if [ -f requirements.txt ] 
+then
+    echo "This file exists in this system"
+fi
+
+git checkout feature/install_vector_mapper_dependencies
+
+# Upgrade pip
 echo "Upgrading pip..."
 pip3 install --upgrade pip
+
+#Installing dependencies
 echo "Installing dependencies"
 pip3 install -r requirements.txt
 sudo apt-get install python3-tk
