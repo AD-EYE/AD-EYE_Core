32e2b38e7b1856c725c80bb92009214346ae5728
Alicia Fossard <fossard@kth.se>
2021-09-27 15:45:59 +0200

    Modified to put values inside csv file

--- AD-EYE/ROS_Packages/src/AD-EYE_scenario_managers/src/scenario_30.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE_scenario_managers/src/scenario_30.cpp
@@ -11,22 +11,30 @@
 #include <iostream>
 #include <sstream>
 #include <boost/filesystem.hpp>
+#include <fstream>
 
 /*!
  * \brief scenario30 node : Manages start and stop of the recording & experiment for scenario30
  */
-class scenario30: public ScenarioManagerTemplate {
+class Scenario30: public ScenarioManagerTemplate {
 
     private:
         ros::Subscriber speed_sub_;
         ros::Subscriber speed_sub2_;
         ros::Subscriber speed_sub3_;
         ros::Subscriber speed_sub4_;
+        ros::Subscriber collision_distance_sub5_;
+        ros::NodeHandle nh;
+        double distance_ ;
+        double initial_ego_speed_ ;
+        double initial_non_ego_speed_ ;
         float SPEED_THRESHOLD_STOP_ = 0.0f;
         float ego_speed_ = 0.0;
         float non_ego_speed_ = 0.0;
         float non_ego_angle_ = 0.0;
         float pedestrian_speed_ = 0.0;
+        float distance_ego_actor_ = 0.0;
+        float collision_distance_ = 0.0;
 
         void speedCallback(geometry_msgs::TwistStamped msg)
         {
@@ -48,6 +56,11 @@ class scenario30: public ScenarioManagerTemplate {
             pedestrian_speed_ = msg.twist.linear.x;
         }
 
+        void speedCallback5(geometry_msgs::TwistStamped msg)
+        {
+            collision_distance_ = msg.twist.linear.x;
+        }
+
 
     public:
         /*!
@@ -56,13 +69,14 @@ class scenario30: public ScenarioManagerTemplate {
         * \param frequency The frequency at which the main loop will be run.
         * \details Initializes the node and its components such the as subscribers.
         */
-        scenario30(ros::NodeHandle nh, int frequency): ScenarioManagerTemplate(nh, frequency)
+        Scenario30(ros::NodeHandle nh, int frequency): ScenarioManagerTemplate(nh, frequency)
         {
-            speed_sub_ = ScenarioManagerTemplate::nh_.subscribe("/current_velocity", 10, &scenario30::speedCallback, this);
+            speed_sub_ = ScenarioManagerTemplate::nh_.subscribe("/current_velocity", 10, &Scenario30::speedCallback, this);
             // ScenarioManagerTemplate::nh_.param<float>("/simulink/rain_intensity", rain_intensity_, 0.0);
-            speed_sub2_ = ScenarioManagerTemplate::nh_.subscribe("/other_velocity_1", 10, &scenario30::speedCallback2, this);
-            speed_sub3_ = ScenarioManagerTemplate::nh_.subscribe("/other_velocity_1", 10, &scenario30::speedCallback3, this);
-            speed_sub4_ = ScenarioManagerTemplate::nh_.subscribe("/other_velocity_2", 10, &scenario30::speedCallback4, this);
+            speed_sub2_ = ScenarioManagerTemplate::nh_.subscribe("/other_velocity_1", 10, &Scenario30::speedCallback2, this);
+            speed_sub3_ = ScenarioManagerTemplate::nh_.subscribe("/other_velocity_1", 10, &Scenario30::speedCallback3, this);
+            speed_sub4_ = ScenarioManagerTemplate::nh_.subscribe("/other_velocity_2", 10, &Scenario30::speedCallback4, this);
+            collision_distance_sub5_ = ScenarioManagerTemplate::nh_.subscribe("/distance_to_ego_1", 10, &Scenario30::speedCallback5, this);
         }
 
         
@@ -72,7 +86,18 @@ class scenario30: public ScenarioManagerTemplate {
         */
         void startExperiment()
         {
-            std::cout << "scenario30: started experiment" << std::endl;
+            // std::cout << "scenario30: started experiment" << std::endl;
+            nh.getParam("/ConditionROS_1_1",  distance_) ;
+            nh.getParam("/initial_speed_0",  initial_ego_speed_) ;
+            nh.getParam("/initial_speed_1",  initial_non_ego_speed_) ;   
+            std::fstream myfile;
+            myfile.open ("/home/adeye/Experiment_Results/Pedestrian_Action/Pedestrian.csv", std::ios_base::app);
+            myfile << "\n New experiment\n";
+            myfile << "Initial speed of the ego car:," << initial_ego_speed_ << "\n";
+            myfile << "Iniatial speed of the non ego car:," << initial_non_ego_speed_ << "\n";
+            std::cout << "scenario30: stopped recording" << std::endl;
+            myfile << "Trigger distance between the pedestrian and the non ego car:," << distance_<< "\n";
+            myfile.close();
         }
 
         /*!
@@ -80,15 +105,16 @@ class scenario30: public ScenarioManagerTemplate {
         */
         void stopExperiment()
         {
-            std::cout << "scenario30: stopped experiment" << std::endl;
-        }
+            //std::cout << "scenario30: stopped experiment" << std::endl;
+        }        
 
         /*!
         * \brief Starts the experiment relevant recording
         */
         void startRecording()
         {
-            std::cout << "scenario30: started recording" << std::endl;
+            //std::cout << "scenario30: started recording" << std::endl;
+
         }
 
         /*!
@@ -96,7 +122,15 @@ class scenario30: public ScenarioManagerTemplate {
         */
         void stopRecording()
         {
-            std::cout << "scenario30: stopped recording" << std::endl;
+            //std::cout << "scenario30: stopped recording" << std::endl;
+
+            if (collision_distance_<6) {
+                std::ofstream myfile;
+                myfile.open ("/home/adeye/Experiment_Results/Pedestrian_Action/Pedestrian.csv", std::ios_base::app);
+                std::cout << "scenario30: stopped recording" << std::endl;
+                myfile << "Speed of the ego car at collision:," << ego_speed_ << "\n";
+                myfile.close();
+            }
         }
 
 
@@ -118,7 +152,7 @@ class scenario30: public ScenarioManagerTemplate {
         bool stopRecordingConditionFulfilled()
         {
             
-            return (non_ego_speed_ ==0);
+            return (ego_speed_ < 1.2);
         }
 
         /*!
@@ -126,7 +160,9 @@ class scenario30: public ScenarioManagerTemplate {
         */
         bool startExperimentConditionFulfilled()
         {
-            return (non_ego_angle_ < 0);
+
+            return (ego_speed_ > 5);
+            
         }
 
         /*!
@@ -135,10 +171,8 @@ class scenario30: public ScenarioManagerTemplate {
         bool stopExperimentConditionFulfilled()
         {
             
-            return (non_ego_speed_ ==0);
+            return (ego_speed_ < 1 || collision_distance_< 5);
         }
-
-        
 
 
 };
@@ -149,7 +183,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "scenario30");
     ros::NodeHandle private_nh("~");
 
-    std::cout << "Analyzing scenario30" << std::endl;
-    scenario30 scenario_30(private_nh, 20);
+
+    //std::cout << "Analyzing scenario30" << std::endl;
+    Scenario30 scenario_30(private_nh, 20);
     scenario_30.run();
 }
