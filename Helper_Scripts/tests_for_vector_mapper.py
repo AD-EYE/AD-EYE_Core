02c637ca49565290644e1c0ca99b0343fb4512bc
yfyu <yfyu@kth.se>
2021-10-08 19:24:37 +0200

    Modified: can read csv file given only its world name

--- Helper_Scripts/tests_for_vector_mapper.py
+++ Helper_Scripts/tests_for_vector_mapper.py
@@ -2,66 +2,64 @@ import roslaunch
 import rospy
 import pandas as pd 
 import sys
-from roslaunch.core import RLException
+import os
+from roslaunch.config import load_config_default
 
 
 
-#Command line input: python3 tests_for_vector_mapper.py world_name:=W01_Base_Map point.csv
+#Command line input: python3 tests_for_vector_mapper.py world_name:=W01_Base_Map
 
-
-#Input: launch file path
-#Make a dictionary from arg"
-#Return: Dictionary of key and values 
-def get_args(roslaunch_file):
+##A function for reading launch_file and making a dictionary of arg". 
+#Returning it as keys and values 
+#@param roslaunch_file The list containing the str of the file path
+def getArgs(roslaunch_file):
         loader = roslaunch.xmlloader.XmlLoader(resolve_anon=False)
         config = load_config_default(roslaunch_file, None, loader=loader, verbose=False, assign_machines=False)
         return loader.root_context.resolve_dict['arg']
 
-def read_csv_file(csv_path, csv_file_name):
+##A function for reading point.csv file and returning its start and end point.
+#@param csv_path The str of csv_path
+#@param csv_file_name The str of csv name
+def readCsvFile(csv_path, csv_file_name):
+
         list_of_points = pd.read_csv(csv_path + csv_file_name, delimiter=',', header=None, skip_blank_lines=True,float_precision=None)
 
-        """     Point.csv
-                self.B = 0.0        # Latitude
-                self.L = 0.0        # Longitude
-                self.H = z       # Altitude
-                self.Ly = x         # Global Y, values are swapped for Autoware, does not work swapping the lines
-                self.Bx = y         # Global X
-                self.ReF = 7
-                self.MCODE1 = 0
-                self.MCODE2 = 0
-                self.MCODE3 = 0
-
-        Ignores blank line. start with index 0, so 4th line is read when writing 3.
-        (list_of_points.loc[ [3],[4,5] ] ) gives in the following format:
-
-                4           5
-        3  84.041486  138.819932 
-        """
-         
-
-        start_point = (list_of_points.loc[ [3],[4,5] ])
+        start_point = (list_of_points.loc[ [3],[4,5] ]) #[3]=row, [4,5]=coloumn represent Global Y, X
         end_point = (list_of_points.loc[ [22],[4,5] ])
 
         print(start_point)
         print(end_point) 
 
-def check_input_args():
-        try:
-        
-            args = rospy.myargv(argv=sys.argv) #pass arguments to ros
-            return args
-        #if len(args) != 2: #Check proper length. this python file path + name of the file to open
-        #except IndexError as error:
-        except RLException as e:
+##A function for checking the user input is correct.
+#By cheking world_name, if name exist, or if any input is given.
+#@param csv_path The str of csv_path
+#@param csv_file_name The str of csv name
+def checkInputArgs(csv_path, csv_file_name):
+        example_input = "Example of input: python3 tests_for_vector_mapper.py world_name:=W01_Base_Map"
 
-        #except rospy.ROSInterruptException:
-            error_message = """ ERROR: No file name provided. 
-            Example of input: python3 test_launch_file.py world_name:=W01_Base_Map point.csv"""
-            print(error_message)
-        #    sys.exit(1)
+        try:
+            
+            sys.argv[1] #Check if we have one input. If not, goes to "except IndexError as error"
+            myargv = rospy.myargv(argv=sys.argv)
+            path_name = csv_path + csv_file_name
+            if not os.path.isfile(path_name):
+                print("Error: Not a valid world name.")
+                print(example_input)
+                sys.exit(1)
+                raise rospy.exceptions.ROSInitException('exit')
+            
+            #Check proper length. this python file path and ignore name of the world
+            if len(myargv) != 1:
+                print("Error: Too many input.")
+                print(example_input)
+            
         
-            print(str(e), file=sys.stderr)
-            sys.exit(1)
+        except IndexError:
+            print("Please specify a world name. Right now world_name:=W01_Base_Map is used.")
+            print(example_input)
+        
+
+
 if __name__ == "__main__":
         
         rospy.init_node('vector_map_loader', anonymous=True) #Initializing ROS Node given nodename. 
@@ -71,19 +69,14 @@ if __name__ == "__main__":
         launch = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
         
         launch.start()
-        args = check_input_args()
-        
-        
-        args_dict = get_args(roslaunch_file)
+
+        args_dict = getArgs(roslaunch_file)
         csv_path = args_dict.get("VectorMap_Files_Folder")
-   
-        #/home/adeye/AD-EYE_Core/AD-EYE/Experiments/W01_Base_Map/Vector_Map_Files/point.csv
-        csv_file_name = args[1]  
-        read_csv_file(csv_path, csv_file_name)
-                 
+        csv_file_name = "point.csv"  
+
+        checkInputArgs(csv_path, csv_file_name)
+        readCsvFile(csv_path, csv_file_name)
         
         rospy.sleep(3)
         # 3 seconds later
         launch.shutdown()   
-
-
