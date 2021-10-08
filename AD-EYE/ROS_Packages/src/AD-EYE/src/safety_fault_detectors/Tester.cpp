9e8d3ca34d16fe7d19c16bde7068b9731c9ff0e7
maximesc@kth.se <maximesc@kth.se>
2021-10-08 15:13:26 +0200

    Created active nodes checker
    This fault detection will monitor if some nodes are still alive.

--- AD-EYE/ROS_Packages/src/AD-EYE/src/safety_fault_detectors/Tester.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/safety_fault_detectors/Tester.cpp
@@ -3,22 +3,41 @@
 //
 
 #include <iostream>
+#include <ros/ros.h>
+#include <unistd.h>
 
-#include "active_nodes_checker.h"
+#include "safety_fault_detectors/active_nodes_checker.h"
 
-void TestIncrement() {
-    ActiveNodeChecker active_node_checker(1,1,4,4);
-    std::cout << "active_node_checker value: " << active_node_checker.isFaulty() << std::endl;
+void TestIncrement(ros::NodeHandle nh) {
+    nh.setParam("/critical_nodes_level_one", std::vector<std::string>{"/non_existing_node"});
+    ActiveNodeChecker active_node_checker(1,1,4,4,1, nh);
+//    std::cout << "active_node_checker value: " << active_node_checker.isFaulty() << std::endl;
     active_node_checker.updateCounter();
     active_node_checker.updateCounter();
     active_node_checker.updateCounter();
     active_node_checker.updateCounter();
-    std::cout << "active_node_checker value: " << active_node_checker.isFaulty() << std::endl;
+//    std::cout << "active_node_checker value: " << active_node_checker.isFaulty() << std::endl;
+    assert(active_node_checker.isFaulty() == 0); // since the node to track does not exist, the module should return a fault
+
+}
+
+void TestDecrement(ros::NodeHandle nh) {
+    nh.setParam("/critical_nodes_level_one", std::vector<std::string>{"/TesterNode"});
+    ActiveNodeChecker active_node_checker(1,1,4,4,1, nh);
+//    std::cout << "active_node_checker value: " << active_node_checker.isFaulty() << std::endl;
     active_node_checker.updateCounter();
+    active_node_checker.updateCounter();
+    active_node_checker.updateCounter();
+    active_node_checker.updateCounter();
+    assert(active_node_checker.isFaulty() == 1); // since the node to track exists, the module should return no fault
+//    std::cout << "active_node_checker value: " << active_node_checker.isFaulty() << std::endl;
 
 }
 
 
-int main(void) {
-    TestIncrement();
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "TesterNode");
+    ros::NodeHandle nh;
+    TestIncrement(nh);
+    TestDecrement(nh);
 }
--- AD-EYE/ROS_Packages/src/AD-EYE/src/safety_fault_detectors/safety_fault_detector.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/safety_fault_detectors/safety_fault_detector.cpp
@@ -2,7 +2,7 @@
 // Created by adeye on 2021-10-08.
 //
 
-#include "safety_fault_detector.h"
+#include "safety_fault_detectors/safety_fault_detector.h"
 
 SafetyFaultDetector::SafetyFaultDetector(int increment_value, int decrement_value, int high_threshold, int low_threshold)
         : DECREMENT_VALUE_(decrement_value), HIGH_THRESHOLD_(high_threshold), INCREMENT_VALUE_(increment_value),
@@ -16,7 +16,7 @@ void SafetyFaultDetector::updateCounter() {
 }
 
 bool SafetyFaultDetector::isFaulty() {
-    return is_test_passing_;
+    return !is_test_passing_;
 }
 
 void SafetyFaultDetector::incrementAndSaturateCounter() {
