14d92df2b72feeb426eaa8d8cb5c33f11fa7e349
yfyu <yfyu@kth.se>
2021-10-14 16:47:24 +0200

    Added: generate random start/end position and error handling

--- Helper_Scripts/tests_for_vector_mapper.py
+++ Helper_Scripts/tests_for_vector_mapper.py
@@ -4,8 +4,8 @@ import pandas as pd
 import sys
 import os
 from roslaunch.config import load_config_default
-
-
+from numpy.random import randint
+from numpy.random import seed
 
 #Command line input: python3 tests_for_vector_mapper.py world_name:=W01_Base_Map
 
@@ -17,16 +17,35 @@ def getArgs(roslaunch_file):
         config = load_config_default(roslaunch_file, None, loader=loader, verbose=False, assign_machines=False)
         return loader.root_context.resolve_dict['arg']
 
+##A function that calls for random postion
+#@param Integer file_rows length.
+#return random start, end row value
+def randomPosition(file_rows):
+        seed(1)
+
+        random_start_row = randint(0, file_rows) #random values from 0 to file_rows.
+        random_end_row = randint(0, file_rows)
+        return random_start_row, random_end_row
+
+
 ##A function for reading point.csv file and returning its start and end point.
 #@param csv_path The str of csv_path
 #@param csv_file_name The str of csv name
 def readCsvFile(csv_path, csv_file_name):
 
         list_of_points = pd.read_csv(csv_path + csv_file_name, delimiter=',', header=None, skip_blank_lines=True,float_precision=None)
-
-        start_point = (list_of_points.loc[ [3],[4,5] ]) #[3]=row, [4,5]=coloumn represent Global Y, X
-        end_point = (list_of_points.loc[ [22],[4,5] ])
-
+        
+        file_rows = len(list_of_points)-1
+        
+        position_rows = randomPosition(file_rows)
+        
+        #Check if their distance if too close. If it is, call for new random position again.
+        while abs(position_rows[0] - position_rows[1]) <= 10:
+                position_rows = randomPosition(file_rows)
+        
+        start_point = (list_of_points.loc[ [position_rows[0]],[4,5] ]) #[3]=row, [4,5]=coloumn represent Global Y, X
+        end_point = (list_of_points.loc[ [position_rows[1]],[4,5] ])
+        
         print(start_point)
         print(end_point) 
 
@@ -59,9 +78,8 @@ def checkInputArgs(csv_path, csv_file_name):
             print(example_input)
         
 
-
-if __name__ == "__main__":
-        
+##Main function that initizalize node and corresponding launch file.
+def main():
         rospy.init_node('vector_map_loader', anonymous=True) #Initializing ROS Node given nodename. 
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
         roslaunch.configure_logging(uuid)
@@ -71,12 +89,20 @@ if __name__ == "__main__":
         launch.start()
 
         args_dict = getArgs(roslaunch_file)
-        csv_path = args_dict.get("VectorMap_Files_Folder")
+        csv_path = args_dict.get("VectorMap_Files_Folder") #Get the key value which is a path.
         csv_file_name = "point.csv"  
 
         checkInputArgs(csv_path, csv_file_name)
         readCsvFile(csv_path, csv_file_name)
-        
-        rospy.sleep(3)
-        # 3 seconds later
-        launch.shutdown()   
+
+        try:
+            launch.spin()
+        finally:
+            # After Ctrl+C, stop all nodes from running
+            launch.shutdown()
+
+if __name__ == "__main__":
+        main()
+
+          
+
