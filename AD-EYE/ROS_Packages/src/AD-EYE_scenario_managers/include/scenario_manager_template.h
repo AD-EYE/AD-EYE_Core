1c83c255fdac985c1f8f3ab846208ac4cae212d5
maximesc@kth.se <maximesc@kth.se>
2021-10-19 13:24:54 +0200

    Created the fault experiment scenario manager

--- AD-EYE/ROS_Packages/src/AD-EYE_scenario_managers/include/scenario_manager_template.h
+++ AD-EYE/ROS_Packages/src/AD-EYE_scenario_managers/include/scenario_manager_template.h
@@ -6,107 +6,121 @@
 
 class ScenarioManagerTemplate {
 
-    private:
-        bool experiment_started_ = false;
-        bool recording_started_ = false;
-        ros::Publisher stop_pub_;
+private:
+    bool experiment_started_ = false;
+    bool recording_started_ = false;
+    bool experiment_running_ = false;
+    bool recording_running_ = false;
+    ros::Publisher stop_pub_;
 
-    protected:
-        ros::NodeHandle nh_;
-        ros::Rate rate_;
+protected:
+    ros::NodeHandle nh_;
+    ros::Rate rate_;
 
-    public:
-        
-        ScenarioManagerTemplate(ros::NodeHandle nh, int frequency): nh_(nh), rate_(frequency)
+public:
+
+    ScenarioManagerTemplate(ros::NodeHandle nh, int frequency): nh_(nh), rate_(frequency)
+    {
+        stop_pub_ = nh_.advertise<std_msgs::Bool> ("/simulink/stop_experiment", 1);
+    }
+
+
+    /*!
+    * \brief Starts the recording
+    * \details This method only affect the state
+    */
+    virtual void startRecording() = 0;
+
+    /*!
+    * \brief Stops the recording
+    * \details This method only affect the state
+    */
+    virtual void stopRecording() = 0;
+
+    /*!
+    * \brief Getter to know if the recording has been started
+    */
+    virtual bool isRecordingRunning() {return recording_running_;};
+
+    /*!
+    * \brief Getter to know if the experiment has been started
+    */
+    virtual bool isExperimentRunning() {return experiment_running_;};
+
+    /*!
+    * \brief Getter to know if the recording has been started
+    */
+    virtual bool hasRecordingStarted() {return recording_started_;};
+
+    /*!
+    * \brief Getter to know if the experiment has been started
+    */
+    virtual bool hasExperimentStarted() {return experiment_started_;};
+
+    /*!
+    * \brief Sends a message to stop the experiment
+    * \details This method sends a ROS message that once received by Simulink should stop the simulation
+    */
+    virtual void stopExperiment() = 0;
+    virtual void startExperiment() = 0;
+
+    void stopSimulinkExperiment() {
+        std_msgs::Bool msg;
+        msg.data = true;
+        stop_pub_.publish(msg);
+    }
+
+    virtual bool startRecordingConditionFulfilled() = 0;
+    virtual bool stopRecordingConditionFulfilled() = 0;
+    virtual bool startExperimentConditionFulfilled() = 0;
+    virtual bool stopExperimentConditionFulfilled() = 0;
+
+    /*!
+    * \brief Contains the main loop, which runs the state machine of the scenario manager
+    */
+    void run()
+    {
+        while (ros::ok())
         {
-            stop_pub_ = nh_.advertise<std_msgs::Bool> ("/simulink/stop_experiment", 1);
-        }
-
-
-        /*!
-        * \brief Starts the recording
-        * \details This method only affect the state
-        */
-        virtual void startRecording() = 0;
-
-        /*!
-        * \brief Stops the recording
-        * \details This method only affect the state
-        */
-        virtual void stopRecording() = 0;
-
-        /*!
-        * \brief Getter to know if the recording is started
-        */
-        virtual bool hasRecordingStarted() {return recording_started_;};
-
-        /*!
-        * \brief Getter to know if the experiment is started
-        */
-        virtual bool hasExperimentStarted() {return experiment_started_;};
-
-        /*!
-        * \brief Sends a message to stop the experiment
-        * \details This method sends a ROS message that once received by Simulink should stop the simulation
-        */
-        virtual void stopExperiment() = 0;
-        virtual void startExperiment() = 0;
-
-        void stopSimulinkExperiment() {
-            std_msgs::Bool msg;
-            msg.data = true;
-            stop_pub_.publish(msg);
-        }
-
-        virtual bool startRecordingConditionFulfilled() = 0;
-        virtual bool stopRecordingConditionFulfilled() = 0;
-        virtual bool startExperimentConditionFulfilled() = 0;
-        virtual bool stopExperimentConditionFulfilled() = 0;
-
-        /*!
-        * \brief Contains the main loop, which runs the state machine of the scenario manager
-        */
-        void run() 
-        {
-            while (ros::ok())
+            if(hasExperimentStarted())
             {
-                if(hasExperimentStarted())
-                {
-                    if(!hasRecordingStarted()){
-                        if(startRecordingConditionFulfilled())
-                        {
-                            recording_started_ = true;
-                            startRecording();
-                        }
-                    }
-                    else {
-                        if(stopRecordingConditionFulfilled())
-                        {
-                            recording_started_ = false;
-                            stopRecording();
-                        }
-                    }
-
-                    if(stopExperimentConditionFulfilled()) // stop condition of the experiment
+                if(!hasRecordingStarted()){
+                    if(startRecordingConditionFulfilled())
                     {
-                        experiment_started_ = false;
-                        stopExperiment();
-                        stopSimulinkExperiment();
+                        recording_started_ = true;
+                        recording_running_ = true;
+                        startRecording();
                     }
                 }
-                else
-                {
-                    if(startExperimentConditionFulfilled()) // start condition of the experiment
+                else {
+                    if(stopRecordingConditionFulfilled())
                     {
-                        experiment_started_ = true;
-                        startExperiment();
-
+                        recording_running_ = false;
+                        stopRecording();
                     }
                 }
-                ros::spinOnce();
-                rate_.sleep();
+
+                if(stopExperimentConditionFulfilled() && !isRecordingRunning()) // stop condition of the experiment
+                {
+                    experiment_running_ = false;
+                    stopExperiment();
+                    stopSimulinkExperiment();
+                }
             }
+            else
+            {
+                if(startExperimentConditionFulfilled()) // start condition of the experiment
+                {
+                    experiment_started_ = true;
+                    experiment_running_ = true;
+                    startExperiment();
+
+                }
+            }
+            ros::spinOnce();
+            rate_.sleep();
         }
+    }
 
 
 };
---
+++ AD-EYE/ROS_Packages/src/AD-EYE_scenario_managers/src/fault_experiment_manager.cpp
@@ -0,0 +1,246 @@
+#include "ros/ros.h"
+#include <scenario_manager_template.h>
+
+
+#include <std_msgs/Bool.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <autoware_msgs/LaneArray.h>
+#include "op_planner/PlannerH.h"
+#include "op_ros_helpers/op_ROSHelpers.h"
+#include <cstdio>
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <boost/filesystem.hpp>
+
+/*!
+ * \brief Scenario1 node : Manages start and stop of the recording & experiment for Scenario1
+ */
+class FaultExperimentManager: public ScenarioManagerTemplate {
+
+private:
+    ros::Subscriber speed_sub_;
+    ros::Subscriber sub_gnss_;
+    ros::Subscriber sub_autoware_global_plan_;
+    bool has_received_global_plan_ = false;
+    ros::Publisher pub_autoware_goal_;
+    float ego_speed_ = 0.0;
+    geometry_msgs::Pose ego_pose_;
+    std::vector<std::vector<PlannerHNS::WayPoint>> autoware_global_path_;
+
+    const double SPED_STOP_THRESHOLD_ = 0.05;
+
+    const std::string RESULT_FILE = "/home/adeye/Experiment_Results/FaultExperiments.csv";
+
+    void speedCallback(geometry_msgs::TwistStamped msg)
+    {
+        ego_speed_ = msg.twist.linear.x;
+    }
+
+
+    /*!
+     * \brief Gnss Callback : Called when the gnss information has changed.
+     * \param msg A smart pointer to the message from the topic.
+     * \details Updates gnss information of the vehicle position.
+     */
+    void gnssCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+    {
+        geometry_msgs::PoseStamped gnss = *msg;
+        ego_pose_ = gnss.pose;
+    }
+
+
+    /*!
+     * \brief Autoware global plan Callback : Called when the global plan from autoware has changed.
+     * \param msg A smart pointer to the message from the topic.
+     */
+    void autowareGlobalPlanCallback(const autoware_msgs::LaneArrayConstPtr& msg)
+    {
+        if(msg->lanes.size() > 0)
+        {
+            std::vector<PlannerHNS::WayPoint> m_temp_path;
+            for(unsigned int i = 0 ; i < msg->lanes.size(); i++)
+            {
+                PlannerHNS::ROSHelpers::ConvertFromAutowareLaneToLocalLane(msg->lanes.at(i), m_temp_path);
+                PlannerHNS::PlanningHelpers::CalcAngleAndCost(m_temp_path);
+                autoware_global_path_.push_back(m_temp_path);
+            }
+        }
+        has_received_global_plan_ = true;
+    }
+
+    void publishOriginalGoal()
+    {
+        geometry_msgs::PoseStamped newGoal;
+        newGoal.header.frame_id = "world";
+        newGoal.pose.position.x = -24;
+        newGoal.pose.position.y = 113;
+        newGoal.pose.position.z = 0;
+        newGoal.pose.orientation.x = 0;
+        newGoal.pose.orientation.y = 0;
+        newGoal.pose.orientation.z = -0.707;
+        newGoal.pose.orientation.w = 0.707;
+        pub_autoware_goal_.publish(newGoal);
+    }
+
+public:
+    /*!
+    * \brief Constructor
+    * \param nh ros::NodeHandle initialized in the main function.
+    * \param frequency The frequency at which the main loop will be run.
+    * \details Initializes the node and its components such the as subscribers.
+    */
+    FaultExperimentManager(ros::NodeHandle nh, int frequency): ScenarioManagerTemplate(nh, frequency)
+    {
+        speed_sub_ = ScenarioManagerTemplate::nh_.subscribe("/current_velocity", 10, &FaultExperimentManager::speedCallback, this);
+        sub_gnss_ = nh_.subscribe<geometry_msgs::PoseStamped>("/ground_truth_pose", 100, &FaultExperimentManager::gnssCallback, this);
+        sub_autoware_global_plan_ = nh.subscribe("/lane_waypoints_array", 1, &FaultExperimentManager::autowareGlobalPlanCallback, this);
+        pub_autoware_goal_ = nh_.advertise<geometry_msgs::PoseStamped>("/adeye/overwriteGoal", 1, true);
+
+        // ScenarioManagerTemplate::nh_.param<float>("/simulink/rain_intensity", rain_intensity_, 0.0);
+    }
+
+
+    int getExpIndex()
+    {
+        std::ifstream file(RESULT_FILE);
+        std::string line;
+        int rows = 0;
+        while (getline(file, line))
+            rows++;
+        return rows;
+    }
+
+    /*!
+    * \brief Starts the experiment, contains what needs to be done during the experiment
+    */
+    void startExperiment() override
+    {
+        std::cout << "FaultExperimentManager: started experiment" << std::endl;
+
+        switch(getExpIndex() % 4)
+        {
+            case 0:
+                killTwistFilter();
+                break;
+            case 1:
+                killSSDCamera1();
+                break;
+            case 2:
+                killSSDCamera2();
+                break;
+            case 3:
+                killGoalSequencer();
+                break;
+        }
+    }
+
+    /*!
+    * \brief Stops the experiment, contains the post experiment cleanups
+    */
+    void stopExperiment() override
+    {
+        std::cout << "FaultExperimentManager: stopped experiment" << std::endl;
+    }
+
+    /*!
+    * \brief Starts the experiment relevant recording
+    */
+    void startRecording() override
+    {
+        std::cout << "FaultExperimentManager: started recording" << std::endl;
+        has_received_global_plan_ = false;
+        publishOriginalGoal();
+    }
+
+    /*!
+    * \brief Stops the the experiment relevant recording
+    */
+    void stopRecording() override
+    {
+        std::cout << "FaultExperimentManager: stopped recording" << std::endl;
+
+        PlannerHNS::WayPoint pose_wp(ego_pose_.position.x, ego_pose_.position.y, 0, 0);
+        int current_wp_index = PlannerHNS::PlanningHelpers::GetClosestNextPointIndexFastV2(autoware_global_path_.front(), pose_wp, 0);
+        double remaining_traj_length = PlannerHNS::PlanningHelpers::GetDistanceOnTrajectory_obsolete(autoware_global_path_.front(), current_wp_index, autoware_global_path_.front().back());
+        std::cout << remaining_traj_length << " meters remaining to goal" << std::endl;
+        std::ofstream myfile;
+        myfile.open(RESULT_FILE, std::ios::app);
+        myfile << remaining_traj_length << "\n";
+        myfile.close();
+    }
+
+
+    void killSSDCamera1()
+    {
+        system("rosnode kill /camera_1/vision_ssd_detect");
+    }
+    void killSSDCamera2()
+    {
+        system("rosnode kill /camera_2/vision_ssd_detect");
+    }
+    void killTwistFilter()
+    {
+        system("rosnode kill /twist_filter");
+    }
+    void killGoalSequencer()
+    {
+        system("rosnode kill /goalSequencer");
+    }
+
+
+
+
+
+
+
+    /*!
+    * \brief Checks if the conditions to start recording are met
+    */
+    bool startRecordingConditionFulfilled() override
+    {
+        return (hasExperimentStarted() && ego_speed_ < SPED_STOP_THRESHOLD_);
+    }
+
+    /*!
+    * \brief Checks if the conditions to stop recording are met
+    */
+    bool stopRecordingConditionFulfilled() override
+    {
+        return (ego_speed_ < SPED_STOP_THRESHOLD_);
+//        return (has_received_global_plan_);
+    }
+
+    /*!
+    * \brief Checks if the conditions to start the experiment are met
+    */
+    bool startExperimentConditionFulfilled() override
+    {
+        return (ego_speed_ > 1 && ego_pose_.position.x > 65 + 10 * (int) (getExpIndex() / 4));
+    }
+
+    /*!
+    * \brief Checks if the conditions to stop the experiment are met
+    */
+    bool stopExperimentConditionFulfilled() override
+    {
+
+        return (ego_speed_ < SPED_STOP_THRESHOLD_);
+    }
+
+
+
+
+};
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "FaultExperimentManager");
+    ros::NodeHandle private_nh("~");
+    FaultExperimentManager fem(private_nh, 20);
+    fem.run();
+}
