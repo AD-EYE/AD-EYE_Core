ddf4b62791191c51be05fe4d45dead742aa56f5c
maximesc@kth.se <maximesc@kth.se>
2021-10-19 13:28:40 +0200

    Updated the source script for TA
    Before, TA would not be able to start ssd_detect because of missing library paths.

--- AD-EYE/ROS_Packages/src/AD-EYE/sh/launchManagerFile.sh
+++ AD-EYE/ROS_Packages/src/AD-EYE/sh/launchManagerFile.sh
@@ -3,5 +3,3 @@
 export DISPLAY=:0
 source $HOME/AD-EYE_Core/AD-EYE/ROS_Packages/src/AD-EYE/sh/sourceROSAndPackages.sh
 roslaunch adeye manager_simulation.launch test_automation:=true>/dev/null 2>&- & disown
-echo "error" > file=sys.stderr
-exit
