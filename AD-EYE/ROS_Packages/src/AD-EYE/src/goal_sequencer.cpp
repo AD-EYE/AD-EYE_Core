bb9fe113695a602b90ef92b4474e19ba69ce1ca6
yfyu <yfyu@kth.se>
2022-01-19 16:39:17 +0100

    Updated according to the first PR

--- AD-EYE/ROS_Packages/src/AD-EYE/src/goal_sequencer.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/goal_sequencer.cpp
@@ -9,16 +9,17 @@
 #include <cmath>
 #include <iostream>
 #include <tf/transform_broadcaster.h>
-
+#include <stdlib.h>
+#include <tuple>
 /*
 Example on how to run the program:
 
-Terminal1 type: rosrun adeye goalSequencer
-Terminal2 type: roslaunch adeye my_map.la world_name:=W01_Base_Map
+Terminal1 type: catkin_make, then: rosrun adeye goalSequencer
+Terminal2 type: roslaunch adeye my_map.launch world_name:=W01_Base_Map, or: roslaunch adeye my_map.launch
 Terminal3 type one of the below:
 rostopic pub /adeye/goals geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 0.0, y: 0.0, z: 49.156}, orientation: {z: 0.416, w: 0.909}}}'
 rostopic pub /adeye/goals geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 137.998049077445, y: 121.23713661199696, z: 49.156}, orientation: {z: 0.760, w: 0.650}}}'
-rostopic pub /adeye/goalseometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 120.0, y: 130.0, z: 1.0}, orientation: {w: 1.0}}}'
+rostopic pub /adeye/goals geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 140.82, y: 62.27, z: 49.156}, orientation: {z: 0, w: 1}}}'
 rostopic pub /current_behavior geometry_msgs/TwistStamped "{header: {stamp: now, frame_id: "map"}, twist: {linear: {x: 1.0, y: 2.0, z: 3.0}, angular: {x: 1.0,y: 13.0,z: 1.0}}}"
 */
 
@@ -60,6 +61,7 @@ private:
     bool received_first_goal_ = false;
     bool received_vector_mapper_ = false;
 
+    int file_size_;
     // Autoware behavior state status
     double autoware_behavior_state_;
 
@@ -96,38 +98,22 @@ private:
      */
     void storeGoalCoordinatesCallback(const geometry_msgs::PoseStamped::ConstPtr &msg)
     {
-        // Store the first goal if the goal queue is empty and boolean for upcoming goal is false
-
-        if (goal_coordinates_.empty() && received_vector_mapper_ && checkGoalValidity(msg -> pose.position.x, msg -> pose.position.y, msg -> pose.orientation) ) 
-        {   
-            // Store the first real-world map goal coordinates  
-            goal_coordinates_.push(*msg);       
-
-            ROS_INFO("The first goal has been received. Position:- x = %lf, y = %lf, z = %lf",
-                goal_coordinates_.front().pose.position.x, goal_coordinates_.front().pose.position.y, goal_coordinates_.front().pose.position.z);
-            
-            // Boolean for receiving the first goal
-            received_first_goal_ = true;
-            
-        }
-
-        else if (received_vector_mapper_ == false)
+        if(!received_vector_mapper_)
         {
             ROS_INFO("No vector map received, please close the goal publisher and start vector map in another terminal by typing for example:");
             ROS_INFO("roslaunch adeye my_map.launch world_name:=W01_Base_Map");
         }
-
-        else if (goal_coordinates_.empty() == false && checkGoalValidity(msg -> pose.position.x, msg -> pose.position.y, msg -> pose.orientation))
+        // Stores the goal if it is valid
+        else if(isGoalValid(msg -> pose.position.x, msg -> pose.position.y,  msg -> pose.position.z, msg -> pose.orientation))
         {
-            goal_coordinates_.push(*msg);
-
-            // Print the new goal positions
-            ROS_INFO("The next goal has been received. Position:- x = %lf, y = %lf, z = %lf",goal_coordinates_.back().pose.position.x, goal_coordinates_.back().pose.position.y, goal_coordinates_.back().pose.position.z );
-        } 
+            // Store the real-world map goal coordinates  
+            goal_coordinates_.push(*msg);       
+        }
         else
         {
             ROS_INFO("The received goal is not valid");
         }
+
     }
     
     /*!
@@ -150,7 +136,7 @@ private:
         // Local planner value
         std_msgs::Int32 local_planner;
 
-        if (should_update_global_planner_)
+        if(should_update_global_planner_)
         {
             // Update the local planner for the next goal
             local_planner.data = 1;
@@ -170,6 +156,8 @@ private:
         ROS_INFO("Recived vector mapper");
         received_vector_mapper_ = true;
         vector_map_data_ = *msg;
+        //extra -1 since we need to calculate data point of i+1 in the loop
+        file_size_ = vector_map_data_.data.size() - 2; 
     }
     
 
@@ -190,90 +178,164 @@ private:
     double getOrientation(const double& x_one, const double& x_two, const double& y_one, const double& y_two)
     {
         double angle = atan2(y_two-y_one, x_two-x_one);
-        if (angle < 0)
+        if(angle < 0)
         {
             angle += 2*M_PI;
         }
     return angle*(180.0/M_PI);
     }
 
+
     /*!
-    * \brief Help function for the function checkGoalValidity
-    * \param x, y is the position of the received goal. file_size_ is the number of lines in the point.csv file that contains vector map data.
-    *        yaw_angle is the angle of the received goal  
-    * \details Loop through the vector map to see if the received goal position is close or within the vector map
+    * \brief Finds the closest distance to a VM point
+    * \param x, y is the position of the received goal
+    * \details Loop through each point of the vector map and calculate its distance to x, y 
+    *          When the shortest distance is found, we return the distance and index to the closest VM point
     */
-    bool ifInVectorMapp(const double& x, const double& y, const int& file_size_, const double& yaw_angle)
+    std::tuple<double, int> findClosestVMDistance(const double& x, const double& y)
     {
-        for (unsigned int i = 0; i < file_size_; i++)
+        double closest_vm_distance = 0.0;
+        double vm_distance;
+        int index;
+        for(unsigned int i = 0; i < file_size_; i++)
         {
-            //Check if the goal is close or within the vector map
-            if (getDistance(x, vector_map_data_.data[i].ly, y, vector_map_data_.data[i].bx) <= VMAP_DISTANCE_TOLERANCE_ ) 
+            vm_distance = getDistance(x, vector_map_data_.data[i].ly, y, vector_map_data_.data[i].bx);
+
+            //Update the point and distance until we find the shortest distance-
+            if(closest_vm_distance == 0.0 || vm_distance < closest_vm_distance)
             {
-                //Calcluate the orientation of the vector map
-                double vmap_orientation = getOrientation(vector_map_data_.data[i].ly, vector_map_data_.data[i+1].ly, vector_map_data_.data[i].bx, vector_map_data_.data[i+1].bx); 
-                
-                //Check if the difference between goal and vector map orientation is small enoguh. 
-                if (yaw_angle-vmap_orientation < ORIENTAION_TOLERANCE_)
-                {
-                    return true;
-                }
-                /*Continue the for loop to see if there exist other points in the vector map 
-                with small enough orientation difference with the received goal*/
-                else
-                {
-                    continue;
-                }
+                closest_vm_distance = vm_distance;
+                index = i;
             }
         }
-        ROS_INFO("Recieved goal is not within the vector map or goal orientation difference from the vector map is too large");
-        return false;
+        return std::make_tuple(closest_vm_distance, index); 
     }
 
     /*!
+    * \brief Finds the orientation of a certain VM point given the index.
+    * \param i, is the index to the desired VM point.
+    */
+    double findOrientationOfVM(const int& i)
+    {
+        double vmap_orientation = getOrientation(vector_map_data_.data[i].ly, vector_map_data_.data[i+1].ly, vector_map_data_.data[i].bx, vector_map_data_.data[i+1].bx); 
+        return vmap_orientation;
+    }
+
+    /*!
+    * \brief Check if the goal is close to the VM
+    * \param distance is the distance between goal and VM
+    */
+    bool isPointCloseToVM(const double& distance)
+    {
+        if(distance <= VMAP_DISTANCE_TOLERANCE_)
+        {
+            return true; 
+        }
+        else
+        {
+            ROS_INFO("Recieved goal is not within the vector map");
+            return false;
+        }
+    }
+    
+    /*!
+    * \brief Check if the difference of VM orientation and yaw angle is small enough
+    * \param vmap_orientation is the orientation of the VM point
+    * \param yaw_angle is the orientation of the goal
+    */
+    bool isPointAlignedWithVM(const int& vmap_orientation, const double& yaw_angle)
+    {
+        //Check if the difference between goal and vector map orientation is small enough. 
+        if(abs(yaw_angle-vmap_orientation < ORIENTAION_TOLERANCE_))
+        {
+            return true;
+        }
+        else
+        {
+            ROS_INFO("Recieved orientation difference from the vector map is too large");
+            return false;
+        }
+
+    }
+
+    /*!
+    * \brief Checking the distance and orientation between two consequtive goals 
+    * \param distance_between_goals is the distance between two consequtive goals
+    * \param angle_between_goals_headings is the orientation between two consequtive goals 
+    */
+                
+    bool areGoalsDifferentEnough(const double& distance_between_goals, const double& angle_between_goals_headings)
+    {
+        //Goals can be some degrees apart from each other even though they are close to each other.
+        if(distance_between_goals > GOAL_DISTANCE_TOLERANCE_ || angle_between_goals_headings > ORIENTAION_TOLERANCE_)
+        {
+            return true;
+        }
+        else
+        {
+            ROS_INFO("Distance or orientation between current goal and previous goal is not large enough");
+            return false;
+        }
+
+    }
+
+    
+    /*!
      * \brief Boolean function that checks the validity of the received goal
-     * \param x, y and goal_orientation are the are data provided by the goals
+     * \param x, y,z and goal_orientation are the are data provided by the goals
      * \details Check if the received goal exist in the vector map and their orientation difference 
      *          Also check the distance and orientation with the pervious stored goal.
      */
-    bool checkGoalValidity(const double& x, const double& y, const geometry_msgs::Quaternion& goal_orientation)
+    bool isGoalValid(const double& x, const double& y, const double& z, const geometry_msgs::Quaternion& goal_orientation)
     {
-        //extra -1 since we need to calculate data point of i+1 in the loop
-        int file_size_ = vector_map_data_.data.size() - 2; 
+        bool is_goal_valid;
+        double closest_vm_distance;
+        int index;
 
         //Transform quaternion to euler angles
         double yaw_angle = tf::getYaw(goal_orientation)*(180.0/M_PI);
 
-        //Check goal validity for the first goal
-        //If it is within or close to the vector map and if the orientation is similar.
-        if (goal_coordinates_.empty())
-        {
-            
-            return ifInVectorMapp(x, y, file_size_, yaw_angle);
-            
-        }
-        //Check the goal validity when there exist more than one goal.
-        else 
-        {
+        //Get the closest distance to a point of VM and corresponding index of that point.
+        std::tie(closest_vm_distance, index) = findClosestVMDistance(x, y);
+        
+        //Get the orientation of the closest point of the VM
+        double vmap_orientation = findOrientationOfVM(index);
 
+        //Check goal validity for the first goal by checking if the goal is close enough to the VM with large orientation.
+        is_goal_valid = isPointCloseToVM(closest_vm_distance) && isPointAlignedWithVM(vmap_orientation, yaw_angle);
+        
+        //If the received goal is the first goal
+        if(goal_coordinates_.empty())
+        {
+            ROS_INFO("The first goal has been received. Position:- x = %lf, y = %lf, z = %lf",
+            x, y, z);
+            
+            // Boolean for receiving the first goal
+            received_first_goal_ = true;
+        }
+
+        else
+        {
             //Distance between two consecutive goals
-            double goal_distance = getDistance(goal_coordinates_.back().pose.position.x, x, goal_coordinates_.back().pose.position.y, y); 
+            double distance_between_goals = getDistance(goal_coordinates_.back().pose.position.x, x, goal_coordinates_.back().pose.position.y, y); 
+            
             //yaw angle of previous goal
             double previous_yaw_angle = tf::getYaw(goal_coordinates_.back().pose.orientation)*(180.0/M_PI); 
+            double angle_between_goals_headings = yaw_angle - previous_yaw_angle;
+            
+            //Check the goal validity when there exist more than one goal.
+            is_goal_valid = is_goal_valid && areGoalsDifferentEnough(distance_between_goals, angle_between_goals_headings);
+            // Print the new goal positions
+            ROS_INFO("The next goal has been received. Position:- x = %lf, y = %lf, z = %lf",
+            x, y, z );
+        }
 
-            //Checking the distance and orientation between two consequtive goals. 
-            //They can be some degrees apart from each other even though they are close to each other. 
-            if (goal_distance > GOAL_DISTANCE_TOLERANCE_ || yaw_angle - previous_yaw_angle > ORIENTAION_TOLERANCE_)
-            {
-                return ifInVectorMapp(x, y, file_size_, yaw_angle);
-            }
-            else
-            {
-                ROS_INFO("Distance or orientation between current goal and previous goal is not large enough");
-            }
-
-        }        
+        return is_goal_valid;
     }
+
+
+
+
 public:
     /*!
      * \brief Constructor
@@ -296,12 +358,12 @@ public:
     void run() 
     {
         // Publish the coordinates of the next goal once the car enters in the end state.
-        while (nh_.ok())
+        while(nh_.ok())
         {
             ros::spinOnce();
             
             // Condition for the first goal
-            if (received_first_goal_) 
+            if(received_first_goal_) 
             {      
                 // Publish the first goal 
                 pub_goal_.publish(goal_coordinates_.front());
@@ -314,7 +376,7 @@ public:
             
             // Condition for next (upcoming) goals
             // Publish the next goal when the car enters in end state (end state = 13.0) and condition for removing the previous goal and setting up the next goal 
-            if (autoware_behavior_state_ == END_STATE_ && !has_global_planner_and_goal_been_reset_ && goal_coordinates_.size() >= MINIMUM_GOALS_)
+            if(autoware_behavior_state_ == END_STATE_ && !has_global_planner_and_goal_been_reset_ && goal_coordinates_.size() >= MINIMUM_GOALS_)
             {
                 // Boolean for clearing the goal list in autoware op_global_planner
                 std_msgs::Bool clear_goal_list;
@@ -337,7 +399,7 @@ public:
                 // Update the planner and goal boolean for end state
                 has_global_planner_and_goal_been_reset_ = true;
             }
-            else if (autoware_behavior_state_ == FORWARD_STATE_) // The planner and goal boolean is set to false after publishing the goal and planner (forward state = 2.0)
+            else if(autoware_behavior_state_ == FORWARD_STATE_) // The planner and goal boolean is set to false after publishing the goal and planner (forward state = 2.0)
             {
                 has_global_planner_and_goal_been_reset_ = false;
             }
