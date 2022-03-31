2bd21f2360c11628c53661e9c7f0718928d5d9c6
sc-maxime <79856129+sc-maxime@users.noreply.github.com>
2022-03-31 10:54:48 +0200

    Cleaned experiment startup code

--- AD-EYE/TA/run_fault_experiments.m
+++ AD-EYE/TA/run_fault_experiments.m
@@ -1,22 +1,5 @@
-% nb_runs = 240;
-% nb_runs = 480;
-% 
-% folderNames = ["W15_Road_Side_Parking\W15_Road_Side_Parking_no_camera"];
-% prescanExperimentTemplates = ["W15_Road_Side_Parking"];
-% TARosParametersTemplates = ["AutowareConfigTemplate.xlsx"];
-% TASimulinkParametersTemplates = ["SimulinkEmptyConfig.xlsx"];
-% TASimulinkParametersTemplates = repelem(TASimulinkParametersTemplates, nb_runs);
-% 
-% egoNameArray = ["BMW_X5_SUV_1"];
-% tagsConfigs = [""];
-% SSHConfig = "Configurations/SSHConfig.csv";
-% 
-% TACombinations(folderNames, prescanExperimentTemplates, egoNameArray, TARosParametersTemplates, TASimulinkParametersTemplates, tagsConfigs, SSHConfig)
-% 
-% 
-% 
-% TA('Configurations/TAOrder.xlsx', 1, 5000, 1, false)
 
+SSHConfig = "Configurations/SSHConfig.csv";
 
 [device, hostname] = getSSHDevice(SSHConfig); % Cofiguration of the Ubuntu computer
 
