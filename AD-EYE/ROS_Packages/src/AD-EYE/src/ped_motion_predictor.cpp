b9211cc6d02dbb8afcbccc2d74e309860ecb2474
akadhim <akadhim@kth.se>
2022-04-01 14:19:13 +0200

    removing the vm and the mapping transformation

--- AD-EYE/ROS_Packages/src/AD-EYE/src/ped_motion_predictor.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/ped_motion_predictor.cpp
@@ -6,11 +6,11 @@
 #include <autoware_msgs/DetectedObjectArray.h>
 #include <vector>
 #include <visualization_msgs/Marker.h>
-#include <vector_map_msgs/LaneArray.h>
-#include <vector_map_msgs/NodeArray.h>
-#include <vector_map_msgs/PointArray.h>
-#include <tf2_ros/transform_listener.h>
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+//#include <vector_map_msgs/LaneArray.h>
+//#include <vector_map_msgs/NodeArray.h>
+//#include <vector_map_msgs/PointArray.h>
+//#include <tf2_ros/transform_listener.h>
+//#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 /* The node is predicting the position of a detected pedestrain within the next 5 steps.
 Also, it visualizes the positions with sphere markers.
@@ -22,12 +22,12 @@ private:
   //Node, publisher and subscribers 
   ros::NodeHandle &n_;
   ros::Subscriber sub_objects;
-  ros::Subscriber sub_lane;
-  ros::Subscriber sub_line;
-  ros::Subscriber sub_point;
+ // ros::Subscriber sub_lane;
+ // ros::Subscriber sub_line;
+ // ros::Subscriber sub_point;
   ros::Publisher marker_pub;
-  tf2_ros::Buffer tf_buffer_;
-  std::string target_frame_id_;
+ // tf2_ros::Buffer tf_buffer_;
+ // std::string target_frame_id_;
         
   // x, y postions and velocties, time and marking variables  
   double position_x {0};
@@ -45,27 +45,27 @@ calculation function in order to calculate the next 5 steps positions.
 */
 void trackedObj_Callback(const autoware_msgs::DetectedObjectArray::ConstPtr& msg)
 {
-  geometry_msgs::PoseStamped pose_stamped_in; 
-  geometry_msgs::PoseStamped pose_stamped_out;
-  target_frame_id_ = "map";
-  removeMarkers();
+  //geometry_msgs::PoseStamped pose_stamped_in; 
+  //geometry_msgs::PoseStamped pose_stamped_out;
+  //target_frame_id_ = "map";
+  removeMarkers(); // calling a function to remove the markares 
   count_id = 0;           
           
   for (int i = 0; i < msg->objects.size(); i++) 
   {
     if (msg->objects.at(i).label!="unknown" && msg-> objects.at(i).pose.position.x != 0) 
     {
-      pose_stamped_in.header = msg->objects.at(i).header;
-      pose_stamped_in.pose = msg->objects.at(i).pose;
-      tf_buffer_.transform(pose_stamped_in, pose_stamped_out, target_frame_id_, ros::Duration(0));  
-      std::cout << "map_pose_in " << pose_stamped_in.pose << "map_pose_out "<< pose_stamped_out.pose <<std::endl;
+      //pose_stamped_in.header = msg->objects.at(i).header;
+      //pose_stamped_in.pose = msg->objects.at(i).pose;
+      //tf_buffer_.transform(pose_stamped_in, pose_stamped_out, target_frame_id_, ros::Duration(0));  
+      //std::cout << "map_pose_in " << pose_stamped_in.pose << "map_pose_out "<< pose_stamped_out.pose <<std::endl;
 
       position_x = msg-> objects.at(i).pose.position.x;
       position_y = msg-> objects.at(i).pose.position.y;
       velocity_x = msg-> objects.at(i).velocity.linear.x;
       velocity_y = msg-> objects.at(i).velocity.linear.y; 
       std::cout << "label : " << msg->objects.at(i).label << std::endl; 
-      calculation(position_x, position_y, velocity_x, velocity_y);  // call calculation function
+      calculation(position_x, position_y, velocity_x, velocity_y);  // calling a calculation function
     }
   }
  
@@ -77,9 +77,9 @@ public:
   {
     sub_objects = n_.subscribe("/fusion/objects", 1, &PedestrainMotionPredictor::trackedObj_Callback, this);
     marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-    sub_lane = n_.subscribe<vector_map_msgs::LaneArray>("/vector_map_info/lane", 1, &PedestrainMotionPredictor::vectorMapLaneCallback, this);
-    sub_line = n_.subscribe<vector_map_msgs::NodeArray>("/vector_map_info/node", 1, &PedestrainMotionPredictor::vectorMapNodeCallback, this);
-    sub_point = n_.subscribe<vector_map_msgs::PointArray>("/vector_map_info/point", 1, &PedestrainMotionPredictor::vectorMapPointCallback, this);
+    //sub_lane = n_.subscribe<vector_map_msgs::LaneArray>("/vector_map_info/lane", 1, &PedestrainMotionPredictor::vectorMapLaneCallback, this);
+    //sub_line = n_.subscribe<vector_map_msgs::NodeArray>("/vector_map_info/node", 1, &PedestrainMotionPredictor::vectorMapNodeCallback, this);
+    //sub_point = n_.subscribe<vector_map_msgs::PointArray>("/vector_map_info/point", 1, &PedestrainMotionPredictor::vectorMapPointCallback, this);
   }
      
 /* Calculation function accepts x & y positions and velocities as parameters from the callback function.
@@ -160,29 +160,29 @@ void removeMarkers()
   marker_pub.publish(marker);
 } 
 
-void vectorMapLaneCallback(const vector_map_msgs::LaneArray::ConstPtr &msg)
-  {   
-    for (int i = 0; i < msg->data.size(); i++) 
-      {
+//void vectorMapLaneCallback(const vector_map_msgs::LaneArray::ConstPtr &msg)
+ // {   
+   // for (int i = 0; i < msg->data.size(); i++) 
+     // {
        //std::cout << "Lane starting node : " << msg->data.at(1).bnid << std::endl;
-      }
-  }
+     // }
+  //}
 
-void vectorMapNodeCallback(const vector_map_msgs::NodeArray::ConstPtr &msg)
-    {   
-      for (int i = 0; i < msg->data.size(); i++) 
-      {
+//void vectorMapNodeCallback(const vector_map_msgs::NodeArray::ConstPtr &msg)
+  //  {   
+    //  for (int i = 0; i < msg->data.size(); i++) 
+     // {
       // std::cout << "Node : " << msg->data.at(i).nid << std::endl;
-      }  
-    }
+     // }  
+   // }
 
-void vectorMapPointCallback(const vector_map_msgs::PointArray::ConstPtr &msg)
-    {   
-     for (int i = 0; i < msg->data.size(); i++) 
-      {
-       std::cout << "Global X : " << msg->data.at(i).bx << " , " << " Global Y : " << msg->data.at(i).ly << std::endl;
-      }    
-    }
+//void vectorMapPointCallback(const vector_map_msgs::PointArray::ConstPtr &msg)
+  //  {   
+    // for (int i = 0; i < msg->data.size(); i++) 
+      //{
+       //std::cout << "Global X : " << msg->data.at(i).bx << " , " << " Global Y : " << msg->data.at(i).ly << std::endl;
+      //}    
+   // }
 
 
 void run()
