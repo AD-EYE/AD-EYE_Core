63b751747add29688b69580745648d4f5b261b03
ZacharieBaeumlin <baeumlin@kth.se>
2022-09-16 11:25:33 +0200

    Changed prints to make it clearer, with colors. Removed useless code lines related to the cost of lanes. Backup the previous RVIZ config file and changed the RVIZ config.

--- AD-EYE/ROS_Packages/src/AD-EYE/src/database_influence_local_planning.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/database_influence_local_planning.cpp
@@ -1,12 +1,12 @@
 // ------------------- Database influence local planning --------------------- //
 /* 
- * This c++ code aims to influence the local planner when a pothole is detected
- * The potholes coordinates come from a SQL database
+ * This c++ code aims to influence the local planner when a obstacle is detected
+ * The obstacles coordinates come from a SQL database
  * 
  * Coded by Nicolas Pelissier on summer 2021
  * Changed by Zacharie Baeumlin on September 2022
  * 
- * Lastly edited on September 14, 2022
+ * Lastly edited on September 16, 2022
 */
 // -------------------------------- Libraries -------------------------------- //
 #include <ros/ros.h>
@@ -24,8 +24,6 @@
 
 #include <math.h>
 
-// #include <iomanip> // to print float numbers with a chosen precision
-
 // ---------------- Parameters concerning the SQL database ------------------- //
 #define DATABASE_NAME "adeye_database"              // before: adeye
 #define DATABASE_HOSTNAME "tcp://127.0.0.1:3306"    // before: tcp://130.237.59.134:3306
@@ -33,7 +31,7 @@
 #define DATABASE_PASSWORD "adeye"                   // before: AdeyeSQL05
 
 // --------------- Parameters concerning the car behaviour ------------------- //
-#define DISTANCE_PARAMETER 20.0   // below this distance, if the lane collides with the pothole, the lane is set to "blocked"
+#define DISTANCE_PARAMETER 20.0   // below this distance, if the lane collides with the obstacle, the lane is blocked
 
 
 // --------------------------------------------------------------------------- //
@@ -43,20 +41,20 @@ private:
     // ROS: Node, publishers, subscribers, msg
     ros::NodeHandle &nh_;           // ROS node
     ros::Publisher pub_traj;        // publisher for the trajectory
-    ros::Publisher pub_potholes;    // publisher for the obstacles
+    ros::Publisher pub_obstacles;    // publisher for the obstacles
     ros::Subscriber sub;
 
     autoware_msgs::LaneArray msg1;  // message for the trajectory influencer
-    visualization_msgs::MarkerArray markers_potholes; // message for the potholes markers
+    visualization_msgs::MarkerArray markers_obstacles; // message for the obstacles markers
 
     //MySQL
     sql::Driver *driver_;
     sql::Connection* conn_;
 
     //MySQL data
-    std::vector<float> pothole_x = {};
-    std::vector<float> pothole_y = {};
-    std::vector<float> pothole_size = {};
+    std::vector<float> obstacle_x = {};
+    std::vector<float> obstacle_y = {};
+    std::vector<float> obstacle_size = {};
 
     void readDatabase()
     {
@@ -75,29 +73,29 @@ private:
                 float _y_ = res->getDouble(2);
                 float _size_ = res->getDouble(3);
 
-                pothole_x.push_back(_x_);
-                pothole_y.push_back(_y_);
-                pothole_size.push_back(_size_);
+                obstacle_x.push_back(_x_);
+                obstacle_y.push_back(_y_);
+                obstacle_size.push_back(_size_);
 
-                std::cout << "GET DATA FROM DATABASE -- pothole " << i << ": " << _x_ << " " << _y_ << " " << _size_ << std::endl; // print the pothole
+                std::cout << "GET DATA FROM DATABASE -- obstacle " << i << ": " << _x_ << " " << _y_ << " " << _size_ << std::endl; // print the obstacle
 
                 visualization_msgs::Marker marker_;
                 marker_.header.frame_id = "world";                  // name of the frame, found with the command rosrun tf view_frames
                 marker_.header.stamp = ros::Time::now();
-                marker_.id = markers_potholes.markers.size();
+                marker_.id = markers_obstacles.markers.size();
                 marker_.type = visualization_msgs::Marker::SPHERE;  // type of marker, can be ARROW, CUBE, SPHERE, ...
-                marker_.scale.x = _size_;                           // the marker size is equal to the pothole size
+                marker_.scale.x = _size_;                           // the marker size is equal to the obstacle size
                 marker_.scale.y = _size_;
                 marker_.scale.z = _size_;
-                marker_.pose.position.x = pothole_x[i];             // link the marker position to the pothole position
-                marker_.pose.position.y = pothole_y[i];
+                marker_.pose.position.x = obstacle_x[i];             // link the marker position to the obstacle position
+                marker_.pose.position.y = obstacle_y[i];
                 marker_.pose.position.z = 0.0;                      // show the marker on the ground
                 marker_.color.a = 1.0;                              // opacity to maximum
                 marker_.color.r = 1.0;                              // set the red color (0 to 1)
                 marker_.color.g = 0.0;                              // set the green color (0 to 1)
                 marker_.color.b = 0.0;                              // set the blue color (0 to 1)
 
-                markers_potholes.markers.push_back(marker_);        // add the marker to the marker array
+                markers_obstacles.markers.push_back(marker_);        // add the marker to the marker array
 
                 i += 1;
             }
@@ -124,7 +122,7 @@ public:
     {
         // Initialize the publishers and subscribers
         pub_traj = nh_.advertise<autoware_msgs::LaneArray>("/adeye/overwrite_trajectory_eval", 1);      // publisher for trajectory changes
-        pub_potholes = nh.advertise<visualization_msgs::MarkerArray>("/adeye/pothole_visualisation",1); // publisher for potholes markers
+        pub_obstacles = nh.advertise<visualization_msgs::MarkerArray>("/adeye/obstacle_visualisation",1); // publisher for obstacles markers
         sub = nh_.subscribe<autoware_msgs::LaneArray>("/local_weighted_trajectories", 1, &databaseInfluenceLocalPlanning::callbackLocalTrajectoryCost, this);
     
         //Connection to the MYSQL database
@@ -149,11 +147,12 @@ public:
         
         bool is_change = false;
 
-        // go accross all potholes and show them
-        for(int k=0; k<pothole_x.size(); k++) {
-            std::cout << "obstacle " << k << ":  x: " << pothole_x[k] << "  y: " << pothole_y[k] << "  size: " << pothole_size[k] << std::endl; }
+        // go accross all obstacles and show them
+        for(int k=0; k<obstacle_x.size(); k++) {
+            std::cout << "obstacle " << k << ":  x: " << obstacle_x[k] << "  y: " << obstacle_y[k] << "  size: " << obstacle_size[k] << std::endl; }
+        std::cout << std::endl;
 
-        pub_potholes.publish(markers_potholes); // publish the marker array to visualise the potholes
+        pub_obstacles.publish(markers_obstacles); // publish the marker array to visualise the obstacles
         
         // go accross all lanes
         for (int i=0; i<lane_v.lanes.size(); i++)
@@ -168,18 +167,16 @@ public:
                 int x = lane_v.lanes[i].waypoints[j].pose.pose.position.x; // get the x position of the waypoint j of lane i
                 int y = lane_v.lanes[i].waypoints[j].pose.pose.position.y; // get the y position of the waypoint j of lane i
 
-                float pothole_distance = 0;
+                float obstacle_distance = 0;
                 float car_distance = std::sqrt(std::pow(std::abs(car_x-x),2.0)+std::pow(std::abs(car_y-y),2.0)); // calculate the distance between the car and the waypoint j of lane i
                 
-                // go accross all potholes
-                for(int k=0; k<pothole_x.size(); k++)
+                // go accross all obstacles
+                for(int k=0; k<obstacle_x.size(); k++)
                 {
-                    float pothole_distance = std::sqrt(std::pow(std::abs(pothole_x[k]-x),2.0)+std::pow(std::abs(pothole_y[k]-y),2.0)); // calculate the distance between the pothole and the waypoint j of lane i
-                    if(pothole_distance < pothole_size[k] && car_distance < DISTANCE_PARAMETER)
+                    float obstacle_distance = std::sqrt(std::pow(std::abs(obstacle_x[k]-x),2.0)+std::pow(std::abs(obstacle_y[k]-y),2.0)); // calculate the distance between the obstacle and the waypoint j of lane i
+                    if(obstacle_distance < obstacle_size[k] && car_distance < DISTANCE_PARAMETER)
                     {
-                        // std::cout << "Distance " << pothole_distance << "  " << car_distance << std::endl;
-                        distance_text += "  -  distance to pothole " + std::to_string(k) + ": " + std::to_string(pothole_distance).substr(0,4) + "  distance to car: " + std::to_string(car_distance).substr(0,4); // print distances with 2 decimales
-                        // lane_v.lanes[i].cost = 0.9;             // change the cost of the lane
+                        distance_text += "  -  distance to obstacle " + std::to_string(k) + ": " + std::to_string(obstacle_distance).substr(0,4) + "  distance to car: " + std::to_string(car_distance).substr(0,4); // print distances with 2 decimales
                         lane_v.lanes[i].is_blocked = 1;         // set the lane to "blocked"
                         is_change = true;                       // retain that a change has been made
                         j=lane_v.lanes[i].waypoints.size();     // exit the loop which goes accross all waypoints
@@ -187,20 +184,16 @@ public:
                 }
             }
             std::string is_blocked_text;  // text to show the status of the lane
-            if (lane_v.lanes[i].is_blocked == 0) { is_blocked_text = "not blocked"; }
-            else { is_blocked_text = "BLOCKED"; }
-
-            std::string lane_cost_text = std::to_string(lane_v.lanes[i].cost);  // text to show the cost of the lane
-            lane_cost_text = lane_cost_text.substr(0,5);                        // keep only 3 decimals of the cost
-
-            // std::cout << "Lane " << i << " : " << lane_v.lanes[i].cost << "  " << std::to_string(lane_v.lanes[i].is_blocked) << std::endl;
-            std::cout << "Lane " << i << ":  cost: " << lane_cost_text << "   (" << is_blocked_text << ")" << distance_text << std::endl;  // print the data of the lane
+            if (lane_v.lanes[i].is_blocked == 0) { is_blocked_text = "\033[32mnot blocked\033[0m"; } // print the text in green
+            else { is_blocked_text = "\033[1;31mBLOCKED\033[0m"; }  // print the text in bold red
+            std::cout << "Lane " << i << ":  " << is_blocked_text << distance_text << std::endl;  // print the data of the lane
         }
         
-        if(!is_change) { lane_v.lanes.clear(); }  
-        else { std::cout << "-- LOCAL PLANNING CHANGED --" << std::endl; }  // make awre the user that the code changes the lanes parameters
+        if(!is_change) { 
+            std::cout << "-- LOCAL PLANNING UNCHANGED --" << std::endl;
+            lane_v.lanes.clear(); }  
+        else { std::cout << "-- \033[1mLOCAL PLANNING CHANGED\033[0m --" << std::endl; }  // make the user aware that the code changed the parameters of lanes (prints in bold white)
 
-        // std::cout << "Size : " << lane_v.lanes.size() << std::endl;  // print the size of lane_v.lanes
         std::cout << std::endl;
         pub_traj.publish(lane_v);   // publish on the topic
     }
