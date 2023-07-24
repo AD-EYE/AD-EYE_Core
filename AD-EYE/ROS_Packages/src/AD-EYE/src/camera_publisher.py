bc9d0552cfbc9efe65b165c0a093fcd8e1b67cfd
ArthurBvt <arthub@kth.se>
2023-07-24 12:01:02 +0200

    Add camera_publisher.py node and ZED1 and ZED2 configuration files.

---
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/camera_publisher.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+
+# source: https://github.com/stereolabs/zed-opencv-native/blob/master/python/zed_opencv_native.py
+
+import numpy as np
+import os
+import configparser
+import sys
+import cv2
+
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+
+# ZED1 SN:
+#SERIAL_NUMBER = 21126
+
+# ZED2 SN:
+SERIAL_NUMBER = 23817167
+
+def start_node():
+    rospy.init_node('camera_pub')
+    rospy.loginfo('camera_pub node started')
+
+
+def load_calibration_file(serial_number) :
+    calibration_file = 'SN' + str(serial_number) + '.conf'
+
+    if os.path.isfile(calibration_file) == True:
+        print("Calibration file found. Loading...")
+    
+    else:
+        print("Calibration file \"" + calibration_file + "\" not found.")
+        print("It can be downloaded from: calib.stereolabs.com/?SN=" + SERIAL_NUMBER)
+
+    return calibration_file
+
+
+def init_calibration(calibration_file, image_size) :
+
+    cameraMarix_left = cameraMatrix_right = map_left_y = map_left_x = map_right_y = map_right_x = np.array([])
+
+    config = configparser.ConfigParser()
+    config.read(calibration_file)
+
+    check_data = True
+    resolution_str = ''
+    if image_size.width == 2208 :
+        resolution_str = '2K'
+    elif image_size.width == 1920 :
+        resolution_str = 'FHD'
+    elif image_size.width == 1280 :
+        resolution_str = 'HD'
+    elif image_size.width == 672 :
+        resolution_str = 'VGA'
+    else:
+        resolution_str = 'HD'
+        check_data = False
+
+    T_ = np.array([-float(config['STEREO']['Baseline'] if 'Baseline' in config['STEREO'] else 0),
+                   float(config['STEREO']['TY_'+resolution_str] if 'TY_'+resolution_str in config['STEREO'] else 0),
+                   float(config['STEREO']['TZ_'+resolution_str] if 'TZ_'+resolution_str in config['STEREO'] else 0)])
+
+    left_cam_cx = float(config['LEFT_CAM_'+resolution_str]['cx'] if 'cx' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_cy = float(config['LEFT_CAM_'+resolution_str]['cy'] if 'cy' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_fx = float(config['LEFT_CAM_'+resolution_str]['fx'] if 'fx' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_fy = float(config['LEFT_CAM_'+resolution_str]['fy'] if 'fy' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_k1 = float(config['LEFT_CAM_'+resolution_str]['k1'] if 'k1' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_k2 = float(config['LEFT_CAM_'+resolution_str]['k2'] if 'k2' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_p1 = float(config['LEFT_CAM_'+resolution_str]['p1'] if 'p1' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_p2 = float(config['LEFT_CAM_'+resolution_str]['p2'] if 'p2' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_p3 = float(config['LEFT_CAM_'+resolution_str]['p3'] if 'p3' in config['LEFT_CAM_'+resolution_str] else 0)
+    left_cam_k3 = float(config['LEFT_CAM_'+resolution_str]['k3'] if 'k3' in config['LEFT_CAM_'+resolution_str] else 0)
+
+
+    right_cam_cx = float(config['RIGHT_CAM_'+resolution_str]['cx'] if 'cx' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_cy = float(config['RIGHT_CAM_'+resolution_str]['cy'] if 'cy' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_fx = float(config['RIGHT_CAM_'+resolution_str]['fx'] if 'fx' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_fy = float(config['RIGHT_CAM_'+resolution_str]['fy'] if 'fy' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_k1 = float(config['RIGHT_CAM_'+resolution_str]['k1'] if 'k1' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_k2 = float(config['RIGHT_CAM_'+resolution_str]['k2'] if 'k2' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_p1 = float(config['RIGHT_CAM_'+resolution_str]['p1'] if 'p1' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_p2 = float(config['RIGHT_CAM_'+resolution_str]['p2'] if 'p2' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_p3 = float(config['RIGHT_CAM_'+resolution_str]['p3'] if 'p3' in config['RIGHT_CAM_'+resolution_str] else 0)
+    right_cam_k3 = float(config['RIGHT_CAM_'+resolution_str]['k3'] if 'k3' in config['RIGHT_CAM_'+resolution_str] else 0)
+
+    R_zed = np.array([float(config['STEREO']['RX_'+resolution_str] if 'RX_' + resolution_str in config['STEREO'] else 0),
+                      float(config['STEREO']['CV_'+resolution_str] if 'CV_' + resolution_str in config['STEREO'] else 0),
+                      float(config['STEREO']['RZ_'+resolution_str] if 'RZ_' + resolution_str in config['STEREO'] else 0)])
+
+    R, _ = cv2.Rodrigues(R_zed)
+    cameraMatrix_left = np.array([[left_cam_fx, 0, left_cam_cx],
+                         [0, left_cam_fy, left_cam_cy],
+                         [0, 0, 1]])
+
+    cameraMatrix_right = np.array([[right_cam_fx, 0, right_cam_cx],
+                          [0, right_cam_fy, right_cam_cy],
+                          [0, 0, 1]])
+
+    distCoeffs_left = np.array([[left_cam_k1], [left_cam_k2], [left_cam_p1], [left_cam_p2], [left_cam_k3]])
+
+    distCoeffs_right = np.array([[right_cam_k1], [right_cam_k2], [right_cam_p1], [right_cam_p2], [right_cam_k3]])
+
+    T = np.array([[T_[0]], [T_[1]], [T_[2]]])
+    R1 = R2 = P1 = P2 = np.array([])
+
+    R1, R2, P1, P2 = cv2.stereoRectify(cameraMatrix1=cameraMatrix_left,
+                                       cameraMatrix2=cameraMatrix_right,
+                                       distCoeffs1=distCoeffs_left,
+                                       distCoeffs2=distCoeffs_right,
+                                       R=R, T=T,
+                                       flags=cv2.CALIB_ZERO_DISPARITY,
+                                       alpha=0,
+                                       imageSize=(image_size.width, image_size.height),
+                                       newImageSize=(image_size.width, image_size.height))[0:4]
+
+    map_left_x, map_left_y = cv2.initUndistortRectifyMap(cameraMatrix_left, distCoeffs_left, R1, P1, (image_size.width, image_size.height), cv2.CV_32FC1)
+    map_right_x, map_right_y = cv2.initUndistortRectifyMap(cameraMatrix_right, distCoeffs_right, R2, P2, (image_size.width, image_size.height), cv2.CV_32FC1)
+
+    cameraMatrix_left = P1
+    cameraMatrix_right = P2
+
+    return cameraMatrix_left, cameraMatrix_right, map_left_x, map_left_y, map_right_x, map_right_y
+
+
+# 2K: 2208.1242; FHD: 1920.1080; HD: 1280.720
+class Resolution :
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+
+def main() :
+
+    try:
+        start_node()
+    except rospy.ROSInterruptException:
+        pass
+
+    # Open the ZED camera
+    cap = cv2.VideoCapture(0)
+
+    # Check for correct initialization
+    if cap.isOpened() == 0:
+        print("Video capture initialization fail")
+        exit(-1)
+    else :
+        print("Starting video capture")
+
+    # Set the video resolution
+    image_size = Resolution(1920, 1080)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, image_size.width * 2)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size.height)
+
+    print("Image resolution set to " + str(image_size.width) + "*" + str(image_size.height))
+
+    # Open calibration file
+    calibration_file = load_calibration_file(SERIAL_NUMBER)
+
+    # Compute calibration
+    camera_matrix_left, camera_matrix_right, map_left_x, map_left_y, map_right_x, map_right_y = init_calibration(calibration_file, image_size)
+
+    print("Calibration initiated")
+
+    bridge = CvBridge()
+    left_pub = rospy.Publisher('/camera_1/image_raw', Image, queue_size=10)
+    right_pub = rospy.Publisher('/camera_2/image_raw', Image, queue_size=10)
+
+    while not rospy.is_shutdown():
+        # Get a new frame from camera
+        retval, frame = cap.read()
+        # Extract left and right images from side-by-side
+        left_right_image = np.split(frame, 2, axis=1)
+
+        # Raw images
+        #cv2.imshow("left RAW", left_right_image[0])
+        #cv2.imshow("right RAW", left_right_image[0])
+
+        # Rectified images
+        left_rect = cv2.remap(left_right_image[0], map_left_x, map_left_y, interpolation=cv2.INTER_LINEAR)
+        right_rect = cv2.remap(left_right_image[1], map_right_x, map_right_y, interpolation=cv2.INTER_LINEAR)
+
+        # Converting OpenCV image to ROS image message
+        left_msg = bridge.cv2_to_imgmsg(left_rect, "bgr8")
+        right_msg = bridge.cv2_to_imgmsg(right_rect, "bgr8")
+
+        print("Publishing frame")
+
+        left_pub.publish(left_msg)
+        right_pub.publish(right_msg)
+
+    exit(0)
+
+if __name__ == "__main__":
+    main()
