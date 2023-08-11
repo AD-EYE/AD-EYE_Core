b422e7a75340b5e04d82c8144a5eebe6a1ca5ae3
ArthurBvt <arthub@kth.se>
2023-08-11 11:32:28 +0200

    Bugfix point_cloud_broadcaster

--- AD-EYE/ROS_Packages/src/AD-EYE/src/point_cloud_broadcaster.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/point_cloud_broadcaster.py
@@ -6,41 +6,55 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import PointField
 import numpy as np
 
-##A function to turn the raws points from floats to strings and publish them to the /points_raw topic
-#@param data A Float32MultiArray message 
-def callback_sim(data):
-    msg = PointCloud2()
-    msg.header.stamp = rospy.Time.now()
-    msg.header.frame_id = "velodyne"  # MO "velodyne"
-    msg.height = 1
-    msg.width = len(data.data) / 4  # ADD data.size # ??
 
-    msg.fields = [
-        PointField('x', 0, PointField.FLOAT32, 1),
-        PointField('y', 4, PointField.FLOAT32, 1),
-        PointField('z', 8, PointField.FLOAT32, 1),
-        PointField('intensity', 12, PointField.FLOAT32, 1)]
-    msg.is_bigendian = False
-    msg.point_step = 16
-    msg.row_step = msg.point_step * msg.width
-    msg.is_dense = False
-    msg.data = np.asarray(data.data, np.float32).tostring()
+class point_cloud_broadcaster_Sim():
 
-    pub.publish(msg)
+    def __init__(self):
+        self.sub = rospy.Subscriber("/points_raw_float32", Float32MultiArray, self.callback)
+        self.pub = rospy.Publisher('/points_raw', PointCloud2, queue_size=1)
+
+        self.msg = PointCloud2()
+
+    ##A function to turn the raws points from floats to strings and publish them to the /points_raw topic
+    #@param data A Float32MultiArray message 
+    def callback(self, data):
+        self.msg.header.stamp = rospy.Time.now()
+        self.msg.header.frame_id = "velodyne"  # MO "velodyne"
+        self.msg.height = 1
+        self.msg.width = len(data.data) / 4  # ADD data.size # ??
+
+        self.msg.fields = [
+            PointField('x', 0, PointField.FLOAT32, 1),
+            PointField('y', 4, PointField.FLOAT32, 1),
+            PointField('z', 8, PointField.FLOAT32, 1),
+            PointField('intensity', 12, PointField.FLOAT32, 1)]
+        self.msg.is_bigendian = False
+        self.msg.point_step = 16
+        self.msg.row_step = self.msg.point_step * self.msg.width
+        self.msg.is_dense = False
+        self.msg.data = np.asarray(data.data, np.float32).tostring()
+
+        self.pub.publish(self.msg)
 
 
-def callback_realWorld(data):
-    pub.publish(data)
+class point_cloud_broadcaster_RealWorld():
+
+    def __init__(self):
+        self.sub = rospy.Subscriber("/velodyne_points", PointCloud2, self.callback)
+        self.pub = rospy.Publisher('/points_raw', PointCloud2, queue_size=1)
+
+
+    def callback(self, data):
+        self.pub.publish(data)
 
 
 if __name__ == '__main__':
     rospy.init_node('point_cloud_broadcaster', anonymous=True)
 
-    if rospy.get_param("sensing/lidar_source", "Simulation"):
-        rospy.Subscriber("/points_raw_float32", Float32MultiArray, callback_sim)
+    if rospy.get_param("sensing/lidar_source") == "Simulation":
+        point_cloud_broadcaster_Sim()
     
-    if rospy.get_param("sensing/lidar_source", "RealWorld"):
-        rospy.Subscriber("/velodyne_points", PointCloud2, callback_realWorld)
-    
-    pub = rospy.Publisher('/points_raw', PointCloud2, queue_size=1)
+    if rospy.get_param("sensing/lidar_source") == "RealWorld":
+        point_cloud_broadcaster_RealWorld()
+
     rospy.spin()
