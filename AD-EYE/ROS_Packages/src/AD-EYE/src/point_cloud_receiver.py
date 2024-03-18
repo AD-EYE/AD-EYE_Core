5b1b9360d32f481146b172e823250cf5e0dbbc46
Konstantin Tyurin <konstaty@kth.se>
2024-03-18 15:03:54 +0100

    Republish PointCloud messages with current time

--- AD-EYE/ROS_Packages/src/AD-EYE/src/point_cloud_receiver.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/point_cloud_receiver.py
@@ -7,7 +7,7 @@ from sensor_msgs.msg import PointField
 import numpy as np
 
 ##A function to turn the raws points from floats to strings and publish them to the /points_raw topic
-#@param data A Float32MultiArray message 
+#@param data A Float32MultiArray message
 def mycallback(data):
     msg = PointCloud2()
     msg.header.stamp = rospy.Time.now()
@@ -29,9 +29,15 @@ def mycallback(data):
     pub.publish(msg)
 
 
+def pointsCallback(msg):
+    msg.header.stamp = rospy.Time.now()
+    pub.publish(msg)
+
+
 if __name__ == '__main__':
     rospy.init_node('point_cloud_receiver', anonymous=True)
 
     rospy.Subscriber("/points_raw_float32", Float32MultiArray, mycallback)
+    rospy.Subscriber("/ouster/points", PointCloud2, pointsCallback)
     pub = rospy.Publisher('/points_raw', PointCloud2, queue_size=1)
     rospy.spin()
