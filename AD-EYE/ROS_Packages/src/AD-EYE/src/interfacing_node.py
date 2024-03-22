466b9e7be2cc7811a43c71bad727993cd8c722b0
Péter Telkes <ptelkes@kth.se>
2024-03-22 11:17:02 +0100

    Change car velocity topic
    Changed the topic for reading the car's velocity
    to "/estimated_vel_kmph" to match with the one used by the GUI.
    
    Car velocity is published to Tecosa edge in km/h, NOT m/s.

--- AD-EYE/ROS_Packages/src/AD-EYE/src/interfacing_node.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/interfacing_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import UInt8, Float64, Bool
+from std_msgs.msg import UInt8, Bool, Float32
 from geometry_msgs.msg import PoseArray, TwistStamped
 from sensor_msgs.msg import NavSatFix
 
@@ -10,13 +10,13 @@ class InterfaceNode:
         self.ext_publisher = rospy.Publisher("/sensor_detected_objects", PoseArray, queue_size=1)
         self.rec_speed_publisher = rospy.Publisher("/suggested_speed", UInt8, queue_size=1)
         self.gps_publisher = rospy.Publisher("/tecosa/car_location", NavSatFix, queue_size=1)
-        self.car_speed_publisher = rospy.Publisher("/tecosa/car_speed", Float64, queue_size=1)
+        self.car_speed_publisher = rospy.Publisher("/tecosa/car_speed", Float32, queue_size=1)
         self.warning_publisher = rospy.Publisher("/warning", Bool, queue_size=1)
         
         self.ext_subscriber = rospy.Subscriber("/tecosa/external_detections", PoseArray, self.external_objects_callback, queue_size=1)
         self.rec_speed_subscriber = rospy.Subscriber("/tecosa/recommended_speed", UInt8, self.recommended_speed_callback, queue_size=1)
         self.gps_subscriber = rospy.Subscriber("/fix", NavSatFix, self.gps_callback, queue_size=1)
-        self.car_speed_subscriber = rospy.Subscriber("/current_velocity", TwistStamped, self.car_velocity_callback, queue_size=1)
+        self.car_speed_subscriber = rospy.Subscriber("/estimated_vel_kmph", Float32, self.car_velocity_callback, queue_size=1)
         self.warning_subscriber = rospy.Subscriber("/tecosa/warning", Bool, self.warning_callback, queue_size=1)
 
     def external_objects_callback(self, external_objects):
@@ -29,9 +29,9 @@ class InterfaceNode:
     def gps_callback(self, gps_fix):
         self.gps_publisher.publish(gps_fix)
 
-    # car velocity in m/s
+    # car velocity in km/h
     def car_velocity_callback(self, velocity):
-        self.car_speed_publisher.publish(velocity.twist.linear.x)
+        self.car_speed_publisher.publish(velocity)
 
     def warning_callback(self, is_warning):
         self.warning_publisher.publish(is_warning)
