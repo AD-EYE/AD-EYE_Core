79a0c3104dd21044825f0fca104762951e4e9b74
mer-kos <merkos@ug.kth.se>
2024-03-27 14:10:28 +0100

    goal position file updated to place it after the intersection

--- AD-EYE/ROS_Packages/src/AD-EYE/src/sample_goal_publisher.py
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/sample_goal_publisher.py
@@ -2,29 +2,45 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
+import csv
+
+def parse_pose_line(line):
+    # Parse a line of the pose file and extract the values
+    values = line.strip().split(',')
+    
+    # Skip lines that don't have the correct number of values
+    if len(values) != 10:
+        return None 
+
+    pose_msg = PoseStamped()
+    pose_msg.header.frame_id = "world"
+    #pose_msg.header.seq = int(values[0])
+    pose_msg.pose.position.x = 342.187774658
+    pose_msg.pose.position.y = -521.295288086
+    pose_msg.pose.position.z = 0
+    pose_msg.pose.orientation.x = 0
+    pose_msg.pose.orientation.y = 0
+    pose_msg.pose.orientation.z = -0.703769872127
+    pose_msg.pose.orientation.w = 0.710428016822
+    return pose_msg
 
 if __name__ == '__main__':
     rospy.init_node('sample_goal_publisher', anonymous=True)
     rate = rospy.Rate(10.0)
     pub = rospy.Publisher('/adeye/goals', PoseStamped, queue_size=1)
-    counter = 0
 
-    pose_msg = PoseStamped()
-    pose_msg.header.frame_id = "world"
-    pose_msg.header.seq = 1
-    pose_msg.pose.position.x = 345.0
-    pose_msg.pose.position.y = -605.0
-    pose_msg.pose.position.z = 0
-    pose_msg.pose.orientation.x = 0.0
-    pose_msg.pose.orientation.y = 0.0
-    pose_msg.pose.orientation.z = 0.839
-    pose_msg.pose.orientation.w = 0.545
-    
-    #simulation only publishes a single adeye goal message. so publishing message only after a subscriber has registered
-    while not rospy.is_shutdown():
-        if (pub.get_num_connections() != 0):
-            break
-        rate.sleep()
+    file_path = '/home/adeye/AD-EYE_Core/AD-EYE/Experiments/KTH_Demo/Vector_Map_Files/VECTORMAP_FILE/point.csv' 
 
-    pub.publish(pose_msg)
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        # Skip header since we don't have any headers (based on supposition, we can think that the different components are like defined previously)
+        next(csv_reader) 
+        for line in csv_reader:
+            pose_msg = parse_pose_line(','.join(line))
+            if pose_msg:
+                while pub.get_num_connections() == 0 and not rospy.is_shutdown():
+                    rate.sleep()
+                pub.publish(pose_msg)
+                rate.sleep()
+
     rospy.spin()
