4f0cf0f4a8b8b9092d0f721c2f8e878bc6fba68e
Péter Telkes <ptelkes@kth.se>
2024-05-20 11:08:01 +0200

    Add non-working PID code
    Added the currently non-working code for using the PID controller.
    Needs to be revised and changed before this can be merged.

--- AD-EYE/ROS_Packages/src/AD-EYE/src/twist_to_vehicle.cpp
+++ AD-EYE/ROS_Packages/src/AD-EYE/src/twist_to_vehicle.cpp
@@ -6,85 +6,93 @@
 #include <std_msgs/Float64.h>
 #include "PID.h"
 
-using namespace std;
-
-// TODO
-
 class TwistToVehicle {
 public:
     TwistToVehicle(ros::NodeHandle *nh) {
         linear_speed_target_ = 0.0;
-        linear_speed_current_ = 0.0;
         angular_speed_target_ = 0.0;
+        linear_speed_current_ = 0.0;
+        angular_speed_current_ = 0.0;
 
         P_ = 1.0;
         I_ = 0.0;
         D_ = 0.0;
 
+        accelPIDController = PIDController<double>(P_, I_, D_, pidSource, pidOutput);
+        accelPIDController.registerTimeFunction(pidTimeFunction);
+
         pub_acceleration = nh->advertise<std_msgs::Float64>("acceleration_command", 1);
-        pub_steering_angle = nh->advertise<std_msgs::Float64>("steering_angle_command", 1);
     }
 
     void twistSCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
-        ROS_INFO("Desired TwistS: Long. Vel. = %f, Rot. Spd. = %f", msg->twist.linear.x, msg->twist.angular.z);
+        ROS_INFO("Desired TwistS: Long. Vel. = %s, Rot. Spd. = %s", msg->twist.linear.x, msg->twist.angular.z);
         linear_speed_target_ = msg->twist.linear.x;
         angular_speed_target_ = msg->twist.angular.z;
 
-        double acceleration_command = (linear_speed_target_ - linear_speed_current_) * P_;
-        sendAcceleration(acceleration_command);
-
-        double steering_angle_command = steering(angular_speed_target_);
-        sendSteeringAngle(steering(angular_speed_target_));
+        accelPIDController.setTarget(linear_speed_target_);
     }
 
     void currentVelocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
-        ROS_INFO("Current velocity: Long. Vel. = %f, Rot. Spd. = %f", msg->twist.linear.x, msg->twist.angular.z);
+        ROS_INFO("Current velocity: Long. Vel. = %s, Rot. Spd. = %s", msg->twist.linear.x, msg->twist.angular.z);
         linear_speed_current_ = msg->twist.linear.x;
+        angular_speed_current_ = msg->twist.angular.z;
+    }
+
+    void updatePID() {
+        accelPIDController.tick();
+    }
+
+    void updateSteering() {
+        steering(angular_speed_target_);
     }
 
 private:
     void sendAcceleration(double req) {
-        std_msgs::Float64 accel;
-        accel.data = req;
-        pub_acceleration.publish(accel);
+        pub_acceleration.publish(req);
     }
 
-    void sendSteeringAngle(double req) {
-        std_msgs::Float64 steering;
-        steering.data = req;
-        pub_steering_angle.publish(steering);
-    }
+    void steering(double angular_vel) {
+        using namespace std;
 
-    double steering(double angular_vel) {
         static const double WHEELBASE = 2.984;
         static const double MAX_STEERING_ANGLE = 0.85;
         static const double MAX_STEERING_TAN = tan(MAX_STEERING_ANGLE);
 
-        double steer_ag_req;
-
         if (linear_speed_current_ == 0.0)
         {
-            steer_ag_req = 0.0;
+            double steer_ag_req = 0.0;
         }
         else
         {
-            steer_ag_req = atan(max(min(WHEELBASE * angular_vel / linear_speed_current_, MAX_STEERING_TAN), -MAX_STEERING_TAN));
+            double steer_ag_req = atan(max(min(WHEELBASE * angular_vel / linear_speed_current_, MAX_STEERING_TAN), -MAX_STEERING_TAN));
         }
+        // steer_ag_req = update_rate_limiter(steer_ag_req, spd, 10.0);
+    }
 
-        return steer_ag_req;
+    double pidSource() {
+        return linear_speed_current_;
+    }
+
+    void pidOutput(double acceleration) {
+        sendAcceleration(acceleration);
+    }
+
+    unsigned long pidTimeFunction() {
+        unsigned long time = (ros::Time::now().toSec() * 1000);
+        return time;
     }
 
 private:
     double linear_speed_target_;
-    double linear_speed_current_;
     double angular_speed_target_;
+    double linear_speed_current_;
+    double angular_speed_current_;
 
     double P_, I_, D_;
 
+    PIDController<double> accelPIDController;
     ros::Publisher pub_acceleration;
-    ros::Publisher pub_steering_angle;
 };
-
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "twist_to_vehicle");
@@ -93,8 +101,10 @@ int main(int argc, char **argv) {
     ros::Subscriber sub_twists = nh.subscribe("TwistS", 2, &TwistToVehicle::twistSCallback, &ttv);
     ros::Subscriber sub_currentv = nh.subscribe("current_velocity", 2, &TwistToVehicle::currentVelocityCallback, &ttv);
     ros::Rate r(50);
-    while (ros::ok())
+    while (1)
     {
+        ttv.updatePID();
+        ttv.updateSteering();
         ros::spinOnce();
         r.sleep();
     }
