475f18b62be69f58f58b9b03cdd74f9f91435c31
mktomaszuk <tomaszuk@kth.se>
2025-06-12 12:48:57 +0200

    Updates to the performance log reader script

--- Helper_Scripts/performance_log_reader.py
+++ Helper_Scripts/performance_log_reader.py
@@ -18,78 +18,99 @@ def file_to_lines(filename) -> list:
 
 def read_line(line: str, simple_names: bool = False) -> list:
     """Read and translate a single log line into a list."""
+
     words = line.split()
-    if len(words):
-        if words[0] == "PID":
-            return ["NEWFRAME"]
 
-        elif words[0].isnumeric():
-            command = words[11]
+    if line.startswith("top -"):
+        return ["NEWFRAME", words[2]]
+
+    elif len(words) and words[0].isnumeric():
+        pid = words[0]
+        command = words[11]
+        if "python" in command and len(words) > 12:
+            command += " " + words[12]
+
+
+        if simple_names and not command.startswith("["):
             if "python" in command:
-                command += " " +words[12]
+                split_command = command.split(" ")
+                split_path = split_command[1].split("/")
+                command = split_command[0] + " " + split_path[-1]
+            else:
+                split_path = command.split("/")
+                command = split_path[-1]
 
-            if simple_names and not command.startswith("["):
-                if "python" in command:
-                    split_command = command.split(" ")
-                    split_path = split_command[1].split("/")
-                    command = split_command[0] + " " + split_path[-1]
-                else:
-                    split_path = command.split("/")
-                    command = split_path[-1]
+        cpu = float(words[8].replace(",","."))
+        mem = float(words[9].replace(",","."))
 
-            cpu = float(words[8].replace(",","."))
-            mem = float(words[9].replace(",","."))
-
-            if cpu > 0.0:
-                return [command, cpu, mem]
+        if cpu > 0.0 or mem > 0.0:
+            return [pid, cpu, mem, command]
 
     return [""]
 
+def timestr2seconds(time_str: str, start_time: str) -> float:
+    """Converts a 'timestring' of format HH:MM:SS to just seconds relative to earlier starting time."""
+
+    start_split = start_time.split(":")
+    time_split = time_str.split(":")
+
+    hours = (float(time_split[0]) - float(start_split[0])) % 24
+    minutes = float(time_split[1]) - float(start_split[1])
+    seconds = float(time_split[2]) - float(start_split[2])
+
+    total_seconds = seconds + 60 * minutes + 3600 * hours
+    return total_seconds
 
 def log_to_dataframe(filename: str, simple_names: bool = False) -> pd.DataFrame:
     """Wrapper that opens a given text file and spits out a formatted DataFrame."""
     lines = file_to_lines(filename)
 
-    frame = -1
+    start_time = ""
+    current_time = 0.0
     total_trace = ["total", 0.0, 0.0]
     all_lists = []
     trace_list = {}
+    pid_lut = {}
 
     for line in lines:
         out_list = read_line(line=line, simple_names=simple_names)
-
         if out_list[0] == "NEWFRAME":
-            frame += 1
-            all_lists.append([frame] + total_trace)
-            total_trace = ["total", 0.0, 0.0]
+            if start_time == "":
+                start_time = out_list[1]
+            else:
+                all_lists.append([current_time] + total_trace)
+                total_trace = ["total", 0.0, 0.0]
+                current_time = timestr2seconds(out_list[1], start_time)
 
 
         elif out_list[0] != "":
             if out_list[0] not in trace_list:
                 trace_list[out_list[0]] = 0.0
+                pid_lut[out_list[0]] = out_list[3]
             trace_list[out_list[0]] += out_list[1]
 
-            all_lists.append([frame] + out_list)
+            all_lists.append([current_time] + out_list[0:2])
             total_trace[1] += out_list[1]
             total_trace[2] += out_list[2]
 
-    columns_list = ["frame", "command", "cpu", "mem"]
+    all_lists.append([current_time] + total_trace) # last frame total
+    columns_list = ["frame", "pid", "cpu", "mem"]
     log_data_df = pd.DataFrame(data=all_lists, columns=columns_list)
 
     trace_list = sorted(trace_list.items(), reverse=True, key=lambda kv: kv[1])
 
-    return log_data_df, trace_list
+    return log_data_df, trace_list, pid_lut
 
 
 def plot_log_dataframe(filename: str, max_traces: int, plot_total: bool = False, simple_names: bool = False):
     """Plot points from dataframe."""
 
-    log_data_df, all_traces = log_to_dataframe(filename=filename, simple_names=simple_names)
+    log_data_df, all_traces, pid_lut = log_to_dataframe(filename=filename, simple_names=simple_names)
 
-    _fig, axs = plt.subplots(2,1)
+    fig, axs = plt.subplots(2,1)
 
     if plot_total:
-        temp_df = log_data_df.loc[log_data_df["command"] == "total"]
+        temp_df = log_data_df.loc[log_data_df["pid"] == "total"]
         x_axis = temp_df["frame"]
         y1_axis = temp_df["cpu"]
         axs[0].plot(x_axis, y1_axis, label="total")
@@ -98,8 +119,9 @@ def plot_log_dataframe(filename: str, max_traces: int, plot_total: bool = False,
 
     traces = 0
     for trace in all_traces:
-        command = trace[0]
-        temp_df = log_data_df.loc[log_data_df["command"] == command]
+        pid = trace[0]
+        command = pid_lut[pid]
+        temp_df = log_data_df.loc[log_data_df["pid"] == pid]
         x_axis = temp_df["frame"]
         y1_axis = temp_df["cpu"]
         axs[0].plot(x_axis, y1_axis, label=command)
@@ -109,8 +131,14 @@ def plot_log_dataframe(filename: str, max_traces: int, plot_total: bool = False,
         if traces >= max_traces:
             break
 
+    fig.suptitle(filename)
+    axs[0].set_title("CPU usage")
+    axs[0].set_xlabel("time (s)")
+    axs[0].set_ylabel("percentage (1 CPU core equivalent)")
+    axs[1].set_title("Memory usage")
+    axs[1].set_xlabel("time (s)")
+    axs[1].set_ylabel("percentage total")
     axs[1].legend(loc="upper right")
-    plt.show()
 
 def plot_performance_logs_wrapper(args: argparse.Namespace):
     """Wrapper for plotting performance logs."""
@@ -120,25 +148,18 @@ def plot_performance_logs_wrapper(args: argparse.Namespace):
                            max_traces=args.max_traces,
                            plot_total=args.plot_total,
                            simple_names=args.simple_names)
+        plt.show()
     else:
         entries = os.scandir(args.folder)
         sorted_entries = sorted(entries, key=lambda entry: entry.name)
 
-        trace_count = 0
         for file in sorted_entries:
             if file.name.endswith(".log"):
-                file_df = log_to_dataframe(filename=file.path, max_traces=0, plot_total=True)
-                x_axis = file_df["frame"]
-                y_axis = file_df["cpu"]
-                plt.plot(x_axis, y_axis, label=file.name)
-                trace_count += 1
-                if trace_count == 7:
-                    plt.legend()
-                    plt.figure()
-                    trace_count = 0
-        plt.legend()
+                plot_log_dataframe(filename=file.path,
+                                   max_traces=args.max_traces,
+                                   plot_total=args.plot_total,
+                                   simple_names=args.simple_names)
         plt.show()
-
 
 if __name__ == "__main__":
 
